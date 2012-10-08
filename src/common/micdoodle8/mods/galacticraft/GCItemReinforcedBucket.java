530f1a051216b00b268111ee67ac2d6568479863
Mitchell Petrie <mitchpetrie29@gmail.com>
2012-10-08 14:37:42 -0400

    Added reinforced buckets

---
+++ src/common/micdoodle8/mods/galacticraft/GCItemReinforcedBucket.java
@@ -0,0 +1,299 @@
+package micdoodle8.mods.galacticraft;
+
+import net.minecraft.src.Block;
+import net.minecraft.src.CreativeTabs;
+import net.minecraft.src.EntityCow;
+import net.minecraft.src.EntityPlayer;
+import net.minecraft.src.EnumAction;
+import net.minecraft.src.EnumMovingObjectType;
+import net.minecraft.src.Item;
+import net.minecraft.src.ItemStack;
+import net.minecraft.src.Material;
+import net.minecraft.src.MovingObjectPosition;
+import net.minecraft.src.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.FillBucketEvent;
+import cpw.mods.fml.common.FMLLog;
+
+public class GCItemReinforcedBucket extends GCItem
+{
+	public int isFull;
+	
+    public GCItemReinforcedBucket(int par1, int par2)
+    {
+        super(par1);
+        this.maxStackSize = 1;
+        this.isFull = par2;
+        this.setCreativeTab(CreativeTabs.tabMisc);
+    }
+
+    @Override
+    public ItemStack onFoodEaten(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
+    {
+    	if (this.shiftedIndex != GCItems.reinforcedBucketMilk.shiftedIndex)
+    	{
+    		super.onFoodEaten(par1ItemStack, par2World, par3EntityPlayer);
+    	}
+    	else
+    	{
+            if (!par3EntityPlayer.capabilities.isCreativeMode)
+            {
+                --par1ItemStack.stackSize;
+            }
+
+            if (!par2World.isRemote)
+            {
+                par3EntityPlayer.curePotionEffects(par1ItemStack);
+            }
+    	}
+
+        return par1ItemStack.stackSize <= 0 ? new ItemStack(GCItems.reinforcedBucketEmpty) : par1ItemStack;
+    }
+    
+    @Override
+    public int getMaxItemUseDuration(ItemStack par1ItemStack)
+    {
+    	if (this.shiftedIndex != GCItems.reinforcedBucketMilk.shiftedIndex)
+    	{
+    		return 0;
+    	}
+    	else
+    	{
+            return 32;
+    	}
+    }
+    
+    @Override
+    public EnumAction getItemUseAction(ItemStack par1ItemStack)
+    {
+    	if (this.shiftedIndex != GCItems.reinforcedBucketMilk.shiftedIndex)
+    	{
+    		return EnumAction.none;
+    	}
+    	else
+    	{
+            return EnumAction.drink;
+    	}
+    }
+    
+    @Override
+    public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
+    {
+    	if (this.shiftedIndex != GCItems.reinforcedBucketMilk.shiftedIndex)
+    	{
+    		float var4 = 1.0F;
+            double var5 = par3EntityPlayer.prevPosX + (par3EntityPlayer.posX - par3EntityPlayer.prevPosX) * (double)var4;
+            double var7 = par3EntityPlayer.prevPosY + (par3EntityPlayer.posY - par3EntityPlayer.prevPosY) * (double)var4 + 1.62D - (double)par3EntityPlayer.yOffset;
+            double var9 = par3EntityPlayer.prevPosZ + (par3EntityPlayer.posZ - par3EntityPlayer.prevPosZ) * (double)var4;
+            boolean var11 = this.isFull == 0;
+            MovingObjectPosition var12 = this.getMovingObjectPositionFromPlayer(par2World, par3EntityPlayer, var11);
+
+            if (var12 == null)
+            {
+                return par1ItemStack;
+            }
+            else
+            {
+                FillBucketEvent event = new FillBucketEvent(par3EntityPlayer, par1ItemStack, par2World, var12);
+                if (MinecraftForge.EVENT_BUS.post(event))
+                {
+                    return par1ItemStack;
+                }
+
+                if (event.isHandeled())
+                {
+                    if (par3EntityPlayer.capabilities.isCreativeMode)
+                    {
+                        return par1ItemStack;
+                    }
+
+                    if (--par1ItemStack.stackSize <= 0)
+                    {
+                        return event.result;
+                    }
+
+                    if (!par3EntityPlayer.inventory.addItemStackToInventory(event.result))
+                    {
+                        par3EntityPlayer.dropPlayerItem(event.result);
+                    }
+
+                    return par1ItemStack;
+                }
+
+                if (var12.typeOfHit == EnumMovingObjectType.TILE)
+                {
+                    int var13 = var12.blockX;
+                    int var14 = var12.blockY;
+                    int var15 = var12.blockZ;
+
+                    if (!par2World.canMineBlock(par3EntityPlayer, var13, var14, var15))
+                    {
+                        return par1ItemStack;
+                    }
+
+                    if (this.isFull == 0)
+                    {
+                        if (!par3EntityPlayer.canPlayerEdit(var13, var14, var15))
+                        {
+                            return par1ItemStack;
+                        }
+
+                        if (par2World.getBlockMaterial(var13, var14, var15) == Material.water && par2World.getBlockMetadata(var13, var14, var15) == 0)
+                        {
+                            par2World.setBlockWithNotify(var13, var14, var15, 0);
+
+                            if (par3EntityPlayer.capabilities.isCreativeMode)
+                            {
+                                return par1ItemStack;
+                            }
+
+                            if (--par1ItemStack.stackSize <= 0)
+                            {
+                                return new ItemStack(GCItems.reinforcedBucketWater);
+                            }
+
+                            if (!par3EntityPlayer.inventory.addItemStackToInventory(new ItemStack(GCItems.reinforcedBucketWater)))
+                            {
+                                par3EntityPlayer.dropPlayerItem(new ItemStack(GCItems.reinforcedBucketWater.shiftedIndex, 1, 0));
+                            }
+
+                            return par1ItemStack;
+                        }
+
+                        if (par2World.getBlockMaterial(var13, var14, var15) == Material.lava && par2World.getBlockMetadata(var13, var14, var15) == 0)
+                        {
+                            par2World.setBlockWithNotify(var13, var14, var15, 0);
+
+                            if (par3EntityPlayer.capabilities.isCreativeMode)
+                            {
+                                return par1ItemStack;
+                            }
+
+                            if (--par1ItemStack.stackSize <= 0)
+                            {
+                                return new ItemStack(GCItems.reinforcedBucketLava);
+                            }
+
+                            if (!par3EntityPlayer.inventory.addItemStackToInventory(new ItemStack(GCItems.reinforcedBucketLava)))
+                            {
+                                par3EntityPlayer.dropPlayerItem(new ItemStack(GCItems.reinforcedBucketLava.shiftedIndex, 1, 0));
+                            }
+
+                            return par1ItemStack;
+                        }
+
+                        if (par2World.getBlockMaterial(var13, var14, var15) == GCBlocks.bacterialSludge && par2World.getBlockMetadata(var13, var14, var15) == 0)
+                        {
+                            par2World.setBlockWithNotify(var13, var14, var15, 0);
+
+                            if (par3EntityPlayer.capabilities.isCreativeMode)
+                            {
+                                return par1ItemStack;
+                            }
+
+                            if (--par1ItemStack.stackSize <= 0)
+                            {
+                                return new ItemStack(GCItems.reinforcedBucketBacteria);
+                            }
+
+                            if (!par3EntityPlayer.inventory.addItemStackToInventory(new ItemStack(GCItems.reinforcedBucketBacteria)))
+                            {
+                                par3EntityPlayer.dropPlayerItem(new ItemStack(GCItems.reinforcedBucketBacteria.shiftedIndex, 1, 0));
+                            }
+
+                            return par1ItemStack;
+                        }
+                    }
+                    else
+                    {
+                        if (this.isFull < 0)
+                        {
+                            return new ItemStack(GCItems.reinforcedBucketEmpty);
+                        }
+
+                        if (var12.sideHit == 0)
+                        {
+                            --var14;
+                        }
+
+                        if (var12.sideHit == 1)
+                        {
+                            ++var14;
+                        }
+
+                        if (var12.sideHit == 2)
+                        {
+                            --var15;
+                        }
+
+                        if (var12.sideHit == 3)
+                        {
+                            ++var15;
+                        }
+
+                        if (var12.sideHit == 4)
+                        {
+                            --var13;
+                        }
+
+                        if (var12.sideHit == 5)
+                        {
+                            ++var13;
+                        }
+
+                        if (!par3EntityPlayer.canPlayerEdit(var13, var14, var15))
+                        {
+                            return par1ItemStack;
+                        }
+
+                        if (this.func_77875_a(par2World, var5, var7, var9, var13, var14, var15) && !par3EntityPlayer.capabilities.isCreativeMode)
+                        {
+                            return new ItemStack(GCItems.reinforcedBucketEmpty);
+                        }
+                    }
+                }
+                else if (this.isFull == 0 && var12.entityHit instanceof EntityCow)
+                {
+                    return new ItemStack(GCItems.reinforcedBucketMilk);
+                }
+
+                return par1ItemStack;
+            }
+    	}
+    	else
+    	{
+            par3EntityPlayer.setItemInUse(par1ItemStack, this.getMaxItemUseDuration(par1ItemStack));
+            return par1ItemStack;
+    	}
+    }
+
+    public boolean func_77875_a(World par1World, double par2, double par4, double par6, int par8, int par9, int par10)
+    {
+        if (this.isFull <= 0)
+        {
+            return false;
+        }
+        else if (!par1World.isAirBlock(par8, par9, par10) && par1World.getBlockMaterial(par8, par9, par10).isSolid())
+        {
+            return false;
+        }
+        else
+        {
+            if (par1World.provider.isHellWorld && this.isFull == Block.waterMoving.blockID)
+            {
+                par1World.playSoundEffect(par2 + 0.5D, par4 + 0.5D, par6 + 0.5D, "random.fizz", 0.5F, 2.6F + (par1World.rand.nextFloat() - par1World.rand.nextFloat()) * 0.8F);
+
+                for (int var11 = 0; var11 < 8; ++var11)
+                {
+                    par1World.spawnParticle("largesmoke", (double)par8 + Math.random(), (double)par9 + Math.random(), (double)par10 + Math.random(), 0.0D, 0.0D, 0.0D);
+                }
+            }
+            else
+            {
+                par1World.setBlockAndMetadataWithNotify(par8, par9, par10, this.isFull, 0);
+            }
+
+            return true;
+        }
+    }
+}
--- src/common/micdoodle8/mods/galacticraft/GCItems.java
+++ src/common/micdoodle8/mods/galacticraft/GCItems.java
@@ -1,7 +1,9 @@
 package micdoodle8.mods.galacticraft;
 
+import net.minecraft.src.Block;
 import net.minecraft.src.EnumArmorMaterial;
 import net.minecraft.src.Item;
+import net.minecraft.src.ItemBucketMilk;
 import net.minecraftforge.common.EnumHelper;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
@@ -23,6 +25,11 @@ public class GCItems
 	public static Item spaceship;
 	public static Item sensorGlasses;
 	public static Item sensorGlassesWithOxygenMask;
+	public static Item reinforcedBucketEmpty;
+	public static Item reinforcedBucketMilk;
+	public static Item reinforcedBucketWater;
+	public static Item reinforcedBucketLava;
+	public static Item reinforcedBucketBacteria;
 	
 	static EnumArmorMaterial OXYGENMASK = EnumHelper.addArmorMaterial("OXYGENMASK", 200, new int[] {0, 0, 0, 0}, 0);
 	static EnumArmorMaterial SENSORGLASSES = EnumHelper.addArmorMaterial("SENSORGLASSES", 200, new int[] {0, 0, 0, 0}, 0);
@@ -39,6 +46,11 @@ public class GCItems
 		spaceship = new GCItemSpaceship(GCConfigManager.idItemSpaceship).setIconIndex(8).setItemName("spaceship");
 		sensorGlasses = new GCItemSensorGlasses(GCConfigManager.idItemSensorGlasses, SENSORGLASSES, 6, 0, false).setIconIndex(9).setItemName("sensorGlasses");
 		sensorGlassesWithOxygenMask = new GCItemSensorGlasses(GCConfigManager.idItemSensorGlassesWithOxygenMask, SENSORGLASSES, 7, 0, true).setIconIndex(10).setItemName("sensorGlassesWithOxygenMask");
+		reinforcedBucketEmpty = new GCItemReinforcedBucket(GCConfigManager.idItemReinforcedBucket, 0).setIconIndex(11).setItemName("reinfocedBucket");
+		reinforcedBucketMilk = new GCItemReinforcedBucket(GCConfigManager.idItemReinforcedBucketMilk, 1).setIconIndex(14).setItemName("reinfocedBucketMilk");
+		reinforcedBucketWater = new GCItemReinforcedBucket(GCConfigManager.idItemReinforcedBucketWater, Block.waterMoving.blockID).setIconIndex(12).setItemName("reinfocedBucketWater");
+		reinforcedBucketLava = new GCItemReinforcedBucket(GCConfigManager.idItemReinforcedBucketLava, Block.lavaMoving.blockID).setIconIndex(13).setItemName("reinfocedBucketLava");
+		reinforcedBucketBacteria = new GCItemReinforcedBucket(GCConfigManager.idItemReinforcedBucketBacteria, GCBlocks.bacterialSludgeMoving.blockID).setIconIndex(15).setItemName("reinfocedBucketSludge");
 	}
 
 	public static void addNames() 
@@ -53,5 +65,10 @@ public class GCItems
 		LanguageRegistry.instance().addNameForObject(heavyOxygenTankEmpty, "en_US", "Empty Heavy Oxygen Tank");
 		LanguageRegistry.instance().addNameForObject(sensorGlasses, "en_US", "Sensor Glasses");
 		LanguageRegistry.instance().addNameForObject(sensorGlassesWithOxygenMask, "en_US", "Sensor Glasses with Oxygen Mask");
+		LanguageRegistry.instance().addNameForObject(reinforcedBucketEmpty, "en_US", "Reinforced Bucket");
+		LanguageRegistry.instance().addNameForObject(reinforcedBucketMilk, "en_US", "Reinforced Milk Bucket");
+		LanguageRegistry.instance().addNameForObject(reinforcedBucketWater, "en_US", "Reinforced Water Bucket");
+		LanguageRegistry.instance().addNameForObject(reinforcedBucketLava, "en_US", "Reinforced Lava Bucket");
+		LanguageRegistry.instance().addNameForObject(reinforcedBucketBacteria, "en_US", "Reinforced Sludge Bucket");
 	}
 }
