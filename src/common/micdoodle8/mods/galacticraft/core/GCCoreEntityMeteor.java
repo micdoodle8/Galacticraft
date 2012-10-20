dbcc0a35597bfa2ef034503ea0dc74b0176f605d
Mitchell Petrie <mitchpetrie29@gmail.com>
2012-10-20 17:26:44 -0400

    Meteors!

---
+++ src/common/micdoodle8/mods/galacticraft/core/GCCoreEntityMeteor.java
@@ -0,0 +1,176 @@
+package micdoodle8.mods.galacticraft.core;
+
+import java.util.Iterator;
+import java.util.List;
+
+import net.minecraft.src.AxisAlignedBB;
+import net.minecraft.src.DamageSource;
+import net.minecraft.src.Entity;
+import net.minecraft.src.EntityDamageSourceIndirect;
+import net.minecraft.src.EntityLiving;
+import net.minecraft.src.EntityPlayer;
+import net.minecraft.src.MovingObjectPosition;
+import net.minecraft.src.NBTTagCompound;
+import net.minecraft.src.StatCollector;
+import net.minecraft.src.Vec3;
+import net.minecraft.src.World;
+import cpw.mods.fml.common.Side;
+import cpw.mods.fml.common.asm.SideOnly;
+
+public class GCCoreEntityMeteor extends Entity
+{
+    public EntityLiving shootingEntity;
+    public int size;
+    
+	public GCCoreEntityMeteor(World world)
+	{
+		super(world);
+	}
+
+    public GCCoreEntityMeteor(World world, double x, double y, double z, double motX, double motY, double motZ, int size)
+    {
+    	this(world);
+        this.size = size;
+        this.setSize(10.0F, 10.0F);
+        this.setLocationAndAngles(x, y, z, this.rotationYaw, this.rotationPitch);
+        this.setPosition(x, y, z);
+        this.motionX = motX;
+        this.motionY = motY;
+        this.motionZ = motZ;
+        this.setSize(size);
+    }
+
+    public void onUpdate()
+    {
+    	this.setRotation(this.rotationYaw + 2F, this.rotationPitch + 2F);
+        this.prevPosX = this.posX;
+        this.prevPosY = this.posY;
+        this.prevPosZ = this.posZ;
+        this.motionY -= 0.03999999910593033D;
+        this.moveEntity(this.motionX, this.motionY, this.motionZ);
+        this.motionY *= 0.9800000190734863D;
+        
+        if (this.worldObj.isRemote)
+        {
+        	spawnParticles();
+        }
+
+        Vec3 var15 = Vec3.getVec3Pool().getVecFromPool(this.posX, this.posY, this.posZ);
+        Vec3 var2 = Vec3.getVec3Pool().getVecFromPool(this.posX + this.motionX, this.posY + this.motionY, this.posZ + this.motionZ);
+        MovingObjectPosition var3 = this.worldObj.rayTraceBlocks(var15, var2);
+        var15 = Vec3.getVec3Pool().getVecFromPool(this.posX, this.posY, this.posZ);
+        var2 = Vec3.getVec3Pool().getVecFromPool(this.posX + this.motionX, this.posY + this.motionY, this.posZ + this.motionZ);
+
+        if (var3 != null)
+        {
+            var2 = Vec3.getVec3Pool().getVecFromPool(var3.hitVec.xCoord, var3.hitVec.yCoord, var3.hitVec.zCoord);
+        }
+
+        Entity var4 = null;
+        List var5 = this.worldObj.getEntitiesWithinAABBExcludingEntity(this, this.boundingBox.addCoord(this.motionX, this.motionY, this.motionZ).expand(1.0D, 1.0D, 1.0D));
+        double var6 = 0.0D;
+        Iterator var8 = var5.iterator();
+
+        while (var8.hasNext())
+        {
+            Entity var9 = (Entity)var8.next();
+
+            if (var9.canBeCollidedWith() && (!var9.isEntityEqual(this.shootingEntity)))
+            {
+                float var10 = 0.3F;
+                AxisAlignedBB var11 = var9.boundingBox.expand((double)var10, (double)var10, (double)var10);
+                MovingObjectPosition var12 = var11.calculateIntercept(var15, var2);
+
+                if (var12 != null)
+                {
+                    double var13 = var15.distanceTo(var12.hitVec);
+
+                    if (var13 < var6 || var6 == 0.0D)
+                    {
+                        var4 = var9;
+                        var6 = var13;
+                    }
+                }
+            }
+        }
+
+        if (var4 != null)
+        {
+            var3 = new MovingObjectPosition(var4);
+        }
+
+        if (var3 != null || this.motionY == 0)
+        {
+            this.onImpact(var3);
+        }
+        
+        if (this.posY <= -20 || this.posY >= 400)
+        {
+        	this.setDead();
+        }
+    }
+    
+    protected void spawnParticles()
+    {
+    	this.worldObj.spawnParticle("smoke", this.posX, this.posY, this.posZ, 0.0D, 0.0D, 0.0D);
+    	this.worldObj.spawnParticle("smoke", this.posX + 1D, this.posY, this.posZ, 0.0D, 0.0D, 0.0D);
+    	this.worldObj.spawnParticle("smoke", this.posX, this.posY, this.posZ + 1D, 0.0D, 0.0D, 0.0D);
+    	this.worldObj.spawnParticle("smoke", this.posX - 1D, this.posY, this.posZ, 0.0D, 0.0D, 0.0D);
+    	this.worldObj.spawnParticle("smoke", this.posX, this.posY, this.posZ - 1D, 0.0D, 0.0D, 0.0D);
+    }
+
+    protected void onImpact(MovingObjectPosition par1MovingObjectPosition)
+    {
+        if (!this.worldObj.isRemote)
+        {
+        	if (par1MovingObjectPosition != null)
+        	{
+                if (par1MovingObjectPosition.entityHit != null)
+                {
+                    par1MovingObjectPosition.entityHit.attackEntityFrom(this.causeMeteorDamage(this, this.shootingEntity), 6);
+                }
+        	}
+
+            this.worldObj.newExplosion((Entity)null, this.posX, this.posY, this.posZ, this.size / 2, true);
+            this.setDead();
+        }
+    }
+
+    public static DamageSource causeMeteorDamage(GCCoreEntityMeteor par0EntityMeteor, Entity par1Entity)
+    {
+    	if (par1Entity != null && par1Entity instanceof EntityPlayer)
+    	{
+            StatCollector.translateToLocalFormatted("death." + "meteor", ((EntityPlayer)par1Entity).username + " was hit by a meteor! That's gotta hurt!");
+    	}
+        return new EntityDamageSourceIndirect("meteor", par0EntityMeteor, par1Entity).setProjectile();
+    }
+
+	@Override
+	protected void entityInit() 
+	{
+        this.dataWatcher.addObject(16, (int) this.size);
+	}
+	
+    public int getSize()
+    {
+        return this.dataWatcher.getWatchableObjectInt(16);
+    }
+
+    /**
+     * Sets the state of creeper, -1 to idle and 1 to be 'in fuse'
+     */
+    public void setSize(int par1)
+    {
+        this.dataWatcher.updateObject(16, Integer.valueOf(par1));
+    }
+
+	@Override
+	protected void readEntityFromNBT(NBTTagCompound par1NBTTagCompound) 
+	{
+	}
+
+	@Override
+	protected void writeEntityToNBT(NBTTagCompound par1NBTTagCompound) 
+	{
+	}
+}
--- src/common/micdoodle8/mods/galacticraft/moon/GCMoonPlayerBaseServer.java
+++ src/common/micdoodle8/mods/galacticraft/moon/GCMoonPlayerBaseServer.java
@@ -1,5 +1,6 @@
 package micdoodle8.mods.galacticraft.moon;
 
+import micdoodle8.mods.galacticraft.core.GCCoreEntityMeteor;
 import net.minecraft.src.MathHelper;
 import net.minecraft.src.ServerPlayerAPI;
 import net.minecraft.src.ServerPlayerBase;
@@ -7,6 +8,8 @@ import cpw.mods.fml.common.FMLLog;
 
 public class GCMoonPlayerBaseServer extends ServerPlayerBase
 {
+	public int lastStep;
+	
 	public GCMoonPlayerBaseServer(ServerPlayerAPI var1) 
 	{
 		super(var1);
@@ -19,13 +22,56 @@ public class GCMoonPlayerBaseServer extends ServerPlayerBase
 		
 		if (this.player.worldObj != null && this.player.worldObj.provider instanceof GCMoonWorldProvider)
 		{
-//			FMLLog.info("d" + this.player.worldObj.getBlockId(MathHelper.floor_double(player.posX), MathHelper.floor_double(player.posY - 1), MathHelper.floor_double(player.posZ)) + " " + GCMoonBlocks.moonGrass.blockID);
 			if (this.player.worldObj.getBlockId(MathHelper.floor_double(player.posX), MathHelper.floor_double(player.posY - 1), MathHelper.floor_double(player.posZ)) == GCMoonBlocks.moonGrass.blockID)
 			{
-				if (this.player.worldObj.getBlockMetadata(MathHelper.floor_double(player.posX), MathHelper.floor_double(player.posY - 1), MathHelper.floor_double(player.posZ)) != 1)
+				if (this.player.worldObj.getBlockMetadata(MathHelper.floor_double(player.posX), MathHelper.floor_double(player.posY - 1), MathHelper.floor_double(player.posZ)) == 0)
 				{
-					this.player.worldObj.setBlockMetadataWithNotify(MathHelper.floor_double(player.posX), MathHelper.floor_double(player.posY - 1), MathHelper.floor_double(player.posZ), 1);
+					switch (this.lastStep)
+					{
+					case 1:
+						this.player.worldObj.setBlockMetadataWithNotify(MathHelper.floor_double(player.posX), MathHelper.floor_double(player.posY - 1), MathHelper.floor_double(player.posZ), 2);
+						this.lastStep = 2;
+						break;
+					case 2:
+						this.player.worldObj.setBlockMetadataWithNotify(MathHelper.floor_double(player.posX), MathHelper.floor_double(player.posY - 1), MathHelper.floor_double(player.posZ), 1);
+						this.lastStep = 1;
+						break;
+					default:
+						this.player.worldObj.setBlockMetadataWithNotify(MathHelper.floor_double(player.posX), MathHelper.floor_double(player.posY - 1), MathHelper.floor_double(player.posZ), 1);
+						this.lastStep = 1;
+						break;
+					}
 				}
+			}
+		}
+		
+		if (player.worldObj.provider instanceof GCMoonWorldProvider)
+		{
+			if (player.worldObj.rand.nextInt(20) == 0)
+			{
+				int x, y, z;
+				double motX, motZ;
+				x = player.worldObj.rand.nextInt(20) - 10;
+				y = player.worldObj.rand.nextInt(20) + 200;
+				z = player.worldObj.rand.nextInt(20) - 10;
+				motX = player.worldObj.rand.nextDouble() * 5;
+				motZ = player.worldObj.rand.nextDouble() * 5;
+				
+				GCCoreEntityMeteor meteor = new GCCoreEntityMeteor(player.worldObj, player.posX + x, y, player.posZ + z, motX - 2.5D, 0, motZ - 2.5D, 1);
+				player.worldObj.spawnEntityInWorld(meteor);
+			}
+			if (player.worldObj.rand.nextInt(100) == 0)
+			{
+				int x, y, z;
+				double motX, motZ;
+				x = player.worldObj.rand.nextInt(20) - 10;
+				y = player.worldObj.rand.nextInt(20) + 200;
+				z = player.worldObj.rand.nextInt(20) - 10;
+				motX = player.worldObj.rand.nextDouble() * 5;
+				motZ = player.worldObj.rand.nextDouble() * 5;
+				
+				GCCoreEntityMeteor meteor = new GCCoreEntityMeteor(player.worldObj, player.posX + x, y, player.posZ + z, motX - 2.5D, 0, motZ - 2.5D, 6);
+				player.worldObj.spawnEntityInWorld(meteor);
 			}
 		}
     }
