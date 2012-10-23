588e188154629a3a72beaea021f7a1406ddc691f
Mitchell Petrie <mitchpetrie29@gmail.com>
2012-10-23 19:21:38 -0400

    Entities will be burnt upon touching a meteor, also smoke effect and sound.

--- src/common/micdoodle8/mods/galacticraft/core/GCCoreBlockFallenMeteor.java
+++ src/common/micdoodle8/mods/galacticraft/core/GCCoreBlockFallenMeteor.java
@@ -1,7 +1,9 @@
 package micdoodle8.mods.galacticraft.core;
 
-import net.minecraft.src.CreativeTabs;
+import net.minecraft.src.Entity;
+import net.minecraft.src.EntityLiving;
 import net.minecraft.src.Material;
+import net.minecraft.src.World;
 
 public class GCCoreBlockFallenMeteor extends GCCoreBlock
 {
@@ -15,5 +17,32 @@ public class GCCoreBlockFallenMeteor extends GCCoreBlock
     public int getRenderType()
     {
         return GalacticraftCore.proxy.getGCMeteorRenderID();
+    }
+
+	@Override
+    public boolean isOpaqueCube()
+    {
+        return false;
+    }
+
+	@Override
+    public void onEntityCollidedWithBlock(World par1World, int par2, int par3, int par4, Entity par5Entity)
+    {
+		if (par5Entity instanceof EntityLiving)
+		{
+			EntityLiving livingEntity = (EntityLiving) par5Entity;
+			
+	        par1World.playSoundEffect((double)((float)par2 + 0.5F), (double)((float)par3 + 0.5F), (double)((float)par4 + 0.5F), "random.fizz", 0.5F, 2.6F + (par1World.rand.nextFloat() - par1World.rand.nextFloat()) * 0.8F);
+
+	        for (int var5 = 0; var5 < 8; ++var5)
+	        {
+	            par1World.spawnParticle("largesmoke", (double)par2 + Math.random(), (double)par3 + 0.2D + Math.random(), (double)par4 + Math.random(), 0.0D, 0.0D, 0.0D);
+	        }
+	        
+	        if (!livingEntity.isBurning())
+	        {
+		        livingEntity.setFire(5);
+	        }
+		}
     }
 }
