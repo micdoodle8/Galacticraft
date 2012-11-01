cb6292a703007f24d452613cedb0e57641fabdbe
micdoodle8 <mitchpetrie29@gmail.com>
2012-11-01 02:04:26 -0700

    Meteors now fall if above ground.

--- src/common/micdoodle8/mods/galacticraft/core/GCCoreBlockFallenMeteor.java
+++ src/common/micdoodle8/mods/galacticraft/core/GCCoreBlockFallenMeteor.java
@@ -1,6 +1,10 @@
 package micdoodle8.mods.galacticraft.core;
 
+import java.util.Random;
+
+import net.minecraft.src.Block;
 import net.minecraft.src.Entity;
+import net.minecraft.src.EntityFallingSand;
 import net.minecraft.src.EntityLiving;
 import net.minecraft.src.Material;
 import net.minecraft.src.World;
@@ -55,5 +59,65 @@ public class GCCoreBlockFallenMeteor extends GCCoreBlock
 
             livingEntity.knockBack(livingEntity, 1, var9 / 2, var7 / 2);
 		}
+    }
+
+	@Override
+    public void onBlockAdded(World par1World, int par2, int par3, int par4)
+    {
+        par1World.scheduleBlockUpdate(par2, par3, par4, this.blockID, this.tickRate());
+    }
+
+	@Override
+    public void onNeighborBlockChange(World par1World, int par2, int par3, int par4, int par5)
+    {
+        par1World.scheduleBlockUpdate(par2, par3, par4, this.blockID, this.tickRate());
+    }
+
+	@Override
+    public void updateTick(World par1World, int par2, int par3, int par4, Random par5Random)
+    {
+        if (!par1World.isRemote)
+        {
+            this.tryToFall(par1World, par2, par3, par4);
+        }
+    }
+	
+    private void tryToFall(World par1World, int par2, int par3, int par4)
+    {
+        if (canFallBelow(par1World, par2, par3 - 1, par4) && par3 >= 0)
+        {
+            byte var8 = 32;
+            
+            par1World.setBlockWithNotify(par2, par3, par4, 0);
+
+            while (canFallBelow(par1World, par2, par3 - 1, par4) && par3 > 0)
+            {
+                --par3;
+            }
+
+            if (par3 > 0)
+            {
+                par1World.setBlockWithNotify(par2, par3, par4, this.blockID);
+            }
+        }
+    }
+
+    public static boolean canFallBelow(World par0World, int par1, int par2, int par3)
+    {
+        int var4 = par0World.getBlockId(par1, par2, par3);
+
+        if (var4 == 0)
+        {
+            return true;
+        }
+        else if (var4 == Block.fire.blockID)
+        {
+            return true;
+        }
+        else
+        {
+            Material var5 = Block.blocksList[var4].blockMaterial;
+            return var5 == Material.water ? true : var5 == Material.lava;
+        }
     }
 }
