6fee6aaea2b0f4052998fe635d26204cb01f9dd0
micdoodle8 <mitchpetrie29@gmail.com>
2012-11-02 16:35:08 -0700

    Add names for Moon items.

--- src/common/micdoodle8/mods/galacticraft/moon/GCMoonItems.java
+++ src/common/micdoodle8/mods/galacticraft/moon/GCMoonItems.java
@@ -10,18 +10,23 @@ public class GCMoonItems
 {
 	public static Item cheeseCurd;
 	public static Item meteoricIronRaw;
+	public static Item meteoricIronIngot;
     public static Item cheeseBlock;
 	
 	public static void initItems() 
 	{
-		cheeseCurd = new GCMoonItem(GCMoonConfigManager.idItemCheeseCurd).setIconIndex(0).setItemName("cheeseCurd");
+		cheeseCurd = new GCMoonItemCheese(GCMoonConfigManager.idItemCheeseCurd, 1, 0.1F, false).setIconIndex(0).setItemName("cheeseCurd");
 		cheeseBlock = new GCMoonItemReed(GCMoonConfigManager.idItemBlockCheese, GCMoonBlocks.cheeseBlock).setMaxStackSize(1).setIconIndex(1).setItemName("cheeseBlock");
-		meteoricIronRaw = new GCMoonItem(GCMoonConfigManager.idItemMeteoricIronRaw).setIconIndex(2).setItemName("meteoricIron");
+		meteoricIronRaw = new GCMoonItem(GCMoonConfigManager.idItemMeteoricIronRaw).setIconIndex(2).setItemName("meteoricIronRaw");
+		meteoricIronIngot = new GCMoonItem(GCMoonConfigManager.idItemMeteoricIronIngot).setIconIndex(3).setItemName("meteoricIronIngot");
 	}
 	
 	public static void addNames() 
 	{
-		
+		addName(cheeseCurd);
+		addName(cheeseBlock);
+		addName(meteoricIronRaw);
+		addName(meteoricIronIngot);
 	}
 
 	private static void addName(Item item)
