601a2610ec5d4fd515459fc8168d374118a7c463
micdoodle8 <mitchpetrie29@gmail.com>
2012-11-02 16:38:04 -0700

    Fix NPE and added double usage for reinforced buckets.

--- src/common/micdoodle8/mods/galacticraft/mars/GCMarsItemReinforcedBucket.java
+++ src/common/micdoodle8/mods/galacticraft/mars/GCMarsItemReinforcedBucket.java
@@ -6,13 +6,16 @@ import net.minecraft.src.EntityCow;
 import net.minecraft.src.EntityPlayer;
 import net.minecraft.src.EnumAction;
 import net.minecraft.src.EnumMovingObjectType;
+import net.minecraft.src.Item;
 import net.minecraft.src.ItemStack;
 import net.minecraft.src.Material;
 import net.minecraft.src.MovingObjectPosition;
+import net.minecraft.src.NBTTagCompound;
 import net.minecraft.src.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.Event;
 import net.minecraftforge.event.entity.player.FillBucketEvent;
+import cpw.mods.fml.common.FMLLog;
 
 public class GCMarsItemReinforcedBucket extends GCMarsItem
 {
@@ -29,13 +32,20 @@ public class GCMarsItemReinforcedBucket extends GCMarsItem
     @Override
     public ItemStack onFoodEaten(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
     {
+    	
     	if (this.shiftedIndex != GCMarsItems.reinforcedBucketMilk.shiftedIndex)
     	{
     		super.onFoodEaten(par1ItemStack, par2World, par3EntityPlayer);
     	}
     	else
     	{
-            if (!par3EntityPlayer.capabilities.isCreativeMode)
+            if (!par3EntityPlayer.capabilities.isCreativeMode && par1ItemStack.getTagCompound().getInteger("bucketUses") > 1)
+            {
+            	FMLLog.info("" + par1ItemStack.getTagCompound().getInteger("bucketUses"));
+            	par1ItemStack.getTagCompound().setInteger("bucketUses", par1ItemStack.getTagCompound().getInteger("bucketUses") - 1);
+            	return par1ItemStack;
+            }
+            else if (!par3EntityPlayer.capabilities.isCreativeMode && par1ItemStack.getTagCompound().getInteger("bucketUses") == 1)
             {
                 --par1ItemStack.stackSize;
             }
@@ -78,47 +88,32 @@ public class GCMarsItemReinforcedBucket extends GCMarsItem
     @Override
     public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
     {
+		if (par1ItemStack.getTagCompound() == null)
+		{
+			par1ItemStack.setTagCompound(new NBTTagCompound());
+		}
+		
+		if (par1ItemStack != null && par1ItemStack.getTagCompound().getInteger("bucketUses") != 1 && par1ItemStack.getTagCompound().getInteger("bucketUses") != 2 && par1ItemStack.getTagCompound().getInteger("bucketUses") != 3)
+		{
+			par1ItemStack.getTagCompound().setInteger("bucketUses", 2);
+		}
+		
     	if (this.shiftedIndex != GCMarsItems.reinforcedBucketMilk.shiftedIndex)
     	{
     		float var4 = 1.0F;
-            double var5 = par3EntityPlayer.prevPosX + (par3EntityPlayer.posX - par3EntityPlayer.prevPosX) * var4;
-            double var7 = par3EntityPlayer.prevPosY + (par3EntityPlayer.posY - par3EntityPlayer.prevPosY) * var4 + 1.62D - par3EntityPlayer.yOffset;
-            double var9 = par3EntityPlayer.prevPosZ + (par3EntityPlayer.posZ - par3EntityPlayer.prevPosZ) * var4;
+            double var5 = par3EntityPlayer.prevPosX + (par3EntityPlayer.posX - par3EntityPlayer.prevPosX) * (double)var4;
+            double var7 = par3EntityPlayer.prevPosY + (par3EntityPlayer.posY - par3EntityPlayer.prevPosY) * (double)var4 + 1.62D - (double)par3EntityPlayer.yOffset;
+            double var9 = par3EntityPlayer.prevPosZ + (par3EntityPlayer.posZ - par3EntityPlayer.prevPosZ) * (double)var4;
             boolean var11 = this.isFull == 0;
             MovingObjectPosition var12 = this.getMovingObjectPositionFromPlayer(par2World, par3EntityPlayer, var11);
 
             if (var12 == null)
             {
-                return par1ItemStack;
+            	if (par1ItemStack != null)
+                	return par1ItemStack;
             }
             else
             {
-                FillBucketEvent event = new FillBucketEvent(par3EntityPlayer, par1ItemStack, par2World, var12);
-                if (MinecraftForge.EVENT_BUS.post(event))
-                {
-                    return par1ItemStack;
-                }
-
-                if (event.getResult() == Event.Result.ALLOW)
-                {
-                    if (par3EntityPlayer.capabilities.isCreativeMode)
-                    {
-                        return par1ItemStack;
-                    }
-
-                    if (--par1ItemStack.stackSize <= 0)
-                    {
-                        return event.result;
-                    }
-
-                    if (!par3EntityPlayer.inventory.addItemStackToInventory(event.result))
-                    {
-                        par3EntityPlayer.dropPlayerItem(event.result);
-                    }
-
-                    return par1ItemStack;
-                }
-
                 if (var12.typeOfHit == EnumMovingObjectType.TILE)
                 {
                     int var13 = var12.blockX;
@@ -127,14 +122,16 @@ public class GCMarsItemReinforcedBucket extends GCMarsItem
 
                     if (!par2World.canMineBlock(par3EntityPlayer, var13, var14, var15))
                     {
-                        return par1ItemStack;
+                    	if (par1ItemStack != null)
+                        	return par1ItemStack;
                     }
 
                     if (this.isFull == 0)
                     {
                         if (!par3EntityPlayer.func_82247_a(var13, var14, var15, var12.sideHit, par1ItemStack))
                         {
-                            return par1ItemStack;
+                        	if (par1ItemStack != null)
+                            	return par1ItemStack;
                         }
 
                         if (par2World.getBlockMaterial(var13, var14, var15) == Material.water && par2World.getBlockMetadata(var13, var14, var15) == 0)
@@ -143,20 +140,28 @@ public class GCMarsItemReinforcedBucket extends GCMarsItem
 
                             if (par3EntityPlayer.capabilities.isCreativeMode)
                             {
-                                return par1ItemStack;
+                            	if (par1ItemStack != null)
+                                	return par1ItemStack;
                             }
 
                             if (--par1ItemStack.stackSize <= 0)
                             {
-                                return new ItemStack(GCMarsItems.reinforcedBucketWater);
+                            	ItemStack bucket = new ItemStack(GCMarsItems.reinforcedBucketWater);
+                            	bucket.setTagCompound(new NBTTagCompound());
+                            	bucket.getTagCompound().setInteger("bucketUses", 2);
+                                return bucket;
                             }
 
-                            if (!par3EntityPlayer.inventory.addItemStackToInventory(new ItemStack(GCMarsItems.reinforcedBucketWater)))
+                            if (!par3EntityPlayer.inventory.addItemStackToInventory(new ItemStack(Item.bucketWater)))
                             {
-                                par3EntityPlayer.dropPlayerItem(new ItemStack(GCMarsItems.reinforcedBucketWater.shiftedIndex, 1, 0));
+                            	ItemStack bucket = new ItemStack(GCMarsItems.reinforcedBucketWater);
+                            	bucket.setTagCompound(new NBTTagCompound());
+                            	bucket.getTagCompound().setInteger("bucketUses", 2);
+                                par3EntityPlayer.dropPlayerItem(bucket);
                             }
 
-                            return par1ItemStack;
+                            if (par1ItemStack != null)
+                            	return par1ItemStack;
                         }
 
                         if (par2World.getBlockMaterial(var13, var14, var15) == Material.lava && par2World.getBlockMetadata(var13, var14, var15) == 0)
@@ -165,20 +170,28 @@ public class GCMarsItemReinforcedBucket extends GCMarsItem
 
                             if (par3EntityPlayer.capabilities.isCreativeMode)
                             {
-                                return par1ItemStack;
+                            	if (par1ItemStack != null)
+                                	return par1ItemStack;
                             }
 
                             if (--par1ItemStack.stackSize <= 0)
                             {
-                                return new ItemStack(GCMarsItems.reinforcedBucketLava);
+                            	ItemStack bucket = new ItemStack(GCMarsItems.reinforcedBucketLava);
+                            	bucket.setTagCompound(new NBTTagCompound());
+                            	bucket.getTagCompound().setInteger("bucketUses", 2);
+                                return bucket;
                             }
 
-                            if (!par3EntityPlayer.inventory.addItemStackToInventory(new ItemStack(GCMarsItems.reinforcedBucketLava)))
+                            if (!par3EntityPlayer.inventory.addItemStackToInventory(new ItemStack(Item.bucketLava)))
                             {
-                                par3EntityPlayer.dropPlayerItem(new ItemStack(GCMarsItems.reinforcedBucketLava.shiftedIndex, 1, 0));
+                            	ItemStack bucket = new ItemStack(GCMarsItems.reinforcedBucketLava);
+                            	bucket.setTagCompound(new NBTTagCompound());
+                            	bucket.getTagCompound().setInteger("bucketUses", 2);
+                                par3EntityPlayer.dropPlayerItem(bucket);
                             }
 
-                            return par1ItemStack;
+                            if (par1ItemStack != null)
+                            	return par1ItemStack;
                         }
 
                         if (par2World.getBlockMaterial(var13, var14, var15) == GCMarsBlocks.bacterialSludge && par2World.getBlockMetadata(var13, var14, var15) == 0)
@@ -187,21 +200,33 @@ public class GCMarsItemReinforcedBucket extends GCMarsItem
 
                             if (par3EntityPlayer.capabilities.isCreativeMode)
                             {
-                                return par1ItemStack;
+                            	if (par1ItemStack != null)
+                                	return par1ItemStack;
                             }
 
                             if (--par1ItemStack.stackSize <= 0)
                             {
-                                return new ItemStack(GCMarsItems.reinforcedBucketBacteria);
+                            	ItemStack bucket = new ItemStack(GCMarsItems.reinforcedBucketBacteria);
+                            	bucket.setTagCompound(new NBTTagCompound());
+                            	bucket.getTagCompound().setInteger("bucketUses", 2);
+                                return bucket;
                             }
 
                             if (!par3EntityPlayer.inventory.addItemStackToInventory(new ItemStack(GCMarsItems.reinforcedBucketBacteria)))
                             {
-                                par3EntityPlayer.dropPlayerItem(new ItemStack(GCMarsItems.reinforcedBucketBacteria.shiftedIndex, 1, 0));
+                            	ItemStack bucket = new ItemStack(GCMarsItems.reinforcedBucketBacteria);
+                            	bucket.setTagCompound(new NBTTagCompound());
+                            	bucket.getTagCompound().setInteger("bucketUses", 2);
+                                par3EntityPlayer.dropPlayerItem(bucket);
                             }
 
-                            return par1ItemStack;
+                            if (par1ItemStack != null)
+                            	return par1ItemStack;
                         }
+                        
+                        if (par1ItemStack != null)
+                        	if (par1ItemStack != null)
+                            	return par1ItemStack;
                     }
                     else
                     {
@@ -245,9 +270,13 @@ public class GCMarsItemReinforcedBucket extends GCMarsItem
                             return par1ItemStack;
                         }
 
-                        if (this.func_77875_a(par2World, var5, var7, var9, var13, var14, var15) && !par3EntityPlayer.capabilities.isCreativeMode)
+                        if (this.tryPlaceContainedLiquid(par2World, var5, var7, var9, var13, var14, var15) && !par3EntityPlayer.capabilities.isCreativeMode && par1ItemStack.getTagCompound() != null && par1ItemStack.getTagCompound().getInteger("bucketUses") == 1)
                         {
                             return new ItemStack(GCMarsItems.reinforcedBucketEmpty);
+                        }
+                        else if (this.tryPlaceContainedLiquid(par2World, var5, var7, var9, var13, var14, var15) && !par3EntityPlayer.capabilities.isCreativeMode && par1ItemStack.getTagCompound().getInteger("bucketUses") > 0)
+                        {
+                        	par1ItemStack.getTagCompound().setInteger("bucketUses", par1ItemStack.getTagCompound().getInteger("bucketUses") - 1);
                         }
                     }
                 }
@@ -256,19 +285,21 @@ public class GCMarsItemReinforcedBucket extends GCMarsItem
                     return new ItemStack(GCMarsItems.reinforcedBucketMilk);
                 }
 
-                return par1ItemStack;
+                if (par1ItemStack != null)
+                	return par1ItemStack;
             }
     	}
     	else
     	{
             par3EntityPlayer.setItemInUse(par1ItemStack, this.getMaxItemUseDuration(par1ItemStack));
-            return par1ItemStack;
     	}
+    	
+		return par1ItemStack;
     }
-
-    public boolean func_77875_a(World par1World, double par2, double par4, double par6, int par8, int par9, int par10)
+    
+    public boolean tryPlaceContainedLiquid(World par1World, double par2, double par4, double par6, int par8, int par9, int par10)
     {
-        if (this.isFull <= 0)
+    	if (this.isFull <= 0)
         {
             return false;
         }
@@ -278,13 +309,13 @@ public class GCMarsItemReinforcedBucket extends GCMarsItem
         }
         else
         {
-            if (par1World.provider.isHellWorld && this.isFull == Block.waterMoving.blockID)
+            if (par1World.provider.isHellWorld && this.isFull == GCMarsItems.reinforcedBucketWater.shiftedIndex)
             {
                 par1World.playSoundEffect(par2 + 0.5D, par4 + 0.5D, par6 + 0.5D, "random.fizz", 0.5F, 2.6F + (par1World.rand.nextFloat() - par1World.rand.nextFloat()) * 0.8F);
 
                 for (int var11 = 0; var11 < 8; ++var11)
                 {
-                    par1World.spawnParticle("largesmoke", par8 + Math.random(), par9 + Math.random(), par10 + Math.random(), 0.0D, 0.0D, 0.0D);
+                    par1World.spawnParticle("largesmoke", (double)par8 + Math.random(), (double)par9 + Math.random(), (double)par10 + Math.random(), 0.0D, 0.0D, 0.0D);
                 }
             }
             else
