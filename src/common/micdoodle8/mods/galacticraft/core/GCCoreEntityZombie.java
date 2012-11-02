2aaa72f60fd2d7a30ad7dddd2938b9a82d53b0c0
micdoodle8 <mitchpetrie29@gmail.com>
2012-11-02 16:59:05 -0700

    Updated zombie to 1.4 code.

--- src/common/micdoodle8/mods/galacticraft/core/GCCoreEntityZombie.java
+++ src/common/micdoodle8/mods/galacticraft/core/GCCoreEntityZombie.java
@@ -1,5 +1,9 @@
 package micdoodle8.mods.galacticraft.core;
 
+import java.util.Calendar;
+
+import net.minecraft.src.Block;
+import net.minecraft.src.Entity;
 import net.minecraft.src.EntityAIAttackOnCollide;
 import net.minecraft.src.EntityAIBreakDoor;
 import net.minecraft.src.EntityAIHurtByTarget;
@@ -10,21 +14,24 @@ import net.minecraft.src.EntityAINearestAttackableTarget;
 import net.minecraft.src.EntityAISwimming;
 import net.minecraft.src.EntityAIWander;
 import net.minecraft.src.EntityAIWatchClosest;
+import net.minecraft.src.EntityLiving;
 import net.minecraft.src.EntityPlayer;
 import net.minecraft.src.EntityVillager;
 import net.minecraft.src.EnumCreatureAttribute;
 import net.minecraft.src.Item;
+import net.minecraft.src.ItemStack;
 import net.minecraft.src.MathHelper;
+import net.minecraft.src.NBTTagCompound;
+import net.minecraft.src.Potion;
+import net.minecraft.src.PotionEffect;
 import net.minecraft.src.World;
+import cpw.mods.fml.common.Side;
+import cpw.mods.fml.common.asm.SideOnly;
 
-/**
- * Copyright 2012, micdoodle8
- * 
- *  All rights reserved.
- *
- */
 public class GCCoreEntityZombie extends GCCoreEntityMob
 {
+    private int field_82234_d = 0;
+
     public GCCoreEntityZombie(World par1World)
     {
         super(par1World);
@@ -45,8 +52,34 @@ public class GCCoreEntityZombie extends GCCoreEntityMob
         this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityVillager.class, 16.0F, 0, false));
     }
 
-    @Override
-	public int getMaxHealth()
+    /**
+     * This method returns a value to be applied directly to entity speed, this factor is less than 1 when a slowdown
+     * potion effect is applied, more than 1 when a haste potion effect is applied and 2 for fleeing entities.
+     */
+    public float getSpeedModifier()
+    {
+        return super.getSpeedModifier() * (this.isChild() ? 1.5F : 1.0F);
+    }
+
+    protected void entityInit()
+    {
+        super.entityInit();
+        this.getDataWatcher().addObject(12, Byte.valueOf((byte)0));
+        this.getDataWatcher().addObject(13, Byte.valueOf((byte)0));
+        this.getDataWatcher().addObject(14, Byte.valueOf((byte)0));
+    }
+
+    @SideOnly(Side.CLIENT)
+
+    /**
+     * Returns the texture's file path as a String.
+     */
+    public String getTexture()
+    {
+        return "/micdoodle8/mods/galacticraft/core/client/entities/zombie.png";
+    }
+
+    public int getMaxHealth()
     {
         return 20;
     }
@@ -54,75 +87,158 @@ public class GCCoreEntityZombie extends GCCoreEntityMob
     /**
      * Returns the current armor value as determined by a call to InventoryPlayer.getTotalArmorValue
      */
-    @Override
-	public int getTotalArmorValue()
+    public int getTotalArmorValue()
     {
-        return 2;
+        int var1 = super.getTotalArmorValue() + 2;
+
+        if (var1 > 20)
+        {
+            var1 = 20;
+        }
+
+        return var1;
     }
 
     /**
      * Returns true if the newer Entity AI code should be run
      */
-    @Override
-	protected boolean isAIEnabled()
+    protected boolean isAIEnabled()
     {
         return true;
+    }
+
+    /**
+     * If Animal, checks if the age timer is negative
+     */
+    public boolean isChild()
+    {
+        return this.getDataWatcher().getWatchableObjectByte(12) == 1;
+    }
+
+    public void func_82227_f(boolean par1)
+    {
+        this.getDataWatcher().updateObject(12, Byte.valueOf((byte)1));
+    }
+
+    public boolean func_82231_m()
+    {
+        return this.getDataWatcher().getWatchableObjectByte(13) == 1;
+    }
+
+    public void func_82229_g(boolean par1)
+    {
+        this.getDataWatcher().updateObject(13, Byte.valueOf((byte)(par1 ? 1 : 0)));
     }
 
     /**
      * Called frequently so the entity can update its state every tick as required. For example, zombies and skeletons
      * use this to react to sunlight and start to burn.
      */
-    @Override
-	public void onLivingUpdate()
+    public void onLivingUpdate()
     {
-        if (this.worldObj.isDaytime() && !this.worldObj.isRemote)
+        if (this.worldObj.isDaytime() && !this.worldObj.isRemote && !this.isChild())
         {
             float var1 = this.getBrightness(1.0F);
 
-            if (var1 > 0.5F && this.worldObj.canBlockSeeTheSky(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.posY), MathHelper.floor_double(this.posZ)) && this.rand.nextFloat() * 30.0F < (var1 - 0.4F) * 2.0F)
+            if (var1 > 0.5F && this.rand.nextFloat() * 30.0F < (var1 - 0.4F) * 2.0F && this.worldObj.canBlockSeeTheSky(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.posY), MathHelper.floor_double(this.posZ)))
             {
-                this.setFire(8);
+                boolean var2 = true;
+                ItemStack var3 = this.getCurrentItemOrArmor(4);
+
+                if (var3 != null)
+                {
+                    if (var3.isItemStackDamageable())
+                    {
+                        var3.setItemDamage(var3.getItemDamageForDisplay() + this.rand.nextInt(2));
+
+                        if (var3.getItemDamageForDisplay() >= var3.getMaxDamage())
+                        {
+                            this.renderBrokenItemStack(var3);
+                            this.func_70062_b(4, (ItemStack)null);
+                        }
+                    }
+
+                    var2 = false;
+                }
+
+                if (var2)
+                {
+                    this.setFire(8);
+                }
             }
         }
-        
-        this.motionY += 0.06F;
 
         super.onLivingUpdate();
     }
 
     /**
+     * Called to update the entity's position/logic.
+     */
+    public void onUpdate()
+    {
+        if (!this.worldObj.isRemote && this.func_82230_o())
+        {
+            int var1 = this.func_82233_q();
+            this.field_82234_d -= var1;
+
+            if (this.field_82234_d <= 0)
+            {
+                this.func_82232_p();
+            }
+        }
+
+        super.onUpdate();
+    }
+
+    public int func_82193_c(Entity par1Entity)
+    {
+        ItemStack var2 = this.getHeldItem();
+        int var3 = 4;
+
+        if (var2 != null)
+        {
+            var3 += var2.getDamageVsEntity(this);
+        }
+
+        return var3;
+    }
+
+    /**
      * Returns the sound this mob makes while it's alive.
      */
-    @Override
-	protected String getLivingSound()
+    protected String getLivingSound()
     {
-        return "mob.zombie";
+        return "mob.zombie.say";
     }
 
     /**
      * Returns the sound this mob makes when it is hurt.
      */
-    @Override
-	protected String getHurtSound()
+    protected String getHurtSound()
     {
-        return "mob.zombiehurt";
+        return "mob.zombie.hurt";
     }
 
     /**
      * Returns the sound this mob makes on death.
      */
-    @Override
-	protected String getDeathSound()
+    protected String getDeathSound()
     {
-        return "mob.zombiedeath";
+        return "mob.zombie.death";
+    }
+
+    /**
+     * Plays step sound at given x, y, z for the entity
+     */
+    protected void playStepSound(int par1, int par2, int par3, int par4)
+    {
+        this.worldObj.playSoundAtEntity(this, "mob.zombie.step", 0.15F, 1.0F);
     }
 
     /**
      * Returns the item ID for the item the mob drops on death.
      */
-    @Override
-	protected int getDropItemId()
+    protected int getDropItemId()
     {
         return Item.rottenFlesh.shiftedIndex;
     }
@@ -130,28 +246,249 @@ public class GCCoreEntityZombie extends GCCoreEntityMob
     /**
      * Get this Entity's EnumCreatureAttribute
      */
-    @Override
-	public EnumCreatureAttribute getCreatureAttribute()
+    public EnumCreatureAttribute getCreatureAttribute()
     {
         return EnumCreatureAttribute.UNDEAD;
     }
 
-    @Override
-	protected void dropRareDrop(int par1)
+    protected void dropRareDrop(int par1)
     {
-        switch (this.rand.nextInt(4))
+        switch (this.rand.nextInt(3))
         {
             case 0:
-                this.dropItem(Item.swordSteel.shiftedIndex, 1);
-                break;
-            case 1:
-                this.dropItem(Item.helmetSteel.shiftedIndex, 1);
-                break;
-            case 2:
                 this.dropItem(Item.ingotIron.shiftedIndex, 1);
                 break;
-            case 3:
-                this.dropItem(Item.shovelSteel.shiftedIndex, 1);
+            case 1:
+                this.dropItem(Item.field_82797_bK.shiftedIndex, 1);
+                break;
+            case 2:
+                this.dropItem(Item.field_82794_bL.shiftedIndex, 1);
         }
+    }
+
+    protected void func_82164_bB()
+    {
+        super.func_82164_bB();
+
+        if (this.rand.nextFloat() < (this.worldObj.difficultySetting == 3 ? 0.05F : 0.01F))
+        {
+            int var1 = this.rand.nextInt(3);
+
+            if (var1 == 0)
+            {
+                this.func_70062_b(0, new ItemStack(Item.swordSteel));
+            }
+            else
+            {
+                this.func_70062_b(0, new ItemStack(Item.shovelSteel));
+            }
+        }
+    }
+
+    /**
+     * (abstract) Protected helper method to write subclass entity data to NBT.
+     */
+    public void writeEntityToNBT(NBTTagCompound par1NBTTagCompound)
+    {
+        super.writeEntityToNBT(par1NBTTagCompound);
+
+        if (this.isChild())
+        {
+            par1NBTTagCompound.setBoolean("IsBaby", true);
+        }
+
+        if (this.func_82231_m())
+        {
+            par1NBTTagCompound.setBoolean("IsVillager", true);
+        }
+
+        par1NBTTagCompound.setInteger("ConversionTime", this.func_82230_o() ? this.field_82234_d : -1);
+    }
+
+    /**
+     * (abstract) Protected helper method to read subclass entity data from NBT.
+     */
+    public void readEntityFromNBT(NBTTagCompound par1NBTTagCompound)
+    {
+        super.readEntityFromNBT(par1NBTTagCompound);
+
+        if (par1NBTTagCompound.getBoolean("IsBaby"))
+        {
+            this.func_82227_f(true);
+        }
+
+        if (par1NBTTagCompound.getBoolean("IsVillager"))
+        {
+            this.func_82229_g(true);
+        }
+
+        if (par1NBTTagCompound.hasKey("ConversionTime") && par1NBTTagCompound.getInteger("ConversionTime") > -1)
+        {
+            this.func_82228_a(par1NBTTagCompound.getInteger("ConversionTime"));
+        }
+    }
+
+    /**
+     * This method gets called when the entity kills another one.
+     */
+    public void onKillEntity(EntityLiving par1EntityLiving)
+    {
+        super.onKillEntity(par1EntityLiving);
+
+        if (this.worldObj.difficultySetting >= 2 && par1EntityLiving instanceof EntityVillager)
+        {
+            if (this.worldObj.difficultySetting == 2 && this.rand.nextBoolean())
+            {
+                return;
+            }
+
+            GCCoreEntityZombie var2 = new GCCoreEntityZombie(this.worldObj);
+            var2.func_82149_j(par1EntityLiving);
+            this.worldObj.setEntityDead(par1EntityLiving);
+            var2.func_82163_bD();
+            var2.func_82229_g(true);
+
+            if (par1EntityLiving.isChild())
+            {
+                var2.func_82227_f(true);
+            }
+
+            this.worldObj.spawnEntityInWorld(var2);
+            this.worldObj.playAuxSFXAtEntity((EntityPlayer)null, 1016, (int)this.posX, (int)this.posY, (int)this.posZ, 0);
+        }
+    }
+
+    public void func_82163_bD()
+    {
+        this.field_82172_bs = this.rand.nextFloat() < field_82181_as[this.worldObj.difficultySetting];
+
+        if (this.worldObj.rand.nextFloat() < 0.05F)
+        {
+            this.func_82229_g(true);
+        }
+
+        this.func_82164_bB();
+        this.func_82162_bC();
+
+        if (this.getCurrentItemOrArmor(4) == null)
+        {
+            Calendar var1 = this.worldObj.func_83015_S();
+
+            if (var1.get(2) + 1 == 10 && var1.get(5) == 31 && this.rand.nextFloat() < 0.25F)
+            {
+                this.func_70062_b(4, new ItemStack(this.rand.nextFloat() < 0.1F ? Block.pumpkinLantern : Block.pumpkin));
+                this.field_82174_bp[4] = 0.0F;
+            }
+        }
+    }
+
+    /**
+     * Called when a player interacts with a mob. e.g. gets milk from a cow, gets into the saddle on a pig.
+     */
+    public boolean interact(EntityPlayer par1EntityPlayer)
+    {
+        ItemStack var2 = par1EntityPlayer.getCurrentEquippedItem();
+
+        if (var2 != null && var2.getItem() == Item.appleGold && var2.getItemDamage() == 0 && this.func_82231_m() && this.isPotionActive(Potion.weakness))
+        {
+            if (!par1EntityPlayer.capabilities.isCreativeMode)
+            {
+                --var2.stackSize;
+            }
+
+            if (var2.stackSize <= 0)
+            {
+                par1EntityPlayer.inventory.setInventorySlotContents(par1EntityPlayer.inventory.currentItem, (ItemStack)null);
+            }
+
+            if (!this.worldObj.isRemote)
+            {
+                this.func_82228_a(this.rand.nextInt(2401) + 3600);
+            }
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    protected void func_82228_a(int par1)
+    {
+        this.field_82234_d = par1;
+        this.getDataWatcher().updateObject(14, Byte.valueOf((byte)1));
+        this.func_82170_o(Potion.weakness.id);
+        this.addPotionEffect(new PotionEffect(Potion.damageBoost.id, par1, Math.min(this.worldObj.difficultySetting - 1, 0)));
+        this.worldObj.setEntityState(this, (byte)16);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void handleHealthUpdate(byte par1)
+    {
+        if (par1 == 16)
+        {
+            this.worldObj.playSound(this.posX + 0.5D, this.posY + 0.5D, this.posZ + 0.5D, "mob.zombie.remedy", 1.0F + this.rand.nextFloat(), this.rand.nextFloat() * 0.7F + 0.3F);
+        }
+        else
+        {
+            super.handleHealthUpdate(par1);
+        }
+    }
+
+    public boolean func_82230_o()
+    {
+        return this.getDataWatcher().getWatchableObjectByte(14) == 1;
+    }
+
+    protected void func_82232_p()
+    {
+        EntityVillager var1 = new EntityVillager(this.worldObj);
+        var1.func_82149_j(this);
+        var1.func_82163_bD();
+        var1.func_82187_q();
+
+        if (this.isChild())
+        {
+            var1.setGrowingAge(-24000);
+        }
+
+        this.worldObj.setEntityDead(this);
+        this.worldObj.spawnEntityInWorld(var1);
+        var1.addPotionEffect(new PotionEffect(Potion.confusion.id, 200, 0));
+        this.worldObj.playAuxSFXAtEntity((EntityPlayer)null, 1017, (int)this.posX, (int)this.posY, (int)this.posZ, 0);
+    }
+
+    protected int func_82233_q()
+    {
+        int var1 = 1;
+
+        if (this.rand.nextFloat() < 0.01F)
+        {
+            int var2 = 0;
+
+            for (int var3 = (int)this.posX - 4; var3 < (int)this.posX + 4 && var2 < 14; ++var3)
+            {
+                for (int var4 = (int)this.posY - 4; var4 < (int)this.posY + 4 && var2 < 14; ++var4)
+                {
+                    for (int var5 = (int)this.posZ - 4; var5 < (int)this.posZ + 4 && var2 < 14; ++var5)
+                    {
+                        int var6 = this.worldObj.getBlockId(var3, var4, var5);
+
+                        if (var6 == Block.fenceIron.blockID || var6 == Block.bed.blockID)
+                        {
+                            if (this.rand.nextFloat() < 0.3F)
+                            {
+                                ++var1;
+                            }
+
+                            ++var2;
+                        }
+                    }
+                }
+            }
+        }
+
+        return var1;
     }
 }
