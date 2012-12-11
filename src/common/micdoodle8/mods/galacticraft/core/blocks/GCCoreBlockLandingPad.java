2d99ff5cbed0bb557a301a22c19e329bad2a4a4c
micdoodle8 <mitchpetrie29@gmail.com>
2012-12-11 02:22:09 -0500

    Don't allow placing of landing pads on top of each other or in slabs greater than 3x3.

--- src/common/micdoodle8/mods/galacticraft/core/blocks/GCCoreBlockLandingPad.java
+++ src/common/micdoodle8/mods/galacticraft/core/blocks/GCCoreBlockLandingPad.java
@@ -1,18 +1,118 @@
 package micdoodle8.mods.galacticraft.core.blocks;
 
+import net.minecraft.src.Block;
+import net.minecraft.src.Material;
+import net.minecraft.src.World;
+
 /**
  * Copyright 2012, micdoodle8
  * 
  *  All rights reserved.
  *
  */
-public class GCCoreBlockLandingPad extends GCCoreBlock
+public class GCCoreBlockLandingPad extends Block
 {
 	public GCCoreBlockLandingPad(int i, int j) 
 	{
-		super(i, j);
+		super(i, j, Material.iron);
         this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.2F, 1.0F);
 	}
+
+//    @Override
+//    public void onBlockAdded(World par1World, int par2, int par3, int par4) 
+//    {
+//    	int amountOfCorrectBlocks = 0;
+//    	
+//    	for (int i = -2; i < 3; i++)
+//		{
+//			for (int j = -2; j < 3; j++)
+//    		{
+//				if (par1World.getBlockId(par2 + i, par3, par4 + j) == GCCoreBlocks.landingPad.blockID)
+//				{
+//					amountOfCorrectBlocks++;
+//				}
+//    		}
+//		}
+//    	
+//    	if (amountOfCorrectBlocks == 9)
+//    	{
+//			int id;
+//			Block block;
+//			
+//			id = par1World.getBlockId(par2 + 1, par3, par4);
+//			
+//			block = Block.blocksList[id];
+//			
+//			if (block != null)
+//			{
+//		        this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.9F, 1.0F);
+//			}
+//
+//			id = par1World.getBlockId(par2 - 1, par3, par4);
+//			
+//			block = Block.blocksList[id];
+//			
+//			if (block != null)
+//			{
+//		        this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.9F, 1.0F);
+//			}
+//
+//			id = par1World.getBlockId(par2, par3, par4 + 1);
+//			
+//			block = Block.blocksList[id];
+//			
+//			if (block != null)
+//			{
+//		        this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.9F, 1.0F);
+//			}
+//
+//			id = par1World.getBlockId(par2, par3, par4 - 1);
+//			
+//			block = Block.blocksList[id];
+//			
+//			if (block != null)
+//			{
+//		        this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.9F, 1.0F);
+//			}
+//    	}
+//    	
+//    	super.onBlockAdded(par1World, par2, par3, par4);
+//    }
+
+    @Override
+    public boolean canPlaceBlockOnSide(World par1World, int par2, int par3, int par4, int par5)
+    {
+    	int id = GCCoreBlocks.landingPad.blockID;
+
+		if (par1World.getBlockId(par2 + 1, par3, par4) == id && par1World.getBlockId(par2 + 2, par3, par4) == id && par1World.getBlockId(par2 + 3, par3, par4) == id)
+		{
+			return false;
+		}
+
+		if (par1World.getBlockId(par2 - 1, par3, par4) == id && par1World.getBlockId(par2 - 2, par3, par4) == id && par1World.getBlockId(par2 - 3, par3, par4) == id)
+		{
+			return false;
+		}
+
+		if (par1World.getBlockId(par2, par3, par4 + 1) == id && par1World.getBlockId(par2, par3, par4 + 2) == id && par1World.getBlockId(par2, par3, par4 + 3) == id)
+		{
+			return false;
+		}
+
+		if (par1World.getBlockId(par2, par3, par4 - 1) == id && par1World.getBlockId(par2, par3, par4 - 2) == id && par1World.getBlockId(par2, par3, par4 - 3) == id)
+		{
+			return false;
+		}
+    	
+    	if (par1World.getBlockId(par2, par3 - 1, par4) == GCCoreBlocks.landingPad.blockID && par5 == 1)
+    	{
+    		return false;
+    	}
+    	else
+    	{
+            return this.canPlaceBlockAt(par1World, par2, par3, par4);
+    	}
+    }
 	
     @Override
 	public boolean isOpaqueCube()
@@ -24,5 +124,11 @@ public class GCCoreBlockLandingPad extends GCCoreBlock
 	public boolean renderAsNormalBlock()
     {
         return false;
+    }
+
+	@Override
+    public String getTextureFile()
+    {
+    	return "/micdoodle8/mods/galacticraft/core/client/blocks/core.png";
     }
 }
