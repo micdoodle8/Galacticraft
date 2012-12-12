2579f5ddf6eef68f3da6d67420fbbcdb39282668
micdoodle8 <mitchpetrie29@gmail.com>
2012-12-12 13:50:35 -0500

    Added spaceship crash death message.

---
+++ src/common/micdoodle8/mods/galacticraft/core/GCCoreDamageSource.java
@@ -0,0 +1,21 @@
+package micdoodle8.mods.galacticraft.core;
+
+import net.minecraft.src.DamageSource;
+import net.minecraft.src.EntityPlayer;
+import net.minecraft.src.StatCollector;
+
+public class GCCoreDamageSource extends DamageSource
+{
+    public static DamageSource spaceshipExplosion = new GCCoreDamageSource("spaceshipExplosion");
+    
+	public GCCoreDamageSource(String par1Str) 
+	{
+		super(par1Str);
+	}
+
+	@Override
+    public String getDeathMessage(EntityPlayer par1EntityPlayer)
+    {
+        return par1EntityPlayer.username + GalacticraftCore.lang.get("death." + this.damageType);
+    }
+}
