9cf6201e4271cad9473daf8fa24361ca0123b72e
micdoodle8 <mitchpetrie29@gmail.com>
2012-12-13 01:04:06 -0500

    Add missing crap in Io's client.

--- src/minecraft/micdoodle8/mods/galacticraft/io/client/ClientProxyIo.java
+++ src/minecraft/micdoodle8/mods/galacticraft/io/client/ClientProxyIo.java
@@ -1,13 +1,36 @@
 package micdoodle8.mods.galacticraft.io.client;
 
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.util.EnumSet;
+
 import micdoodle8.mods.galacticraft.API.IGalacticraftSubModClient;
 import micdoodle8.mods.galacticraft.API.IPlanetSlotRenderer;
 import micdoodle8.mods.galacticraft.core.GCCoreLocalization;
+import micdoodle8.mods.galacticraft.core.GCCoreUtil;
 import micdoodle8.mods.galacticraft.core.GalacticraftCore;
-import micdoodle8.mods.galacticraft.europa.client.GCEuropaSlotRenderer;
+import micdoodle8.mods.galacticraft.io.blocks.GCIoBlocks;
+import micdoodle8.mods.galacticraft.io.dimension.GCIoWorldProvider;
+import micdoodle8.mods.galacticraft.titan.client.ClientProxyTitan;
+import micdoodle8.mods.galacticraft.titan.client.ClientProxyTitan.ClientPacketHandler;
+import net.minecraft.client.Minecraft;
+import net.minecraft.src.EntityClientPlayerMP;
+import net.minecraft.src.EntityPlayer;
+import net.minecraft.src.INetworkManager;
+import net.minecraft.src.Packet250CustomPayload;
+import net.minecraft.src.WorldClient;
+import net.minecraftforge.client.MinecraftForgeClient;
+import cpw.mods.fml.client.FMLClientHandler;
+import cpw.mods.fml.common.ITickHandler;
+import cpw.mods.fml.common.Side;
+import cpw.mods.fml.common.TickType;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.IPacketHandler;
+import cpw.mods.fml.common.network.NetworkRegistry;
+import cpw.mods.fml.common.network.Player;
+import cpw.mods.fml.common.registry.TickRegistry;
 
 public class ClientProxyIo implements IGalacticraftSubModClient
 {
@@ -26,10 +49,15 @@ public class ClientProxyIo implements IGalacticraftSubModClient
 	public void init(FMLInitializationEvent event) 
 	{
 		GalacticraftCore.registerClientSubMod(this);
+		TickRegistry.registerTickHandler(new TickHandlerClient(), Side.CLIENT);
+        NetworkRegistry.instance().registerChannel(new ClientPacketHandler(), "GcIo", Side.CLIENT);
+		MinecraftForgeClient.preloadTexture("/micdoodle8/mods/galacticraft/io/client/blocks/io.png");
+		MinecraftForgeClient.preloadTexture("/micdoodle8/mods/galacticraft/io/client/items/io.png");
 	}
 
 	public void postInit(FMLPostInitializationEvent event) 
 	{
+		GCIoBlocks.addNames();
 	}
 	
 	@Override
@@ -49,4 +77,63 @@ public class ClientProxyIo implements IGalacticraftSubModClient
 	{
 		return new GCIoSlotRenderer();
 	}
+	
+    public class ClientPacketHandler implements IPacketHandler
+    {
+        @Override
+        public void onPacketData(INetworkManager manager, Packet250CustomPayload packet, Player p)
+        {
+            DataInputStream data = new DataInputStream(new ByteArrayInputStream(packet.data));
+            int packetType = GCCoreUtil.readPacketID(data);
+            EntityPlayer player = (EntityPlayer)p;
+            
+            if (packetType == 0)
+            {
+            	
+            }
+        }
+    }
+	
+	public static class TickHandlerClient implements ITickHandler
+    {
+    	@Override
+    	public void tickStart(EnumSet<TickType> type, Object... tickData)
+        {
+    		ClientProxyTitan.getCurrentTime = System.currentTimeMillis();
+    		
+    		Minecraft minecraft = FMLClientHandler.instance().getClient();
+    		
+            WorldClient world = minecraft.theWorld;
+            
+            EntityClientPlayerMP player = minecraft.thePlayer;
+    		
+    		if (type.equals(EnumSet.of(TickType.CLIENT)))
+            {
+    			if (world != null && world.provider instanceof GCIoWorldProvider)
+    			{
+    				if (world.provider.getSkyProvider() == null)
+                    {
+    					world.provider.setSkyProvider(new GCIoSkyProvider());
+                    }
+    			}
+            }
+        }
+
+    	@Override
+    	public void tickEnd(EnumSet<TickType> type, Object... tickData) 
+    	{
+    	}
+    	
+        @Override
+		public String getLabel()
+        {
+            return "Galacticraft Io Client";
+        }
+
+    	@Override
+    	public EnumSet<TickType> ticks() 
+    	{
+    		return EnumSet.of(TickType.CLIENT);
+    	}
+    }
 }
