87f44d6e319789ca0688ece8bcb33f334cd4235c
micdoodle8 <mitchpetrie29@gmail.com>
2012-12-17 15:04:47 -0500

    Added Pluto.

---
+++ src/minecraft/micdoodle8/mods/galacticraft/pluto/client/GCPlutoMapPlanet.java
@@ -0,0 +1,43 @@
+package micdoodle8.mods.galacticraft.pluto.client;
+
+import micdoodle8.mods.galacticraft.API.IMapPlanet;
+import micdoodle8.mods.galacticraft.API.IPlanetSlotRenderer;
+
+public class GCPlutoMapPlanet implements IMapPlanet
+{
+	@Override
+	public boolean isMoon()
+	{
+		return false;
+	}
+
+	@Override
+	public float getPlanetSize() 
+	{
+		return 15F / 5.55F;
+	}
+
+	@Override
+	public float getDistanceFromCenter() 
+	{
+		return 59100F;
+	}
+
+	@Override
+	public float getPhaseShift() 
+	{
+		return 2780F;
+	}
+
+	@Override
+	public float getStretchValue() 
+	{
+		return 248F;
+	}
+
+	@Override
+	public IPlanetSlotRenderer getSlotRenderer() 
+	{
+		return new GCPlutoSlotRenderer();
+	}
+}
