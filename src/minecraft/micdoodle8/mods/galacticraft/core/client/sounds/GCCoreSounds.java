d5ad92dc16c1c5982825f2d7ea814ed7950e400b
micdoodle8 <mitchpetrie29@gmail.com>
2013-02-07 02:05:50 -0500

    1 in 3 chance of playing new Galacticraft music.

--- src/minecraft/micdoodle8/mods/galacticraft/core/client/sounds/GCCoreSounds.java
+++ src/minecraft/micdoodle8/mods/galacticraft/core/client/sounds/GCCoreSounds.java
@@ -1,12 +1,13 @@
 package micdoodle8.mods.galacticraft.core.client.sounds;
 
-import cpw.mods.fml.client.FMLClientHandler;
 import micdoodle8.mods.galacticraft.API.IGalacticraftWorldProvider;
 import micdoodle8.mods.galacticraft.core.GalacticraftCore;
 import net.minecraft.client.audio.SoundPoolEntry;
 import net.minecraftforge.client.event.sound.PlayBackgroundMusicEvent;
 import net.minecraftforge.client.event.sound.SoundLoadEvent;
 import net.minecraftforge.event.ForgeSubscribe;
+import cpw.mods.fml.client.FMLClientHandler;
+import cpw.mods.fml.common.FMLLog;
 
 /**
  * Copyright 2012-2013, micdoodle8
@@ -35,7 +36,7 @@ public class GCCoreSounds
     {
     	if (FMLClientHandler.instance().getClient().thePlayer.worldObj.provider instanceof IGalacticraftWorldProvider)
     	{
-    		if (FMLClientHandler.instance().getClient().thePlayer.worldObj.rand.nextInt(1) == 0)
+    		if (FMLClientHandler.instance().getClient().thePlayer.worldObj.rand.nextInt(3) == 0)
     		{
     			event.result = new SoundPoolEntry("music/spacerace_JT.ogg", GalacticraftCore.class.getResource("/micdoodle8/mods/galacticraft/core/client/sounds/music/spacerace_JT.ogg"));
     		}
