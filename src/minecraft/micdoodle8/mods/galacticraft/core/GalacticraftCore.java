6e51c037f9c68e27a1788db06ca08d04d5bde284
micdoodle8 <mitchpetrie29@gmail.com>
2013-03-05 00:24:23 -0500

    Push version to 15

--- src/minecraft/micdoodle8/mods/galacticraft/core/GalacticraftCore.java
+++ src/minecraft/micdoodle8/mods/galacticraft/core/GalacticraftCore.java
@@ -100,7 +100,7 @@ public class GalacticraftCore
 	
     public static final int LOCALMAJVERSION = 0;
     public static final int LOCALMINVERSION = 0;
-    public static final int LOCALBUILDVERSION = 14;
+    public static final int LOCALBUILDVERSION = 15;
     public static int remoteMajVer;
     public static int remoteMinVer;
     public static int remoteBuildVer;
