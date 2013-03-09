dbbaa6a0ff458ccdb0a4a9efdd98905a39f0660a
micdoodle8 <mitchpetrie29@gmail.com>
2013-03-09 18:26:08 -0500

    Bump version to 20 for emergency release.

--- src/minecraft/micdoodle8/mods/galacticraft/core/GalacticraftCore.java
+++ src/minecraft/micdoodle8/mods/galacticraft/core/GalacticraftCore.java
@@ -102,7 +102,7 @@ public class GalacticraftCore
 
     public static final int LOCALMAJVERSION = 0;
     public static final int LOCALMINVERSION = 0;
-    public static final int LOCALBUILDVERSION = 19;
+    public static final int LOCALBUILDVERSION = 20;
     public static int remoteMajVer;
     public static int remoteMinVer;
     public static int remoteBuildVer;
