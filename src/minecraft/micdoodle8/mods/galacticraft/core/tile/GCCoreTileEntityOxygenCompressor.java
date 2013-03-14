5dd2d193b2f7723f04abb91d38d8a0023d589bdd
micdoodle8 <mitchpetrie29@gmail.com>
2013-03-13 20:16:22 -0400

    Only let players use compressor within a certain distance

--- src/minecraft/micdoodle8/mods/galacticraft/core/tile/GCCoreTileEntityOxygenCompressor.java
+++ src/minecraft/micdoodle8/mods/galacticraft/core/tile/GCCoreTileEntityOxygenCompressor.java
@@ -260,10 +260,10 @@ public class GCCoreTileEntityOxygenCompressor extends TileEntityElectricityRunna
 	}
 
 	@Override
-	public boolean isUseableByPlayer(EntityPlayer var1)
+	public boolean isUseableByPlayer(EntityPlayer par1EntityPlayer)
 	{
-		return true;
-	}
+        return this.worldObj.getBlockTileEntity(this.xCoord, this.yCoord, this.zCoord) != this ? false : par1EntityPlayer.getDistanceSq(this.xCoord + 0.5D, this.yCoord + 0.5D, this.zCoord + 0.5D) <= 64.0D;
+    }
 
 	@Override
 	public void openChest() {}
