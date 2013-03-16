7a76d5de83110be09efd4b5e897b9830ea125519
micdoodle8 <mitchpetrie29@gmail.com>
2013-03-16 02:33:01 -0400

    Finish up landing pad

--- src/minecraft/micdoodle8/mods/galacticraft/core/blocks/GCCoreBlockLandingPadFull.java
+++ src/minecraft/micdoodle8/mods/galacticraft/core/blocks/GCCoreBlockLandingPadFull.java
@@ -1,6 +1,11 @@
 package micdoodle8.mods.galacticraft.core.blocks;
 
+import java.util.Arrays;
+import java.util.Random;
+
+import mekanism.api.ITubeConnection;
 import micdoodle8.mods.galacticraft.core.GalacticraftCore;
+import micdoodle8.mods.galacticraft.core.oxygen.OxygenNetwork;
 import micdoodle8.mods.galacticraft.core.tile.GCCoreTileEntityLandingPad;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
@@ -8,12 +13,17 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
 import universalelectricity.core.vector.Vector3;
 import universalelectricity.prefab.block.BlockAdvanced;
 import universalelectricity.prefab.multiblock.TileEntityMulti;
 import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 /**
  * Copyright 2012-2013, micdoodle8
@@ -30,21 +40,52 @@ public class GCCoreBlockLandingPadFull extends BlockAdvanced
 	}
 
 	@Override
+    public int quantityDropped(Random par1Random)
+    {
+        return 9;
+    }
+
+	@Override
+    public int idDropped(int par1, Random par2Random, int par3)
+    {
+        return GCCoreBlocks.landingPad.blockID;
+    }
+
+	@Override
     public AxisAlignedBB getSelectedBoundingBoxFromPool(World par1World, int par2, int par3, int par4)
     {
         return AxisAlignedBB.getAABBPool().getAABB((double)par2 - 1.0, (double)par3 + 0.0, (double)par4 - 1.0, (double)par2 + 2.0, (double)par3 + 0.21, (double)par4 + 2.0);
     }
-
+	
 	@Override
-    public void setBlockBoundsBasedOnState(IBlockAccess par1IBlockAccess, int par2, int par3, int par4) 
-    {
-        this.setBlockBounds(-1.0F, 0.0F, -1.0F, 2.0F, 0.2F, 2.0F);
-    }
+	public MovingObjectPosition collisionRayTrace(World world, int x, int y, int z, Vec3 vec3d, Vec3 vec3d1)
+	{
+		this.setBlockBounds(-1.0F, 0.0F, -1.0F, 2.0F, 0.2F, 2.0F);
+
+		final MovingObjectPosition r = super.collisionRayTrace(world, x, y, z, vec3d, vec3d1);
+
+		return super.collisionRayTrace(world, x, y, z, vec3d, vec3d1);
+	}
 
 	public void makeFakeBlock(World worldObj, Vector3 position, Vector3 mainBlock)
 	{
 		worldObj.setBlockAndMetadataWithNotify(position.intX(), position.intY(), position.intZ(), this.blockID, 0, 3);
 		((TileEntityMulti) worldObj.getBlockTileEntity(position.intX(), position.intY(), position.intZ())).setMainBlock(mainBlock);
+	}
+	
+	@Override
+	public void setBlockBoundsBasedOnState(IBlockAccess world, int x, int y, int z)
+	{
+		TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
+		
+		float minX = -1.0F;
+		float minY = 0.0F;
+		float minZ = -1.0F;
+		float maxX = 2.0F;
+		float maxY = 0.2F;
+		float maxZ = 2.0F;
+			
+		this.setBlockBounds(minX, minY, minZ, maxX, maxY, maxZ);
 	}
 
 	@Override
@@ -58,23 +99,6 @@ public class GCCoreBlockLandingPadFull extends BlockAdvanced
     {
     	this.field_94336_cN = par1IconRegister.func_94245_a("galacticraftcore:launch_pad");
     }
-    
-	@Override
-	public void onBlockAdded(World world, int x, int y, int z)
-	{
-		TileEntity tile = (world.getBlockTileEntity(x, y, z));
-		
-		if (tile != null)
-		{
-			Vector3 tileVec = new Vector3(tile);
-			
-			if (tile instanceof GCCoreTileEntityLandingPad)
-			{
-				FMLLog.info("" + tileVec);
-				GCCoreTileEntityLandingPad pad = (GCCoreTileEntityLandingPad) tile;
-			}
-		}
-	}
 	
 	@Override
 	public boolean canPlaceBlockAt(World world, int x, int y, int z)
@@ -87,7 +111,6 @@ public class GCCoreBlockLandingPadFull extends BlockAdvanced
 				{
 					return false;
 				}
-				
 			}
 			
 		}
@@ -106,6 +129,12 @@ public class GCCoreBlockLandingPadFull extends BlockAdvanced
 	{
 		return new GCCoreTileEntityLandingPad();
 	}
+
+	@Override
+    public void onNeighborBlockChange(World par1World, int par2, int par3, int par4, int par5) 
+    {
+		par1World.markBlockForUpdate(par2, par3, par4);
+    }
 
     @Override
 	public boolean isOpaqueCube()
