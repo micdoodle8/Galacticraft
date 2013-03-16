48e8a50e8cc0328c9a652a345d2a54043f790a3a
micdoodle8 <mitchpetrie29@gmail.com>
2013-03-16 02:36:36 -0400

    Finish up render changes of landing pad

--- src/minecraft/micdoodle8/mods/galacticraft/core/client/render/block/GCCoreBlockRendererLandingPad.java
+++ src/minecraft/micdoodle8/mods/galacticraft/core/client/render/block/GCCoreBlockRendererLandingPad.java
@@ -1,5 +1,6 @@
 package micdoodle8.mods.galacticraft.core.client.render.block;
 
+import micdoodle8.mods.galacticraft.core.blocks.GCCoreBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.Tessellator;
@@ -7,8 +8,8 @@ import net.minecraft.world.IBlockAccess;
 
 import org.lwjgl.opengl.GL11;
 
-import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
+import cpw.mods.fml.common.FMLLog;
 
 /**
  * Copyright 2012-2013, micdoodle8
@@ -28,6 +29,7 @@ public class GCCoreBlockRendererLandingPad implements ISimpleBlockRenderingHandl
 	@Override
     public boolean renderWorldBlock(IBlockAccess var1, int var2, int var3, int var4, Block var5, int var6, RenderBlocks var7)
     {
+    	FMLLog.info("true");
     	this.renderBlockLandingPad(var7, var5, var1, var2, var3, var4);
         return true;
     }
@@ -49,7 +51,7 @@ public class GCCoreBlockRendererLandingPad implements ISimpleBlockRenderingHandl
         final Tessellator var3 = Tessellator.instance;
         GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        FMLClientHandler.instance().getClient().renderEngine.func_98187_b("/micdoodle8/mods/galacticraft/core/client/entities/meteor.png");
+//        FMLClientHandler.instance().getClient().renderEngine.func_98187_b("/micdoodle8/mods/galacticraft/core/client/entities/meteor.png");
         var0.setRenderBounds(0.15F, 0.15F, 0.15F, 0.85F, 0.85F, 0.85F);
         var3.startDrawingQuads();
         var3.setNormal(0.0F, -0.8F, 0.0F);
@@ -88,49 +90,115 @@ public class GCCoreBlockRendererLandingPad implements ISimpleBlockRenderingHandl
         final int var5 = var1.getBlockMetadata(par2, par3, par4);
         final int var6 = var5 & 3;
 
-        if (var6 == 0)
+//        if (var6 == 0)
+//        {
+//        	renderBlocks.uvRotateTop = 3;
+//        }
+//        else if (var6 == 3)
+//        {
+//        	renderBlocks.uvRotateTop = 1;
+//        }
+//        else if (var6 == 1)
+//        {
+//        	renderBlocks.uvRotateTop = 2;
+//        }
+//        FMLClientHandler.instance().getClient().renderEngine.func_98187_b("/micdoodle8/mods/galacticraft/core/client/entities/meteor.png");
+
+        renderBlocks.setRenderBounds(0F, 0F, 0F, 1F, 0.4F, 1F);
+        renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
+
+        renderBlocks.setRenderBounds(-1F, 0F, -1F, 2F, 0.2F, 2F);
+        renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
+
+        // +X left
+        if (var1.getBlockId(par2 + 2, par3, par4 - 1) == GCCoreBlocks.fuelLoader.blockID)
         {
-        	renderBlocks.uvRotateTop = 3;
+            renderBlocks.setRenderBounds(1.5F, 0.2F, -0.9F, 2F, 0.901F, -0.1F);
+            renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
         }
-        else if (var6 == 3)
+
+        // +X middle
+        if (var1.getBlockId(par2 + 2, par3, par4) == GCCoreBlocks.fuelLoader.blockID)
         {
-        	renderBlocks.uvRotateTop = 1;
+            renderBlocks.setRenderBounds(1.5F, 0.2F, 0.1F, 2F, 0.9F, 0.9F);
+            renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
         }
-        else if (var6 == 1)
+
+        // +X right
+        if (var1.getBlockId(par2 + 2, par3, par4 + 1) == GCCoreBlocks.fuelLoader.blockID)
         {
-        	renderBlocks.uvRotateTop = 2;
+            renderBlocks.setRenderBounds(1.5F, 0.2F, 1.1F, 2F, 0.9F, 1.9F);
+            renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
         }
-        FMLClientHandler.instance().getClient().renderEngine.func_98187_b("/micdoodle8/mods/galacticraft/core/client/entities/meteor.png");
 
-        renderBlocks.setRenderBounds(-1F, 0F, -1F, 2F, 0.4F, 2F);
-        renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
+        // +Z left
+        if (var1.getBlockId(par2 + 1, par3, par4 + 2) == GCCoreBlocks.fuelLoader.blockID)
+        {
+            renderBlocks.setRenderBounds(1.1F, 0.2F, 1.5F, 1.9F, 0.901F, 2F);
+            renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
+        }
 
-        renderBlocks.setRenderBounds(0.52F, 0.85F, 0.4F, 0.68F, 0.88F, 0.6F);
-        renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
+        // +Z left
+        if (var1.getBlockId(par2, par3, par4 + 2) == GCCoreBlocks.fuelLoader.blockID)
+        {
+            renderBlocks.setRenderBounds(0.1F, 0.2F, 1.5F, 0.9F, 0.901F, 2F);
+            renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
+        }
 
-        renderBlocks.setRenderBounds(0.25F, 0.8F, 0.25F, 0.75F, 0.85F, 0.75F);
-        renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
+        // +Z right
+        if (var1.getBlockId(par2 - 1, par3, par4 + 2) == GCCoreBlocks.fuelLoader.blockID)
+        {
+            renderBlocks.setRenderBounds(-0.9F, 0.2F, 1.5F, -0.1F, 0.9F, 2F);
+            renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
+        }
 
-        renderBlocks.setRenderBounds(0.25F, 0.15F, 0.25F, 0.75F, 0.2F, 0.75F);
-        renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
+        // -X left
+        if (var1.getBlockId(par2 - 2, par3, par4 + 1) == GCCoreBlocks.fuelLoader.blockID)
+        {
+            renderBlocks.setRenderBounds(-1.0F, 0.2F, 1.1F, -0.5F, 0.901F, 1.9F);
+            renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
+        }
 
-        renderBlocks.setRenderBounds(0.15F, 0.3F, 0.25F, 0.2F, 0.7F, 0.75F);
-        renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
+        // -X middle
+        if (var1.getBlockId(par2 - 2, par3, par4) == GCCoreBlocks.fuelLoader.blockID)
+        {
+            renderBlocks.setRenderBounds(-1.0F, 0.2F, 0.1F, -0.5F, 0.9F, 0.9F);
+            renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
+        }
 
-        renderBlocks.setRenderBounds(0.8F, 0.3F, 0.25F, 0.85F, 0.7F, 0.75F);
-        renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
+        // -X right
+        if (var1.getBlockId(par2 - 2, par3, par4 - 1) == GCCoreBlocks.fuelLoader.blockID)
+        {
+            renderBlocks.setRenderBounds(-1.0F, 0.2F, -0.9F, -0.5F, 0.9F, -0.1F);
+            renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
+        }
 
-        renderBlocks.setRenderBounds(0.25F, 0.3F, 0.15F, 0.75F, 0.7F, 0.2F);
-        renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
+        // -Z right
+        if (var1.getBlockId(par2 + 1, par3, par4 - 2) == GCCoreBlocks.fuelLoader.blockID)
+        {
+            renderBlocks.setRenderBounds(1.1F, 0.2F, -1.0F, 1.9F, 0.9F, -0.5F);
+            renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
+        }
 
-        renderBlocks.setRenderBounds(0.25F, 0.3F, 0.8F, 0.75F, 0.7F, 0.85F);
-        renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
+        // -Z middle
+        if (var1.getBlockId(par2, par3, par4 - 2) == GCCoreBlocks.fuelLoader.blockID)
+        {
+            renderBlocks.setRenderBounds(0.1F, 0.2F, -1.0F, 0.9F, 0.9F, -0.5F);
+            renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
+        }
 
-        renderBlocks.setRenderBounds(0.12F, 0.12F, 0.12F, 0.88F, 0.88F, 0.88F);
+        // -Z left
+        if (var1.getBlockId(par2 - 1, par3, par4 - 2) == GCCoreBlocks.fuelLoader.blockID)
+        {
+            renderBlocks.setRenderBounds(-0.9F, 0.2F, -1.0F, -0.1F, 0.901F, -0.5F);
+            renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
+        }
+
+//        renderBlocks.setRenderBounds(-1F, 0.4F, -1F, 2F, 0.4F, 2F);
+//        renderBlocks.renderStandardBlock(par1Block, par2, par3, par4);
 
         renderBlocks.clearOverrideBlockTexture();
         par1Block.setBlockBoundsForItemRender();
         renderBlocks.uvRotateTop = 0;
-        FMLClientHandler.instance().getClient().renderEngine.func_98185_a();
     }
 }
