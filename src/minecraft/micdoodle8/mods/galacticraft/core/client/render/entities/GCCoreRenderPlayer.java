9ee1f016198b1f7f0e935743ad0c3b875e84e6dd
micdoodle8 <mitchpetrie29@gmail.com>
2013-03-19 15:38:38 -0400

    Fix donor cape not rendering

--- src/minecraft/micdoodle8/mods/galacticraft/core/client/render/entities/GCCoreRenderPlayer.java
+++ src/minecraft/micdoodle8/mods/galacticraft/core/client/render/entities/GCCoreRenderPlayer.java
@@ -26,6 +26,7 @@ import net.minecraftforge.client.MinecraftForgeClient;
 import org.lwjgl.opengl.GL11;
 
 import cpw.mods.fml.client.FMLClientHandler;
+import cpw.mods.fml.common.FMLLog;
 
 public class GCCoreRenderPlayer extends RenderPlayer
 {
@@ -42,127 +43,6 @@ public class GCCoreRenderPlayer extends RenderPlayer
 	{
 		return this.modelBipedMain;
 	}
-	
-	protected void renderEquippedItems(EntityLiving par1EntityLiving, float par2)
-    {
-        float f1 = 1.0F;
-        GL11.glColor3f(f1, f1, f1);
-//        super.renderEquippedItems(par1EntityLiving, par2);
-        ItemStack itemstack = par1EntityLiving.getHeldItem();
-        ItemStack itemstack1 = par1EntityLiving.getCurrentArmor(3);
-        float f2;
-
-        if (itemstack1 != null)
-        {
-            GL11.glPushMatrix();
-            this.modelBipedMain.bipedHead.postRender(0.0625F);
-
-            IItemRenderer customRenderer = MinecraftForgeClient.getItemRenderer(itemstack1, EQUIPPED);
-            boolean is3D = (customRenderer != null && customRenderer.shouldUseRenderHelper(EQUIPPED, itemstack1, BLOCK_3D));
-
-            if (itemstack1.getItem() instanceof ItemBlock)
-            {
-                if (is3D || RenderBlocks.renderItemIn3d(Block.blocksList[itemstack1.itemID].getRenderType()))
-                {
-                    f2 = 0.625F;
-                    GL11.glTranslatef(0.0F, -0.25F, 0.0F);
-                    GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
-                    GL11.glScalef(f2, -f2, -f2);
-                }
-
-                this.renderManager.itemRenderer.renderItem(par1EntityLiving, itemstack1, 0);
-            }
-            else if (itemstack1.getItem().itemID == Item.skull.itemID)
-            {
-                f2 = 1.0625F;
-                GL11.glScalef(f2, -f2, -f2);
-                String s = "";
-
-                if (itemstack1.hasTagCompound() && itemstack1.getTagCompound().hasKey("SkullOwner"))
-                {
-                    s = itemstack1.getTagCompound().getString("SkullOwner");
-                }
-
-                TileEntitySkullRenderer.skullRenderer.func_82393_a(-0.5F, 0.0F, -0.5F, 1, 180.0F, itemstack1.getItemDamage(), s);
-            }
-
-            GL11.glPopMatrix();
-        }
-
-        if (itemstack != null)
-        {
-            GL11.glPushMatrix();
-
-            if (this.mainModel.isChild)
-            {
-                f2 = 0.5F;
-                GL11.glTranslatef(0.0F, 0.625F, 0.0F);
-                GL11.glRotatef(-20.0F, -1.0F, 0.0F, 0.0F);
-                GL11.glScalef(f2, f2, f2);
-            }
-
-            this.modelBipedMain.bipedRightArm.postRender(0.0625F);
-            GL11.glTranslatef(-0.0625F, 0.4375F, 0.0625F);
-
-            IItemRenderer customRenderer = MinecraftForgeClient.getItemRenderer(itemstack, EQUIPPED);
-            boolean is3D = (customRenderer != null && customRenderer.shouldUseRenderHelper(EQUIPPED, itemstack, BLOCK_3D));
-
-            if (itemstack.getItem() instanceof ItemBlock && (is3D || RenderBlocks.renderItemIn3d(Block.blocksList[itemstack.itemID].getRenderType())))
-            {
-                f2 = 0.5F;
-                GL11.glTranslatef(0.0F, 0.1875F, -0.3125F);
-                f2 *= 0.75F;
-                GL11.glRotatef(20.0F, 1.0F, 0.0F, 0.0F);
-                GL11.glRotatef(45.0F, 0.0F, 1.0F, 0.0F);
-                GL11.glScalef(-f2, -f2, f2);
-            }
-            else if (itemstack.itemID == Item.bow.itemID)
-            {
-                f2 = 0.625F;
-                GL11.glTranslatef(0.0F, 0.125F, 0.3125F);
-                GL11.glRotatef(-20.0F, 0.0F, 1.0F, 0.0F);
-                GL11.glScalef(f2, -f2, f2);
-                GL11.glRotatef(-100.0F, 1.0F, 0.0F, 0.0F);
-                GL11.glRotatef(45.0F, 0.0F, 1.0F, 0.0F);
-            }
-            else if (Item.itemsList[itemstack.itemID].isFull3D())
-            {
-                f2 = 0.625F;
-
-                if (Item.itemsList[itemstack.itemID].shouldRotateAroundWhenRendering())
-                {
-                    GL11.glRotatef(180.0F, 0.0F, 0.0F, 1.0F);
-                    GL11.glTranslatef(0.0F, -0.125F, 0.0F);
-                }
-
-                GL11.glTranslatef(0.0F, 0.1875F, 0.0F);
-                GL11.glScalef(f2, -f2, f2);
-                GL11.glRotatef(-100.0F, 1.0F, 0.0F, 0.0F);
-                GL11.glRotatef(45.0F, 0.0F, 1.0F, 0.0F);
-            }
-            else
-            {
-                f2 = 0.375F;
-                GL11.glTranslatef(0.25F, 0.1875F, -0.1875F);
-                GL11.glScalef(f2, f2, f2);
-                GL11.glRotatef(60.0F, 0.0F, 0.0F, 1.0F);
-                GL11.glRotatef(-90.0F, 1.0F, 0.0F, 0.0F);
-                GL11.glRotatef(20.0F, 0.0F, 0.0F, 1.0F);
-            }
-
-            this.renderManager.itemRenderer.renderItem(par1EntityLiving, itemstack, 0);
-
-            if (itemstack.getItem().requiresMultipleRenderPasses())
-            {
-                for (int x = 1; x < itemstack.getItem().getRenderPasses(itemstack.getItemDamage()); x++)
-                {
-                    this.renderManager.itemRenderer.renderItem(par1EntityLiving, itemstack, x);
-                }
-            }
-
-            GL11.glPopMatrix();
-        }
-    }
 
     public boolean isAABBInBreathableAirBlock()
     {
