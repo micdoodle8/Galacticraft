e8c553a3662c2ce48d1406084f30a9116232721f
micdoodle8 <mitchpetrie29@gmail.com>
2013-03-22 14:43:13 -0400

    Fix parachute textures

--- src/minecraft/micdoodle8/mods/galacticraft/core/items/GCCoreItemParachute.java
+++ src/minecraft/micdoodle8/mods/galacticraft/core/items/GCCoreItemParachute.java
@@ -80,7 +80,7 @@ public class GCCoreItemParachute extends Item
 
 		for (ItemStack itemStack : list)
 		{
-			this.icons[list.indexOf(itemStack)] = iconRegister.func_94245_a(this.getUnlocalizedName(itemStack).replace("item.", "GalacticraftCore:"));
+			this.icons[list.indexOf(itemStack)] = iconRegister.func_94245_a(this.getUnlocalizedName(itemStack).replace("item.", "galacticraftcore:"));
 		}
 	}
 
