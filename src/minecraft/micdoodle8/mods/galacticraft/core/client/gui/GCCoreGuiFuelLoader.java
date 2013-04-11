921e77491bbb478a0441c162f362462ad177e955
micdoodle8 <mitchpetrie29@gmail.com>
2013-04-11 01:38:50 -0400

    Fuel loader can now be properly disabled

--- src/minecraft/micdoodle8/mods/galacticraft/core/client/gui/GCCoreGuiFuelLoader.java
+++ src/minecraft/micdoodle8/mods/galacticraft/core/client/gui/GCCoreGuiFuelLoader.java
@@ -7,6 +7,7 @@ import micdoodle8.mods.galacticraft.core.inventory.GCCoreContainerFuelLoader;
 import micdoodle8.mods.galacticraft.core.items.GCCoreItemFuelCanister;
 import micdoodle8.mods.galacticraft.core.items.GCCoreItems;
 import micdoodle8.mods.galacticraft.core.tile.GCCoreTileEntityFuelLoader;
+import micdoodle8.mods.galacticraft.core.util.PacketUtil;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.InventoryPlayer;
@@ -19,6 +20,7 @@ import org.lwjgl.opengl.GL11;
 import universalelectricity.core.electricity.ElectricityDisplay;
 import universalelectricity.core.electricity.ElectricityDisplay.ElectricUnit;
 import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.common.network.PacketDispatcher;
 
 /**
  * Copyright 2012-2013, micdoodle8
@@ -39,6 +41,17 @@ public class GCCoreGuiFuelLoader extends GuiContainer
         this.ySize = 180;
 	}
 
+    @Override
+    protected void actionPerformed(GuiButton par1GuiButton) 
+    {
+    	switch (par1GuiButton.id)
+    	{
+    	case 0:
+			PacketDispatcher.sendPacketToServer(PacketUtil.createPacket(GalacticraftCore.CHANNEL, 17, new Object[] {this.fuelLoaderInv.xCoord, this.fuelLoaderInv.yCoord, this.fuelLoaderInv.zCoord}));
+			break;
+    	}
+    }
+
 	@Override
     public void initGui()
     {
@@ -51,6 +64,7 @@ public class GCCoreGuiFuelLoader extends GuiContainer
     protected void drawGuiContainerForegroundLayer(int par1, int par2)
     {
     	this.fontRenderer.drawString("Fuel Loader", 60, 10, 4210752);
+    	this.buttonLoadFuel.displayString = !this.fuelLoaderInv.getDisabled() ? "Stop" : "Load Fuel";
         this.buttonLoadFuel.enabled = !(this.fuelLoaderInv.fuelTank.getLiquid() == null || this.fuelLoaderInv.fuelTank.getLiquid().amount == 0);
 		this.fontRenderer.drawString("Status: " + this.getStatus(), 28, 45 + 23 - 46, 4210752);
 		this.fontRenderer.drawString(ElectricityDisplay.getDisplay(this.fuelLoaderInv.WATTS_PER_TICK * 20, ElectricUnit.WATT), 28, 56 + 23 - 46, 4210752);
@@ -78,6 +92,11 @@ public class GCCoreGuiFuelLoader extends GuiContainer
     	{
     		return EnumColor.DARK_RED + "Item Cannot Be Loaded!";
     	}
+
+    	if (this.fuelLoaderInv.getDisabled())
+    	{
+    		return EnumColor.ORANGE + "Ready";
+    	}
     	
     	if (this.fuelLoaderInv.wattsReceived > 0 || this.fuelLoaderInv.ic2WattsReceived > 0)
     	{
@@ -101,8 +120,9 @@ public class GCCoreGuiFuelLoader extends GuiContainer
 		final int var6 = (this.height - this.ySize) / 2;
 		this.drawTexturedModalRect(var5, var6 + 5, 0, 0, this.xSize, 181);
 		
-		final int fuelLevel = this.fuelLoaderInv.fuelTank.getLiquid() == null ? 0 : (this.fuelLoaderInv.fuelTank.getLiquid().amount / MathHelper.floor_double((2000.0D / GCCoreItems.fuelCanister.getMaxDamage() + 1)));
+//		final int fuelLevel = this.fuelLoaderInv.fuelTank.getLiquid() == null ? 0 : (this.fuelLoaderInv.fuelTank.getLiquid().amount / MathHelper.floor_double((2000.0D / GCCoreItems.fuelCanister.getMaxDamage() + 1)));
 		
+		int fuelLevel = this.fuelLoaderInv.getScaledFuelLevel(38);
         this.drawTexturedModalRect(((width - xSize) / 2) + 7, ((height - ySize) / 2) + 17 + 54 - fuelLevel, 176, 38 - fuelLevel, 16, fuelLevel);
 	}
 }
