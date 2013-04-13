6fc9963011824ad7eeb18e71d0ec75f849099858
micdoodle8 <mitchpetrie29@gmail.com>
2013-04-13 01:24:59 -0400

    Change code to make adding visual recipe display for NEI easier

--- src/minecraft/micdoodle8/mods/galacticraft/core/nei/NEIGalacticraftConfig.java
+++ src/minecraft/micdoodle8/mods/galacticraft/core/nei/NEIGalacticraftConfig.java
@@ -1,14 +1,27 @@
 package micdoodle8.mods.galacticraft.core.nei;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
 import micdoodle8.mods.galacticraft.core.GalacticraftCore;
+import micdoodle8.mods.galacticraft.core.items.GCCoreItems;
+import micdoodle8.mods.galacticraft.core.util.RecipeUtil;
+import codechicken.nei.PositionedStack;
 import codechicken.nei.api.API;
 import codechicken.nei.api.IConfigureNEI;
 
 public class NEIGalacticraftConfig implements IConfigureNEI
 {
+	public static HashMap<ArrayList<PositionedStack>, PositionedStack> rocketBenchRecipes = new HashMap<ArrayList<PositionedStack>, PositionedStack>();
+
 	@Override
 	public void loadConfig()
 	{
+		this.registerRocketBenchRecipes();
 		API.hideItems(GalacticraftCore.hiddenItems);
 		API.registerRecipeHandler(new RocketT1RecipeHandler());
 		API.registerUsageHandler(new RocketT1RecipeHandler());
@@ -24,5 +37,55 @@ public class NEIGalacticraftConfig implements IConfigureNEI
 	public String getVersion()
 	{
 		return GalacticraftCore.LOCALMAJVERSION + "." + GalacticraftCore.LOCALMINVERSION + "." + GalacticraftCore.LOCALBUILDVERSION;
+	}
+	
+	public void registerRocketBenchRecipe(ArrayList<PositionedStack> input, PositionedStack output)
+	{
+		rocketBenchRecipes.put(input, output);
+	}
+
+	public Set<Entry<ArrayList<PositionedStack>, PositionedStack>> getRocketBenchRecipes()
+	{
+		return rocketBenchRecipes.entrySet();
+	}
+	
+	public void registerRocketBenchRecipes()
+	{
+        final int changey = 23;
+        
+		final ArrayList<PositionedStack> input1 = new ArrayList<PositionedStack>();
+		input1.add(new PositionedStack(new ItemStack(GCCoreItems.rocketNoseCone), 45, -8 + changey));
+		input1.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 36, -6 + 0 * 18 + 16 + changey));
+		input1.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 36, -6 + 1 * 18 + 16 + changey));
+		input1.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 36, -6 + 2 * 18 + 16 + changey));
+		input1.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 36, -6 + 3 * 18 + 16 + changey));
+		input1.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 54, -6 + 0 * 18 + 16 + changey));
+		input1.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 54, -6 + 1 * 18 + 16 + changey));
+		input1.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 54, -6 + 2 * 18 + 16 + changey));
+		input1.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 54, -6 + 3 * 18 + 16 + changey));
+		input1.add(new PositionedStack(new ItemStack(GCCoreItems.rocketEngine), 45, 82 + changey));
+		input1.add(new PositionedStack(new ItemStack(GCCoreItems.rocketFins), 18, 64 + changey));
+		input1.add(new PositionedStack(new ItemStack(GCCoreItems.rocketFins), 18, 82 + changey));
+		input1.add(new PositionedStack(new ItemStack(GCCoreItems.rocketFins), 72, 64 + changey));
+		input1.add(new PositionedStack(new ItemStack(GCCoreItems.rocketFins), 72, 82 + changey));
+		this.registerRocketBenchRecipe(input1, new PositionedStack(new ItemStack(GCCoreItems.spaceship, 1, 0), 139, 69 + changey));
+
+		final ArrayList<PositionedStack> input2 = new ArrayList<PositionedStack>();
+		input2.add(new PositionedStack(new ItemStack(GCCoreItems.rocketNoseCone), 45, -8 + changey));
+		input2.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 36, -6 + 0 * 18 + 16 + changey));
+		input2.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 36, -6 + 1 * 18 + 16 + changey));
+		input2.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 36, -6 + 2 * 18 + 16 + changey));
+		input2.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 36, -6 + 3 * 18 + 16 + changey));
+		input2.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 54, -6 + 0 * 18 + 16 + changey));
+		input2.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 54, -6 + 1 * 18 + 16 + changey));
+		input2.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 54, -6 + 2 * 18 + 16 + changey));
+		input2.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 54, -6 + 3 * 18 + 16 + changey));
+		input2.add(new PositionedStack(new ItemStack(GCCoreItems.rocketEngine), 45, 82 + changey));
+		input2.add(new PositionedStack(new ItemStack(GCCoreItems.rocketFins), 18, 64 + changey));
+		input2.add(new PositionedStack(new ItemStack(GCCoreItems.rocketFins), 18, 82 + changey));
+		input2.add(new PositionedStack(new ItemStack(GCCoreItems.rocketFins), 72, 64 + changey));
+		input2.add(new PositionedStack(new ItemStack(GCCoreItems.rocketFins), 72, 82 + changey));
+		input2.add(new PositionedStack(new ItemStack(Block.chest), 90 + 0 * 23, -15 + changey));
+		this.registerRocketBenchRecipe(input2, new PositionedStack(new ItemStack(GCCoreItems.spaceship, 1, 1), 139, 69 + changey));
 	}
 }
--- src/minecraft/micdoodle8/mods/galacticraft/core/nei/RocketT1RecipeHandler.java
+++ src/minecraft/micdoodle8/mods/galacticraft/core/nei/RocketT1RecipeHandler.java
@@ -1,13 +1,10 @@
 package micdoodle8.mods.galacticraft.core.nei;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
-import micdoodle8.mods.galacticraft.core.items.GCCoreItems;
-import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 
 import org.lwjgl.opengl.GL11;
@@ -34,49 +31,7 @@ public class RocketT1RecipeHandler extends TemplateRecipeHandler
 
 	public Set<Entry<ArrayList<PositionedStack>, PositionedStack>> getRecipes()
 	{
-		final HashMap<ArrayList<PositionedStack>, PositionedStack> recipes = new HashMap<ArrayList<PositionedStack>, PositionedStack>();
-
-        final int changex = -3;
-        final int changey = 23;
-
-		// Spaceship T1
-		final ArrayList<PositionedStack> input1 = new ArrayList<PositionedStack>();
-		input1.add(new PositionedStack(new ItemStack(GCCoreItems.rocketNoseCone), 48 + changex, -8 + changey));
-		input1.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 39 + changex, -6 + 0 * 18 + 16 + changey));
-		input1.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 39 + changex, -6 + 1 * 18 + 16 + changey));
-		input1.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 39 + changex, -6 + 2 * 18 + 16 + changey));
-		input1.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 39 + changex, -6 + 3 * 18 + 16 + changey));
-		input1.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 57 + changex, -6 + 0 * 18 + 16 + changey));
-		input1.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 57 + changex, -6 + 1 * 18 + 16 + changey));
-		input1.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 57 + changex, -6 + 2 * 18 + 16 + changey));
-		input1.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 57 + changex, -6 + 3 * 18 + 16 + changey));
-		input1.add(new PositionedStack(new ItemStack(GCCoreItems.rocketEngine), 48 + changex, 82 + changey));
-		input1.add(new PositionedStack(new ItemStack(GCCoreItems.rocketFins), 21 + changex, 64 + changey));
-		input1.add(new PositionedStack(new ItemStack(GCCoreItems.rocketFins), 21 + changex, 82 + changey));
-		input1.add(new PositionedStack(new ItemStack(GCCoreItems.rocketFins), 75 + changex, 64 + changey));
-		input1.add(new PositionedStack(new ItemStack(GCCoreItems.rocketFins), 75 + changex, 82 + changey));
-		recipes.put(input1, new PositionedStack(new ItemStack(GCCoreItems.spaceship, 1, 0), 142 + changex, 69 + changey));
-
-		// Spaceship T1 with 27 storage space
-		final ArrayList<PositionedStack> input2 = new ArrayList<PositionedStack>();
-		input2.add(new PositionedStack(new ItemStack(GCCoreItems.rocketNoseCone), 48 + changex, -8 + changey));
-		input2.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 39 + changex, -6 + 0 * 18 + 16 + changey));
-		input2.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 39 + changex, -6 + 1 * 18 + 16 + changey));
-		input2.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 39 + changex, -6 + 2 * 18 + 16 + changey));
-		input2.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 39 + changex, -6 + 3 * 18 + 16 + changey));
-		input2.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 57 + changex, -6 + 0 * 18 + 16 + changey));
-		input2.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 57 + changex, -6 + 1 * 18 + 16 + changey));
-		input2.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 57 + changex, -6 + 2 * 18 + 16 + changey));
-		input2.add(new PositionedStack(new ItemStack(GCCoreItems.heavyPlating), 57 + changex, -6 + 3 * 18 + 16 + changey));
-		input2.add(new PositionedStack(new ItemStack(GCCoreItems.rocketEngine), 48 + changex, 82 + changey));
-		input2.add(new PositionedStack(new ItemStack(GCCoreItems.rocketFins), 21 + changex, 64 + changey));
-		input2.add(new PositionedStack(new ItemStack(GCCoreItems.rocketFins), 21 + changex, 82 + changey));
-		input2.add(new PositionedStack(new ItemStack(GCCoreItems.rocketFins), 75 + changex, 64 + changey));
-		input2.add(new PositionedStack(new ItemStack(GCCoreItems.rocketFins), 75 + changex, 82 + changey));
-		input2.add(new PositionedStack(new ItemStack(Block.chest), 93 + 0 * 26 + changex, -15 + changey));
-		recipes.put(input2, new PositionedStack(new ItemStack(GCCoreItems.spaceship, 1, 1), 142 + changex, 69 + changey));
-
-		return recipes.entrySet();
+		return NEIGalacticraftConfig.rocketBenchRecipes.entrySet();
 	}
 
 	@Override
@@ -185,7 +140,7 @@ public class RocketT1RecipeHandler extends TemplateRecipeHandler
 	@Override
 	public String getRecipeName()
 	{
-		return "Rocket T1";
+		return "NASA Workbench";
 	}
 
 	@Override
