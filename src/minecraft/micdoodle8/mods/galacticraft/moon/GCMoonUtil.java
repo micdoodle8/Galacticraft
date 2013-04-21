7a41f04cd08d19552c22679042c64cf5f27f5dd4
micdoodle8 <mitchpetrie29@gmail.com>
2013-04-21 13:44:27 -0400

    Fix smelting recipe crash when Basic Components isn't loaded

--- src/minecraft/micdoodle8/mods/galacticraft/moon/GCMoonUtil.java
+++ src/minecraft/micdoodle8/mods/galacticraft/moon/GCMoonUtil.java
@@ -1,5 +1,7 @@
 package micdoodle8.mods.galacticraft.moon;
 
+import ic2.api.Items;
+import micdoodle8.mods.galacticraft.core.GCCoreCompatibilityManager;
 import micdoodle8.mods.galacticraft.moon.blocks.GCMoonBlocks;
 import micdoodle8.mods.galacticraft.moon.items.GCMoonItems;
 import net.minecraft.item.Item;
@@ -30,8 +32,19 @@ public class GCMoonUtil
 	public static void addSmeltingRecipes()
 	{
 		FurnaceRecipes.smelting().addSmelting(GCMoonItems.meteoricIronRaw.itemID, new ItemStack(GCMoonItems.meteoricIronIngot), 1.0F);
-		FurnaceRecipes.smelting().addSmelting(GCMoonBlocks.blockMoon.blockID, 0, new ItemStack(BasicComponents.itemIngot, 1, 0), 1.0F);
-		FurnaceRecipes.smelting().addSmelting(GCMoonBlocks.blockMoon.blockID, 1, new ItemStack(BasicComponents.itemIngot, 1, 1), 1.0F);
+		
+		if (GCCoreCompatibilityManager.isBCompLoaded())
+		{
+			FurnaceRecipes.smelting().addSmelting(GCMoonBlocks.blockMoon.blockID, 0, new ItemStack(BasicComponents.itemIngot, 1, 0), 1.0F);
+			FurnaceRecipes.smelting().addSmelting(GCMoonBlocks.blockMoon.blockID, 1, new ItemStack(BasicComponents.itemIngot, 1, 1), 1.0F);
+		}
+		
+		if (GCCoreCompatibilityManager.isIc2Loaded())
+		{
+			FurnaceRecipes.smelting().addSmelting(GCMoonBlocks.blockMoon.blockID, 0, Items.getItem("copperIngot"), 1.0F);
+			FurnaceRecipes.smelting().addSmelting(GCMoonBlocks.blockMoon.blockID, 1, Items.getItem("tinIngot"), 1.0F);
+		}
+		
 		FurnaceRecipes.smelting().addSmelting(GCMoonBlocks.blockMoon.blockID, 2, new ItemStack(GCMoonItems.cheeseCurd), 1.0F);
 	}
 }
