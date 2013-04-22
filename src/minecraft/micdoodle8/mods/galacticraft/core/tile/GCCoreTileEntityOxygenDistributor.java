c6b6fe2ebfdf2afb71e105b8ed38cff09909093f
micdoodle8 <mitchpetrie29@gmail.com>
2013-04-22 19:20:21 -0400

    Oxygen Distributor should now pull Oxygen properly

--- src/minecraft/micdoodle8/mods/galacticraft/core/tile/GCCoreTileEntityOxygenDistributor.java
+++ src/minecraft/micdoodle8/mods/galacticraft/core/tile/GCCoreTileEntityOxygenDistributor.java
@@ -329,6 +329,6 @@ public class GCCoreTileEntityOxygenDistributor extends GCCoreTileEntityOxygen im
 	@Override
 	public boolean shouldPullOxygen() 
 	{
-		return this.ic2Energy > 0 || this.wattsReceived > 0;
+		return true;
 	}
 }
