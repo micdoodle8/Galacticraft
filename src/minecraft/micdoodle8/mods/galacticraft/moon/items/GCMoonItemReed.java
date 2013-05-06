d10157de66c692720c3c325b587c35064f329319
micdoodle8 <mitchpetrie29@gmail.com>
2013-05-06 01:32:10 -0400

    Indicate that cheese is placeable

--- src/minecraft/micdoodle8/mods/galacticraft/moon/items/GCMoonItemReed.java
+++ src/minecraft/micdoodle8/mods/galacticraft/moon/items/GCMoonItemReed.java
@@ -1,10 +1,16 @@
 package micdoodle8.mods.galacticraft.moon.items;
 
+import java.util.List;
+
+import micdoodle8.mods.galacticraft.core.client.ClientProxyCore;
 import micdoodle8.mods.galacticraft.moon.GalacticraftMoon;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumRarity;
 import net.minecraft.item.ItemReed;
+import net.minecraft.item.ItemStack;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
@@ -27,5 +33,19 @@ public class GCMoonItemReed extends ItemReed
 	public CreativeTabs getCreativeTab()
     {
         return GalacticraftMoon.galacticraftMoonTab;
+    }
+
+    @Override
+	@SideOnly(Side.CLIENT)
+    public EnumRarity getRarity(ItemStack par1ItemStack)
+    {
+        return ClientProxyCore.galacticraftItem;
+    }
+
+    @Override
+	@SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, List par3List, boolean par4) 
+    {
+    	par3List.add("Placeable");
     }
 }
