9ea59d58aaf58d54cac45814fd1f1bd4da45c2dd
micdoodle8 <mitchpetrie29@gmail.com>
2013-06-02 16:27:53 -0400

    Fix rotations in first person and third person while holding spaceship

--- common/micdoodle8/mods/galacticraft/core/client/render/item/GCCoreItemRendererSpaceship.java
+++ common/micdoodle8/mods/galacticraft/core/client/render/item/GCCoreItemRendererSpaceship.java
@@ -26,7 +26,7 @@ public class GCCoreItemRendererSpaceship implements IItemRenderer
 
     public static RenderItem drawItems = new RenderItem();
 
-    private void renderPipeItem(ItemRenderType type, RenderBlocks render, ItemStack item, float translateX, float translateY, float translateZ)
+    private void renderSpaceship(ItemRenderType type, RenderBlocks render, ItemStack item, float translateX, float translateY, float translateZ)
     {
         final EntityPlayer player = FMLClientHandler.instance().getClient().thePlayer;
 
@@ -39,10 +39,24 @@ public class GCCoreItemRendererSpaceship implements IItemRenderer
 
         if (type == ItemRenderType.EQUIPPED)
         {
-            GL11.glRotatef(90, 1.0F, 0, 0);
-            GL11.glRotatef(-20, 0.0F, 1, 0);
-            GL11.glRotatef(20, 0.0F, 1, 1);
-            GL11.glTranslatef(1.6F, 0.0F, 0F);
+            GL11.glRotatef(70, 1.0F, 0, 0);
+            GL11.glRotatef(-10, 0.0F, 1, 0);
+            GL11.glRotatef(50, 0.0F, 1, 1);
+            GL11.glTranslatef(0F, 2.0F, 0F);
+            GL11.glScalef(5.2F, 5.2F, 5.2F);
+
+            if (player != null && player.ridingEntity != null && player.ridingEntity instanceof GCCoreEntityRocketT1)
+            {
+                GL11.glScalef(0.0F, 0.0F, 0.0F);
+            }
+        }
+        
+        if (type == ItemRenderType.EQUIPPED_FIRST_PERSON)
+        {
+            GL11.glTranslatef(-0.5F, 4.2F, 0F);
+            GL11.glRotatef(28, 0.0F, 0, 1);
+            GL11.glRotatef(50 + 180, 0.0F, 1, 0);
+            GL11.glRotatef(73, 1.0F, 0, 0);
             GL11.glScalef(5.2F, 5.2F, 5.2F);
 
             if (player != null && player.ridingEntity != null && player.ridingEntity instanceof GCCoreEntityRocketT1)
@@ -53,6 +67,7 @@ public class GCCoreItemRendererSpaceship implements IItemRenderer
 
         GL11.glTranslatef(var12, var13 - 0.1F, var14);
         GL11.glScalef(-0.4F, -0.4F, 0.4F);
+        
         if (type == ItemRenderType.INVENTORY || type == ItemRenderType.ENTITY)
         {
             if (type == ItemRenderType.INVENTORY)
@@ -70,6 +85,7 @@ public class GCCoreItemRendererSpaceship implements IItemRenderer
             GL11.glTranslatef(0, -0.6F, 0);
             GL11.glRotatef(Sys.getTime() / 90F % 360F, 0F, 1F, 0F);
         }
+        
         FMLClientHandler.instance().getClient().renderEngine.bindTexture("/micdoodle8/mods/galacticraft/core/client/entities/spaceship1.png");
         this.modelSpaceship.render(this.spaceship, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F);
         GL11.glPopMatrix();
@@ -82,7 +98,6 @@ public class GCCoreItemRendererSpaceship implements IItemRenderer
                 FMLClientHandler.instance().getClient().renderEngine.bindTexture("/item/chest.png");
 
                 GL11.glPushMatrix();
-                // GL11.glEnable(GL12.GL_RESCALE_NORMAL);
                 GL11.glDisable(GL11.GL_DEPTH_TEST);
                 GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
                 GL11.glScalef(0.5F, -0.5F, -0.5F);
@@ -99,55 +114,12 @@ public class GCCoreItemRendererSpaceship implements IItemRenderer
                 modelChest.chestBelow.render(0.0625F);
                 modelChest.chestLid.render(0.0625F);
                 modelChest.chestKnob.render(0.0625F);
-                // GL11.glDisable(GL12.GL_RESCALE_NORMAL);
                 GL11.glEnable(GL11.GL_DEPTH_TEST);
                 GL11.glPopMatrix();
                 GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
             }
-            // else if (item.getItemDamage() == 2)
-            // {
-            // GL11.glDisable(GL12.GL_RESCALE_NORMAL);
-            // RenderHelper.disableStandardItemLighting();
-            // GL11.glDisable(GL11.GL_LIGHTING);
-            // GL11.glDisable(GL11.GL_DEPTH_TEST);
-            //
-            // this.renderItemIntoGUI(render,
-            // FMLClientHandler.instance().getClient().fontRenderer,
-            // FMLClientHandler.instance().getClient().renderEngine, new
-            // ItemStack(GCCoreItems.rocketFuelBucket, 1, 60), 0, 0);
-            //
-            // GL11.glEnable(GL12.GL_RESCALE_NORMAL);
-            // RenderHelper.enableStandardItemLighting();
-            // GL11.glEnable(GL11.GL_LIGHTING);
-            // GL11.glEnable(GL11.GL_DEPTH_TEST);
-            // }
         }
     }
-
-    // public void renderItemIntoGUI(RenderBlocks renderBlocks, FontRenderer
-    // par1FontRenderer, RenderEngine par2RenderEngine, ItemStack par3ItemStack,
-    // int par4, int par5)
-    // {
-    // // GL11.glEnable(GL12.GL_RESCALE_NORMAL);
-    // // GL11.glDisable(GL11.GL_DEPTH_TEST);
-    // GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-    // short short1 = 0;
-    //
-    // float f1 = 0;
-    // float f2;
-    //
-    // f1 = 1.0F - f1;
-    // f1 = 1.0F - f1 * f1 * f1;
-    //
-    // // Icon icon =
-    // par2RenderEngine.func_96448_c(par3ItemStack.bindTexture());
-    //
-    // // this.drawItems.zLevel = 300.0F;
-    // // this.drawItems.bindTexture(100, 100, icon, 16, 16);
-    // // GL11.glDisable(GL12.GL_RESCALE_NORMAL);
-    // // GL11.glEnable(GL11.GL_DEPTH_TEST);
-    // GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-    // }
 
     /** IItemRenderer implementation **/
 
@@ -159,6 +131,8 @@ public class GCCoreItemRendererSpaceship implements IItemRenderer
         case ENTITY:
             return true;
         case EQUIPPED:
+            return true;
+        case EQUIPPED_FIRST_PERSON:
             return true;
         case INVENTORY:
             return true;
@@ -179,13 +153,16 @@ public class GCCoreItemRendererSpaceship implements IItemRenderer
         switch (type)
         {
         case EQUIPPED:
-            this.renderPipeItem(type, (RenderBlocks) data[0], item, -0.5f, -0.5f, -0.5f);
+            this.renderSpaceship(type, (RenderBlocks) data[0], item, -0.5f, -0.5f, -0.5f);
+            break;
+        case EQUIPPED_FIRST_PERSON:
+            this.renderSpaceship(type, (RenderBlocks) data[0], item, -0.5f, -0.5f, -0.5f);
             break;
         case INVENTORY:
-            this.renderPipeItem(type, (RenderBlocks) data[0], item, -0.5f, -0.5f, -0.5f);
+            this.renderSpaceship(type, (RenderBlocks) data[0], item, -0.5f, -0.5f, -0.5f);
             break;
         case ENTITY:
-            this.renderPipeItem(type, (RenderBlocks) data[0], item, -0.5f, -0.5f, -0.5f);
+            this.renderSpaceship(type, (RenderBlocks) data[0], item, -0.5f, -0.5f, -0.5f);
             break;
         default:
         }
