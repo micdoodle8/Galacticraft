1f66839e5b80db3e570847ac9f0552e4b66147cc
micdoodle8 <mitchpetrie29@gmail.com>
2013-06-24 14:02:23 -0400

    Update Basic Components, fixes backwards compatibility.

--- common/micdoodle8/mods/galacticraft/core/GalacticraftCore.java
+++ common/micdoodle8/mods/galacticraft/core/GalacticraftCore.java
@@ -118,7 +118,7 @@ import cpw.mods.fml.relauncher.Side;
 @NetworkMod(channels = { GalacticraftCore.CHANNEL }, clientSideRequired = true, serverSideRequired = false, connectionHandler = GCCoreConnectionHandler.class, packetHandler = GCCorePacketManager.class)
 public class GalacticraftCore
 {
-    public static final String NAME = "Galacticraft Core";
+    public static final String NAME = "Galacticraft Core"; 
     public static final String MODID = "GalacticraftCore";
     public static final String CHANNEL = "GalacticraftCore";
     public static final String CHANNELENTITIES = "GCCoreEntities";
