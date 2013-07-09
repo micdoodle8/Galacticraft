ccdbe48edf373395048702415b64187348f79b22
micdoodle8 <mitchpetrie29@gmail.com>
2013-07-08 20:02:47 -0400

    Show an error when people don't read instructions.

--- common/micdoodle8/mods/galacticraft/core/GCCoreThreadRequirementMissing.java
+++ common/micdoodle8/mods/galacticraft/core/GCCoreThreadRequirementMissing.java
@@ -1,8 +1,11 @@
 package micdoodle8.mods.galacticraft.core;
 
+import java.awt.Dialog.ModalityType;
 import javax.swing.JDialog;
 import javax.swing.JEditorPane;
 import javax.swing.JOptionPane;
+import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
 public class GCCoreThreadRequirementMissing extends Thread
@@ -16,25 +19,55 @@ public class GCCoreThreadRequirementMissing extends Thread
 
     public static void startCheck()
     {
-        final Thread thread = new Thread(GCCoreThreadRequirementMissing.instance);
+        Thread thread = new Thread(GCCoreThreadRequirementMissing.instance);
         thread.start();
+    }
+
+    @Override
+    public void start()
+    {
+        
     }
 
     @Override
     public void run()
     {
-        if (!GCCoreConfigManager.forceLoadGC.getBoolean(false) && !GCCoreConfigManager.loadBC.getBoolean(false) && !GCCoreCompatibilityManager.isIc2Loaded() && !GCCoreCompatibilityManager.isTELoaded())
+        JEditorPane ep = null;
+        JOptionPane pane = null;
+        
+        if (!Loader.isModLoaded("Micdoodlecore"))
+        {
+            final String err = "<strong><p>MicdoodleCore not found in mods folder. Galacticraft will not load.</p></strong>";
+            System.out.println(err);
+            ep = new JEditorPane("text/html", "<html>" + err + "</html>");
+
+            ep.setEditable(false);
+            ep.setOpaque(false);
+            
+            pane = new JOptionPane(ep, JOptionPane.ERROR_MESSAGE, JOptionPane.YES_NO_OPTION, null, new Object[] { LanguageRegistry.instance().getStringLocalization("message.button3.name") }, null);
+            final JDialog dialog = pane.createDialog(null, "Missing Dependancy");
+            dialog.setVisible(true);
+            dialog.requestFocusInWindow();
+            final Object selectedValue = pane.getValue();
+
+            if (selectedValue != null)
+            {
+                System.exit(-1);
+            }
+        }
+        else if (!GCCoreConfigManager.forceLoadGC.getBoolean(false) && !GCCoreConfigManager.loadBC.getBoolean(false) && !GCCoreCompatibilityManager.isIc2Loaded() && !GCCoreCompatibilityManager.isTELoaded())
         {
             final String err = "<strong><h1>" + LanguageRegistry.instance().getStringLocalization("message.warning1.name") + "</h1></strong><br /><h3>" + LanguageRegistry.instance().getStringLocalization("message.warning2.name") + "</h3>";
             System.out.println(err);
-            final JEditorPane ep = new JEditorPane("text/html", "<html>" + err + "</html>");
+            ep = new JEditorPane("text/html", "<html>" + err + "</html>");
 
             ep.setEditable(false);
             ep.setOpaque(false);
 
-            final JOptionPane pane = new JOptionPane(ep, JOptionPane.ERROR_MESSAGE, JOptionPane.YES_NO_OPTION, null, new Object[] { LanguageRegistry.instance().getStringLocalization("message.button1.name"), LanguageRegistry.instance().getStringLocalization("message.button2.name"), LanguageRegistry.instance().getStringLocalization("message.button3.name"), LanguageRegistry.instance().getStringLocalization("message.button4.name") }, null);
+            pane = new JOptionPane(ep, JOptionPane.ERROR_MESSAGE, JOptionPane.YES_NO_OPTION, null, new Object[] { LanguageRegistry.instance().getStringLocalization("message.button1.name"), LanguageRegistry.instance().getStringLocalization("message.button2.name"), LanguageRegistry.instance().getStringLocalization("message.button3.name"), LanguageRegistry.instance().getStringLocalization("message.button4.name") }, null);
             final JDialog dialog = pane.createDialog(null, "Missing Dependancy");
             dialog.setVisible(true);
+            dialog.requestFocusInWindow();
             final Object selectedValue = pane.getValue();
 
             if (selectedValue != null && selectedValue.equals(LanguageRegistry.instance().getStringLocalization("message.button3.name")))
