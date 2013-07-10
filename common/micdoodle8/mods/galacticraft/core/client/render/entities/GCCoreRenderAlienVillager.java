2a71397bb8c225b04a6279799d40930ac9ccec34
micdoodle8 <mitchpetrie29@gmail.com>
2013-07-10 00:00:58 -0400

    Fix crash upon rendering alien villager.

--- common/micdoodle8/mods/galacticraft/core/client/render/entities/GCCoreRenderAlienVillager.java
+++ common/micdoodle8/mods/galacticraft/core/client/render/entities/GCCoreRenderAlienVillager.java
@@ -2,12 +2,12 @@ package micdoodle8.mods.galacticraft.core.client.render.entities;
 
 import micdoodle8.mods.galacticraft.core.GalacticraftCore;
 import micdoodle8.mods.galacticraft.core.client.model.GCCoreModelVillager;
+import micdoodle8.mods.galacticraft.core.entities.GCCoreEntityAlienVillager;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.resources.ResourceLocation;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.passive.EntityVillager;
 import org.lwjgl.opengl.GL11;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -25,27 +25,27 @@ public class GCCoreRenderAlienVillager extends RenderLiving
         this.villagerModel = (GCCoreModelVillager) this.mainModel;
     }
 
-    protected int shouldVillagerRenderPass(EntityVillager par1EntityVillager, int par2, float par3)
+    protected int shouldVillagerRenderPass(GCCoreEntityAlienVillager par1EntityVillager, int par2, float par3)
     {
         return -1;
     }
 
-    public void renderVillager(EntityVillager par1EntityVillager, double par2, double par4, double par6, float par8, float par9)
+    public void renderVillager(GCCoreEntityAlienVillager par1EntityVillager, double par2, double par4, double par6, float par8, float par9)
     {
         super.doRenderLiving(par1EntityVillager, par2, par4, par6, par8, par9);
     }
 
-    protected ResourceLocation func_110902_a(EntityVillager par1EntityVillager)
+    protected ResourceLocation func_110902_a(GCCoreEntityAlienVillager par1EntityVillager)
     {
         return GCCoreRenderAlienVillager.villagerTexture;
     }
 
-    protected void renderVillagerEquipedItems(EntityVillager par1EntityVillager, float par2)
+    protected void renderVillagerEquipedItems(GCCoreEntityAlienVillager par1EntityVillager, float par2)
     {
         super.renderEquippedItems(par1EntityVillager, par2);
     }
 
-    protected void preRenderVillager(EntityVillager par1EntityVillager, float par2)
+    protected void preRenderVillager(GCCoreEntityAlienVillager par1EntityVillager, float par2)
     {
         float f1 = 0.9375F;
 
@@ -65,42 +65,42 @@ public class GCCoreRenderAlienVillager extends RenderLiving
     @Override
     public void doRenderLiving(EntityLiving par1EntityLiving, double par2, double par4, double par6, float par8, float par9)
     {
-        this.renderVillager((EntityVillager) par1EntityLiving, par2, par4, par6, par8, par9);
+        this.renderVillager((GCCoreEntityAlienVillager) par1EntityLiving, par2, par4, par6, par8, par9);
     }
 
     @Override
     protected void preRenderCallback(EntityLivingBase par1EntityLivingBase, float par2)
     {
-        this.preRenderVillager((EntityVillager) par1EntityLivingBase, par2);
+        this.preRenderVillager((GCCoreEntityAlienVillager) par1EntityLivingBase, par2);
     }
 
     @Override
     protected int shouldRenderPass(EntityLivingBase par1EntityLivingBase, int par2, float par3)
     {
-        return this.shouldVillagerRenderPass((EntityVillager) par1EntityLivingBase, par2, par3);
+        return this.shouldVillagerRenderPass((GCCoreEntityAlienVillager) par1EntityLivingBase, par2, par3);
     }
 
     @Override
     protected void renderEquippedItems(EntityLivingBase par1EntityLivingBase, float par2)
     {
-        this.renderVillagerEquipedItems((EntityVillager) par1EntityLivingBase, par2);
+        this.renderVillagerEquipedItems((GCCoreEntityAlienVillager) par1EntityLivingBase, par2);
     }
 
     @Override
     public void renderPlayer(EntityLivingBase par1EntityLivingBase, double par2, double par4, double par6, float par8, float par9)
     {
-        this.renderVillager((EntityVillager) par1EntityLivingBase, par2, par4, par6, par8, par9);
+        this.renderVillager((GCCoreEntityAlienVillager) par1EntityLivingBase, par2, par4, par6, par8, par9);
     }
 
     @Override
     protected ResourceLocation func_110775_a(Entity par1Entity)
     {
-        return this.func_110902_a((EntityVillager) par1Entity);
+        return this.func_110902_a((GCCoreEntityAlienVillager) par1Entity);
     }
 
     @Override
     public void doRender(Entity par1Entity, double par2, double par4, double par6, float par8, float par9)
     {
-        this.renderVillager((EntityVillager) par1Entity, par2, par4, par6, par8, par9);
+        this.renderVillager((GCCoreEntityAlienVillager) par1Entity, par2, par4, par6, par8, par9);
     }
 }
