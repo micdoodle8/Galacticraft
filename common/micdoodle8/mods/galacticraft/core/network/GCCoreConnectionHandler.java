f0a785b08e6a009ee4c9874319fe38a11032550f
micdoodle8 <mitchpetrie29@gmail.com>
2013-07-31 18:28:27 -0400

    Fix a severe issue causing crash and player to be teleported back to overworld after leaving the game and rejoining if on the moon.

--- common/micdoodle8/mods/galacticraft/core/network/GCCoreConnectionHandler.java
+++ common/micdoodle8/mods/galacticraft/core/network/GCCoreConnectionHandler.java
@@ -1,6 +1,5 @@
 package micdoodle8.mods.galacticraft.core.network;
 
-import java.util.ArrayList;
 import micdoodle8.mods.galacticraft.core.GalacticraftCore;
 import micdoodle8.mods.galacticraft.core.entities.GCCorePlayerMP;
 import micdoodle8.mods.galacticraft.core.util.PacketUtil;
@@ -16,7 +15,7 @@ import cpw.mods.fml.common.network.Player;
 
 public class GCCoreConnectionHandler implements IConnectionHandler
 {
-    private static ArrayList<INetworkManager> managers = new ArrayList<INetworkManager>();
+    private static boolean clientConnected = false;
 
     @Override
     public void playerLoggedIn(Player player, NetHandler netHandler, INetworkManager manager)
@@ -40,6 +39,7 @@ public class GCCoreConnectionHandler implements IConnectionHandler
     @Override
     public void connectionOpened(NetHandler netClientHandler, String server, int port, INetworkManager manager)
     {
+        clientConnected = true;
     }
 
     @Override
@@ -50,24 +50,16 @@ public class GCCoreConnectionHandler implements IConnectionHandler
     @Override
     public void connectionClosed(INetworkManager manager)
     {
-        if (GCCoreConnectionHandler.managers.contains(manager))
+        if (clientConnected)
         {
-            GCCoreConnectionHandler.managers.remove(manager);
-
-            if (GCCoreConnectionHandler.managers.size() == 0)
-            {
-                WorldUtil.unregisterPlanets();
-                WorldUtil.unregisterSpaceStations();
-            }
+            clientConnected = false;
+            WorldUtil.unregisterPlanets();
+            WorldUtil.unregisterSpaceStations();
         }
     }
 
     @Override
     public void clientLoggedIn(NetHandler clientHandler, INetworkManager manager, Packet1Login login)
     {
-        if (!GCCoreConnectionHandler.managers.contains(manager))
-        {
-            GCCoreConnectionHandler.managers.add(manager);
-        }
     }
 }
