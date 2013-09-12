7d43484bab11e520be2a500083bf5cc337d2c89f
micdoodle8 <mitchpetrie29@gmail.com>
2013-09-12 14:47:46 -0400

    Fix compatibility issue with old versions of forge.

--- common/micdoodle8/mods/galacticraft/core/event/GCCoreLandingPadRemovalEvent.java
+++ common/micdoodle8/mods/galacticraft/core/event/GCCoreLandingPadRemovalEvent.java
@@ -1,15 +1,21 @@
 package micdoodle8.mods.galacticraft.core.event;
 
-import net.minecraft.block.Block;
 import net.minecraft.world.World;
-import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.event.Event;
 
-public class GCCoreLandingPadRemovalEvent extends BlockEvent
+public class GCCoreLandingPadRemovalEvent extends Event
 {
     public boolean allow = true;
+    public final int x;
+    public final int y; 
+    public final int z;
+    public final World world;
     
     public GCCoreLandingPadRemovalEvent(World world, int x, int y, int z)
     {
-        super(x, y, z, world, Block.blocksList[world.getBlockId(x, y, z)], world.getBlockMetadata(x, y, z));
+        this.world = world;
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 }
