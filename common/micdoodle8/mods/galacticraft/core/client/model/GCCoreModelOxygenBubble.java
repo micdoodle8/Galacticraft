87871c61f0f1a8f1f98e8d250796df5f81dfb29c
micdoodle8 <mitchpetrie29@gmail.com>
2013-09-25 13:06:00 -0400

    Add sphere obj file to replace NMT sphere.

--- common/micdoodle8/mods/galacticraft/core/client/model/GCCoreModelOxygenBubble.java
+++ common/micdoodle8/mods/galacticraft/core/client/model/GCCoreModelOxygenBubble.java
@@ -3,11 +3,15 @@ package micdoodle8.mods.galacticraft.core.client.model;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraftforge.client.model.AdvancedModelLoader;
+import net.minecraftforge.client.model.IModelCustom;
 
 public class GCCoreModelOxygenBubble extends ModelBase
 {
 //    public NMTModelRenderer bubble;
 //    public NMTModelRenderer bubble2;
+    
+    IModelCustom model;
 
     public GCCoreModelOxygenBubble()
     {
@@ -16,8 +20,9 @@ public class GCCoreModelOxygenBubble extends ModelBase
 
     public GCCoreModelOxygenBubble(float par1)
     {
-//        this.textureWidth = 512 * 4;
-//        this.textureHeight = 512 * 32;
+        this.textureWidth = 512 * 4;
+        this.textureHeight = 512 * 32;
+        this.model = AdvancedModelLoader.loadModel("/assets/galacticraftcore/models/sphere.obj");
 //        this.bubble = new NMTModelRenderer(this, 40, 20);
 //        this.bubble.addSphere(0.0F, 0.0F, 0.0F, 1.0F, 12, 12, this.textureWidth, this.textureHeight, true, NMTGlobal.NMT_SPHERE_SINUSOIDAL);
 //        this.bubble.setRotationPoint(0F, 0F, 0F);
@@ -42,6 +47,7 @@ public class GCCoreModelOxygenBubble extends ModelBase
     public void render(Entity par1Entity, float par2, float par3, float par4, float par5, float par6, float par7)
     {
         this.setRotationAngles(par2, par3, par4, par5, par6, par7, par1Entity);
+        this.model.renderAll();
 //        this.bubble.render(par7);
 //        this.bubble2.render(par7);
     }
