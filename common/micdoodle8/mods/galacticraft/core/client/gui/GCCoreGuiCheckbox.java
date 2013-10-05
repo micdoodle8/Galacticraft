005b4d14ae94d9a24e57baaecb22048181492b70
micdoodle8 <mitchpetrie29@gmail.com>
2013-10-05 02:04:25 -0400

    Added text box element and updated dropdown & check box.

--- common/micdoodle8/mods/galacticraft/core/client/gui/GCCoreGuiCheckbox.java
+++ common/micdoodle8/mods/galacticraft/core/client/gui/GCCoreGuiCheckbox.java
@@ -3,30 +3,40 @@ package micdoodle8.mods.galacticraft.core.client.gui;
 import micdoodle8.mods.galacticraft.core.GalacticraftCore;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
+import cpw.mods.fml.common.FMLLog;
 
 public class GCCoreGuiCheckbox extends GuiButton
 {
     protected static final ResourceLocation texture = new ResourceLocation(GalacticraftCore.TEXTURE_DOMAIN, "textures/gui/gui.png");
     
-    public boolean isSelected;
+    public Boolean isSelected;
     
-    public GCCoreGuiCheckbox(int id, int x, int y, boolean selected, String text)
+    private ICheckBoxCallback parentGui;
+    
+    public GCCoreGuiCheckbox(int id, ICheckBoxCallback parentGui, int x, int y, String text)
     {
         super(id, x, y, 13, 13, text);
-        this.isSelected = selected;
+        this.parentGui = parentGui;
     }
 
     @Override
     public void drawButton(Minecraft par1Minecraft, int par2, int par3)
     {
+        if (this.isSelected == null)
+        {
+            this.isSelected = this.parentGui.getInitiallySelected(this);
+            FMLLog.info("" + this.isSelected);
+        }
+        
         if (this.drawButton)
         {
             par1Minecraft.getTextureManager().bindTexture(texture);
             GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
             this.field_82253_i = par2 >= this.xPosition && par3 >= this.yPosition && par2 < this.xPosition + this.width && par3 < this.yPosition + this.height;
-            this.drawTexturedModalRect(this.xPosition, this.yPosition, this.isSelected ? 20 : 33, field_82253_i ? 24 : 37, this.width, this.height);
+            this.drawTexturedModalRect(this.xPosition, this.yPosition, this.isSelected ? 33 : 20, field_82253_i ? 24 : 37, this.width, this.height);
             this.mouseDragged(par1Minecraft, par2, par3);
             par1Minecraft.fontRenderer.drawString(this.displayString, this.xPosition + this.width + 3, this.yPosition + (this.height - 6) / 2, 4210752, false);
         }
@@ -35,12 +45,22 @@ public class GCCoreGuiCheckbox extends GuiButton
     @Override
     public boolean mousePressed(Minecraft par1Minecraft, int par2, int par3)
     {
-        if (this.enabled && this.drawButton && par2 >= this.xPosition && par3 >= this.yPosition && par2 < this.xPosition + this.width && par3 < this.yPosition + this.height)
+        if (this.parentGui.canPlayerEdit(this, par1Minecraft.thePlayer) && this.enabled && this.drawButton && par2 >= this.xPosition && par3 >= this.yPosition && par2 < this.xPosition + this.width && par3 < this.yPosition + this.height)
         {
             this.isSelected = !this.isSelected;
+            this.parentGui.onSelectionChanged(this, this.isSelected);
             return true;
         }
         
         return false;
+    }
+    
+    public static interface ICheckBoxCallback
+    {
+        public void onSelectionChanged(GCCoreGuiCheckbox checkbox, boolean newSelected);
+        
+        public boolean canPlayerEdit(GCCoreGuiCheckbox checkbox, EntityPlayer player);
+        
+        public boolean getInitiallySelected(GCCoreGuiCheckbox checkbox);
     }
 }
--- common/micdoodle8/mods/galacticraft/core/client/gui/GCCoreGuiDropdown.java
+++ common/micdoodle8/mods/galacticraft/core/client/gui/GCCoreGuiDropdown.java
@@ -6,10 +6,10 @@ import micdoodle8.mods.galacticraft.core.util.GCCoreUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiButton;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
 import cpw.mods.fml.client.FMLClientHandler;
-import cpw.mods.fml.common.FMLLog;
 
 public class GCCoreGuiDropdown extends GuiButton
 {
@@ -17,13 +17,15 @@ public class GCCoreGuiDropdown extends GuiButton
     
     public boolean dropdownClicked;
     public String[] optionStrings;
-    public int selectedOption;
+    public int selectedOption = -1;
     public GCCoreSmallFontRenderer font;
+    private IDropboxCallback parentClass;
     
-    public GCCoreGuiDropdown(int id, int x, int y, String... text)
+    public GCCoreGuiDropdown(int id, IDropboxCallback parentClass, int x, int y, String... text)
     {
         super(id, x, y, 13, 13, "");
         Minecraft mc = FMLClientHandler.instance().getClient();
+        this.parentClass = parentClass;
         this.font = new GCCoreSmallFontRenderer(mc.gameSettings, new ResourceLocation("textures/font/ascii.png"), mc.renderEngine, false);
         this.optionStrings = text;
         
@@ -31,22 +33,27 @@ public class GCCoreGuiDropdown extends GuiButton
         
         for (int i = 0; i < text.length; i++)
         {
-            largestString = Math.max(largestString, mc.fontRenderer.getStringWidth(text[i]));
+            largestString = Math.max(largestString, this.font.getStringWidth(text[i]));
         }
         
-        this.width = largestString + 4;
-        this.selectedOption = 0;
+        this.width = largestString + 8;
     }
 
     @Override
     public void drawButton(Minecraft par1Minecraft, int par2, int par3)
     {
+        if (this.selectedOption == -1)
+        {
+            this.selectedOption = parentClass.getInitialSelection(this);
+        }
+        
         if (this.drawButton)
         {
+            GL11.glPushMatrix();
+            GL11.glTranslatef(0, 0, 200);
             GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
             this.field_82253_i = par2 >= this.xPosition && par3 >= this.yPosition && par2 < this.xPosition + this.width && par3 < this.yPosition + this.height;
-//            this.drawTexturedModalRect(this.xPosition, this.yPosition, this.isSelected ? 20 : 33, field_82253_i ? 24 : 37, this.width, this.height);
-            Gui.drawRect(this.xPosition, this.yPosition, this.xPosition + this.width, this.yPosition + (this.dropdownClicked ? this.height * this.optionStrings.length : this.height), GCCoreUtil.convertTo32BitColor(255, 255, 255, 255));
+            Gui.drawRect(this.xPosition, this.yPosition, this.xPosition + this.width, this.yPosition + (this.dropdownClicked ? this.height * this.optionStrings.length : this.height), GCCoreUtil.convertTo32BitColor(255, 200, 200, 200));
             Gui.drawRect(this.xPosition + 1, this.yPosition + 1, this.xPosition + this.width - 1, this.yPosition + (this.dropdownClicked ? this.height * this.optionStrings.length : this.height) - 1, GCCoreUtil.convertTo32BitColor(255, 0, 0, 0));
             
             if (this.dropdownClicked && par2 >= this.xPosition && par3 >= this.yPosition && par2 < this.xPosition + this.width && par3 < this.yPosition + this.height * this.optionStrings.length)
@@ -61,7 +68,6 @@ public class GCCoreGuiDropdown extends GuiButton
             {
                 for (int i = 0; i < this.optionStrings.length; i++)
                 {
-//                    this.drawCenteredString(par1Minecraft.fontRenderer, this.optionStrings[i], this.xPosition + this.width / 2, (this.yPosition + (this.height - 8) / 2) + this.height * i, GCCoreUtil.convertTo32BitColor(255, 255, 255, 255));
                     this.font.drawStringWithShadow(this.optionStrings[i], this.xPosition + this.width / 2 - this.font.getStringWidth(this.optionStrings[i]) / 2, (this.yPosition + (this.height - 8) / 2) + this.height * i, GCCoreUtil.convertTo32BitColor(255, 255, 255, 255));
                 }
             }
@@ -69,35 +75,49 @@ public class GCCoreGuiDropdown extends GuiButton
             {
                 this.font.drawStringWithShadow(this.optionStrings[this.selectedOption], this.xPosition + this.width / 2 - this.font.getStringWidth(this.optionStrings[this.selectedOption]) / 2, this.yPosition + (this.height - 8) / 2, GCCoreUtil.convertTo32BitColor(255, 255, 255, 255));
             }
+            GL11.glPopMatrix();
         }
     }
 
     @Override
     public boolean mousePressed(Minecraft par1Minecraft, int par2, int par3)
     {
-        if (!this.dropdownClicked)
+        if (this.parentClass.canBeClickedBy(this, par1Minecraft.thePlayer))
         {
-            if (this.enabled && this.drawButton && par2 >= this.xPosition && par3 >= this.yPosition && par2 < this.xPosition + this.width && par3 < this.yPosition + this.height)
+            if (!this.dropdownClicked)
             {
-                this.dropdownClicked = true;
-                return true;
-            }
-        }
-        else
-        {
-            if (this.enabled && this.drawButton && par2 >= this.xPosition && par3 >= this.yPosition && par2 < this.xPosition + this.width && par3 < this.yPosition + this.height * this.optionStrings.length)
-            {
-                int optionClicked = (par3 - this.yPosition) / this.height;
-                this.selectedOption = optionClicked % this.optionStrings.length;
-                this.dropdownClicked = false;
-                return true;
+                if (this.enabled && this.drawButton && par2 >= this.xPosition && par3 >= this.yPosition && par2 < this.xPosition + this.width && par3 < this.yPosition + this.height)
+                {
+                    this.dropdownClicked = true;
+                    return true;
+                }
             }
             else
             {
-                this.dropdownClicked = false;
+                if (this.enabled && this.drawButton && par2 >= this.xPosition && par3 >= this.yPosition && par2 < this.xPosition + this.width && par3 < this.yPosition + this.height * this.optionStrings.length)
+                {
+                    int optionClicked = (par3 - this.yPosition) / this.height;
+                    this.selectedOption = optionClicked % this.optionStrings.length;
+                    this.dropdownClicked = false;
+                    this.parentClass.onSelectionChanged(this, this.selectedOption);
+                    return true;
+                }
+                else
+                {
+                    this.dropdownClicked = false;
+                }
             }
         }
         
         return false;
+    }
+    
+    public static interface IDropboxCallback
+    {
+        public boolean canBeClickedBy(GCCoreGuiDropdown dropdown, EntityPlayer player);
+        
+        public void onSelectionChanged(GCCoreGuiDropdown dropdown, int selection);
+        
+        public int getInitialSelection(GCCoreGuiDropdown dropdown);
     }
 }
