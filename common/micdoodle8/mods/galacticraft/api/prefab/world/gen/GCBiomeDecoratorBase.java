9f009ccb42e409314353ea8add925d92f0e44ae4
micdoodle8 <mitchpetrie29@gmail.com>
2013-10-09 01:19:14 -0400

    Begin work on some prefabs! Updated API info and added info about using prefabs.

---
+++ common/micdoodle8/mods/galacticraft/api/prefab/world/gen/GCBiomeDecoratorBase.java
@@ -0,0 +1,55 @@
+package micdoodle8.mods.galacticraft.api.prefab.world.gen;
+
+import java.util.Random;
+import micdoodle8.mods.galacticraft.api.event.wgen.GCCoreEventPopulate;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.feature.WorldGenerator;
+import net.minecraftforge.common.MinecraftForge;
+
+/**
+ * Copyright 2012-2013, micdoodle8
+ * 
+ * All rights reserved.
+ * 
+ */
+public abstract class GCBiomeDecoratorBase
+{
+    protected World currentWorld;
+    protected Random rand;
+
+    protected int chunkX;
+    protected int chunkZ;
+
+    public void decorate(World world, Random random, int chunkX, int chunkZ)
+    {
+        if (this.currentWorld != null)
+        {
+            throw new RuntimeException("Already decorating!!");
+        }
+        else
+        {
+            this.currentWorld = world;
+            this.rand = random;
+            this.chunkX = chunkX;
+            this.chunkZ = chunkZ;
+            MinecraftForge.EVENT_BUS.post(new GCCoreEventPopulate.Pre(this.currentWorld, this.rand, this.chunkX, this.chunkZ));
+            this.decorate();
+            MinecraftForge.EVENT_BUS.post(new GCCoreEventPopulate.Post(this.currentWorld, this.rand, this.chunkX, this.chunkZ));
+            this.currentWorld = null;
+            this.rand = null;
+        }
+    }
+
+    protected void generateOre(int amountPerChunk, WorldGenerator worldGenerator, int minY, int maxY)
+    {
+        for (int var5 = 0; var5 < amountPerChunk; ++var5)
+        {
+            final int var6 = this.chunkX + this.rand.nextInt(16);
+            final int var7 = this.rand.nextInt(maxY - minY) + minY;
+            final int var8 = this.chunkZ + this.rand.nextInt(16);
+            worldGenerator.generate(this.currentWorld, this.rand, var6, var7, var8);
+        }
+    }
+
+    protected abstract void decorate();
+}
