970ba91df7a165bd8cd3c36e01e8d1280a75838d
micdoodle8 <mitchpetrie29@gmail.com>
2013-10-12 11:24:00 -0400

    Send chat message explaining the space station invite command.

--- common/micdoodle8/mods/galacticraft/core/dimension/GCCoreSpaceTeleportType.java
+++ common/micdoodle8/mods/galacticraft/core/dimension/GCCoreSpaceTeleportType.java
@@ -1,10 +1,13 @@
 package micdoodle8.mods.galacticraft.core.dimension;
 
 import java.util.Random;
+import mekanism.api.EnumColor;
 import micdoodle8.mods.galacticraft.api.vector.Vector.Vector3;
 import micdoodle8.mods.galacticraft.api.world.ITeleportType;
+import micdoodle8.mods.galacticraft.core.GCCoreConfigManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.ChatMessageComponent;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 
@@ -37,5 +40,9 @@ public class GCCoreSpaceTeleportType implements ITeleportType
     @Override
     public void onSpaceDimensionChanged(World newWorld, EntityPlayerMP player)
     {
+        if (GCCoreConfigManager.spaceStationsRequirePermission && !newWorld.isRemote)
+        {
+            player.sendChatToPlayer(ChatMessageComponent.createFromText(EnumColor.YELLOW + "Type " + EnumColor.AQUA + "/ssinvite <playername> " + EnumColor.YELLOW + "to allow another player to enter this space station!"));
+        }
     }
 }
