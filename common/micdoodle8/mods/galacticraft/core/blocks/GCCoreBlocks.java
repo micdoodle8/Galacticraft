9d27ed85eeaa7032578c8b8e15f73054fa33beee
micdoodle8 <mitchpetrie29@gmail.com>
2013-10-19 00:58:50 -0400

    Adjust ore harvest levels, you can now mine tin with stone pickaxe, silicon and aluminum with iron.

--- common/micdoodle8/mods/galacticraft/core/blocks/GCCoreBlocks.java
+++ common/micdoodle8/mods/galacticraft/core/blocks/GCCoreBlocks.java
@@ -158,9 +158,9 @@ public class GCCoreBlocks
         MinecraftForge.setBlockHarvestLevel(GCCoreBlocks.decorationBlocks, 2, "pickaxe", 3);
         MinecraftForge.setBlockHarvestLevel(GCCoreBlocks.fallenMeteor, "pickaxe", 3);
         MinecraftForge.setBlockHarvestLevel(GCCoreBlocks.decorationBlocks, 5, "pickaxe", 1);
-        MinecraftForge.setBlockHarvestLevel(GCCoreBlocks.decorationBlocks, 6, "pickaxe", 2);
-        MinecraftForge.setBlockHarvestLevel(GCCoreBlocks.decorationBlocks, 7, "pickaxe", 3);
-        MinecraftForge.setBlockHarvestLevel(GCCoreBlocks.decorationBlocks, 8, "pickaxe", 3);
+        MinecraftForge.setBlockHarvestLevel(GCCoreBlocks.decorationBlocks, 6, "pickaxe", 1);
+        MinecraftForge.setBlockHarvestLevel(GCCoreBlocks.decorationBlocks, 7, "pickaxe", 2);
+        MinecraftForge.setBlockHarvestLevel(GCCoreBlocks.decorationBlocks, 8, "pickaxe", 2);
     }
 
     public static void registerBlocks()
