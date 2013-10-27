a14b1693c5127cc0810582ef102cb472cac776f4
micdoodle8 <mitchpetrie29@gmail.com>
2013-10-27 02:06:25 -0400

    Bump version number.

--- common/micdoodle8/mods/galacticraft/core/GalacticraftCore.java
+++ common/micdoodle8/mods/galacticraft/core/GalacticraftCore.java
@@ -142,7 +142,7 @@ public class GalacticraftCore
 
     public static final int LOCALMAJVERSION = 2;
     public static final int LOCALMINVERSION = 0;
-    public static final int LOCALBUILDVERSION = 3;
+    public static final int LOCALBUILDVERSION = 4;
     public static int remoteMajVer;
     public static int remoteMinVer;
     public static int remoteBuildVer;
