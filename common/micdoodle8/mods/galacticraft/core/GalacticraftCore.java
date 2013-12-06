37c512921b8cbbfd50c96e3943a3681fbfb15a1f
micdoodle8 <mitchpetrie29@gmail.com>
2013-12-06 13:36:43 -0500

    Set minecraft version back to 1.6.2 and bump mod version to 2.0.8

--- common/micdoodle8/mods/galacticraft/core/GalacticraftCore.java
+++ common/micdoodle8/mods/galacticraft/core/GalacticraftCore.java
@@ -146,7 +146,7 @@ public class GalacticraftCore
 
     public static final int LOCALMAJVERSION = 2;
     public static final int LOCALMINVERSION = 0;
-    public static final int LOCALBUILDVERSION = 7;
+    public static final int LOCALBUILDVERSION = 8;
     public static int remoteMajVer;
     public static int remoteMinVer;
     public static int remoteBuildVer;
