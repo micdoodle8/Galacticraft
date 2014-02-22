613e4990adfd40fd5f993e9f64752a81d1b0f2a3
Mitchell Petrie <mitchpetrie29@gmail.com>
2014-02-22 01:39:21 -0500

    Update version number.

--- common/micdoodle8/mods/galacticraft/core/GalacticraftCore.java
+++ common/micdoodle8/mods/galacticraft/core/GalacticraftCore.java
@@ -147,7 +147,7 @@ public class GalacticraftCore
 
 	public static final int LOCALMAJVERSION = 2;
 	public static final int LOCALMINVERSION = 0;
-	public static final int LOCALBUILDVERSION = 9;
+	public static final int LOCALBUILDVERSION = 10;
 	public static int remoteMajVer;
 	public static int remoteMinVer;
 	public static int remoteBuildVer;
