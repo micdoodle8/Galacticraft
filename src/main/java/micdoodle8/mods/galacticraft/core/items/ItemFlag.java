32f9466ea610f8a0d0ac1a3c0cc3534cd3207cb7
Mitchell Petrie <mitchpetrie29@gmail.com>
2014-05-03 04:06:14 -0400

    Combine 16 flag items into the single one that's required.

--- src/main/java/micdoodle8/mods/galacticraft/core/items/ItemFlag.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/items/ItemFlag.java
@@ -32,43 +32,15 @@ import cpw.mods.fml.relauncher.SideOnly;
  */
 public class ItemFlag extends Item implements IHoldableItem
 {
-	public static final String[] names = { "american", // 0
-	"black", // 1
-	"blue", // 2
-	"green", // 3
-	"brown", // 4
-	"darkblue", // 5
-	"darkgray", // 6
-	"darkgreen", // 7
-	"gray", // 8
-	"magenta", // 9
-	"orange", // 10
-	"pink", // 11
-	"purple", // 12
-	"red", // 13
-	"teal", // 14
-	"yellow", // 15
-	"white" }; // 16
 	public int placeProgress;
 
 	public ItemFlag(String assetName)
 	{
 		super();
 		this.setMaxDamage(0);
-		this.setHasSubtypes(true);
 		this.setMaxStackSize(1);
 		this.setUnlocalizedName(assetName);
 		this.setTextureName("arrow");
-	}
-
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	@Override
-	public void getSubItems(Item par1, CreativeTabs par2CreativeTabs, List par3List)
-	{
-		for (int i = 0; i < 17; i++)
-		{
-			par3List.add(new ItemStack(par1, 1, i));
-		}
 	}
 
 	@Override
@@ -146,12 +118,6 @@ public class ItemFlag extends Item implements IHoldableItem
 	}
 
 	@Override
-	public int getMetadata(int par1)
-	{
-		return par1;
-	}
-
-	@Override
 	public ItemStack onEaten(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
 	{
 		return par1ItemStack;
@@ -187,54 +153,13 @@ public class ItemFlag extends Item implements IHoldableItem
 	@Override
 	public String getUnlocalizedName(ItemStack itemStack)
 	{
-		return "item.flag." + ItemFlag.names[itemStack.getItemDamage()];
+		return "item.flag";
 	}
 
 	@Override
 	public IIcon getIconFromDamage(int damage)
 	{
 		return super.getIconFromDamage(damage);
-	}
-
-	public static int getFlagDamageValueFromDye(int meta)
-	{
-		switch (meta)
-		{
-		case 0:
-			return 1;
-		case 1:
-			return 13;
-		case 2:
-			return 7;
-		case 3:
-			return 4;
-		case 4:
-			return 5;
-		case 5:
-			return 12;
-		case 6:
-			return 14;
-		case 7:
-			return 8;
-		case 8:
-			return 6;
-		case 9:
-			return 11;
-		case 10:
-			return 3;
-		case 11:
-			return 15;
-		case 12:
-			return 2;
-		case 13:
-			return 9;
-		case 14:
-			return 10;
-		case 15:
-			return 16;
-		}
-
-		return -1;
 	}
 
 	@Override
