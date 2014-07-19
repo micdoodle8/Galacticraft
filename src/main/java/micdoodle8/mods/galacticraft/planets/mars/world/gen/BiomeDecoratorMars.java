41f1975ec71b6891b74f013cfaefef3d6fcaf94a
radfast <conradlondon@gmail.com>
2014-07-19 10:39:38 +0100

    Add sub-surface ice on Mars.

--- src/main/java/micdoodle8/mods/galacticraft/planets/mars/world/gen/BiomeDecoratorMars.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/mars/world/gen/BiomeDecoratorMars.java
@@ -3,6 +3,7 @@ package micdoodle8.mods.galacticraft.planets.mars.world.gen;
 import micdoodle8.mods.galacticraft.api.prefab.world.gen.BiomeDecoratorSpace;
 import micdoodle8.mods.galacticraft.core.world.gen.WorldGenMinableMeta;
 import micdoodle8.mods.galacticraft.planets.mars.blocks.MarsBlocks;
+import net.minecraft.init.Blocks;
 import net.minecraft.world.gen.feature.WorldGenerator;
 
 public class BiomeDecoratorMars extends BiomeDecoratorSpace
@@ -12,6 +13,7 @@ public class BiomeDecoratorMars extends BiomeDecoratorSpace
 	private WorldGenerator tinGen;
 	private WorldGenerator copperGen;
 	private WorldGenerator ironGen;
+	private WorldGenerator iceGen;
 
 	public BiomeDecoratorMars()
 	{
@@ -20,12 +22,14 @@ public class BiomeDecoratorMars extends BiomeDecoratorSpace
 		this.deshGen = new WorldGenMinableMeta(MarsBlocks.marsBlock, 6, 2, true, MarsBlocks.marsBlock, 9);
 		this.ironGen = new WorldGenMinableMeta(MarsBlocks.marsBlock, 8, 3, true, MarsBlocks.marsBlock, 9);
 		this.dirtGen = new WorldGenMinableMeta(MarsBlocks.marsBlock, 32, 6, true, MarsBlocks.marsBlock, 9);
+		this.iceGen = new WorldGenMinableMeta(Blocks.ice, 18, 0, true, MarsBlocks.marsBlock, 6);
 	}
 
     @Override
     protected void decorate()
     {
-        this.generateOre(20, this.dirtGen, 0, 200);
+        this.generateOre(12, this.dirtGen, 0, 120);
+        this.generateOre(8, this.iceGen, 64, 96);
         this.generateOre(15, this.deshGen, 20, 64);
         this.generateOre(26, this.copperGen, 0, 60);
         this.generateOre(23, this.tinGen, 0, 60);
