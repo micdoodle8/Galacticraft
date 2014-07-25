f4c212c15ee295512a370cc1ed38219862891b87
radfast <conradlondon@gmail.com>
2014-07-25 16:34:08 +0100

    Add icon for Carbon Fragments.

--- src/main/java/micdoodle8/mods/galacticraft/planets/mars/blocks/BlockMachineMarsT2.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/mars/blocks/BlockMachineMarsT2.java
@@ -252,9 +252,9 @@ public class BlockMachineMarsT2 extends BlockTileGC
 
 				for (float i = -0.41F + 0.16F * rand.nextFloat(); i < 0.5F; i+= 0.167F)
 				{
-					if (rand.nextInt(3) == 0) GalacticraftCore.proxy.spawnParticle("whiteSmokeTiny", new Vector3(x + i, y, z - 0.422F), new Vector3(0.0D, -0.015D, -0.0015D));
+					if (rand.nextInt(3) == 0) GalacticraftCore.proxy.spawnParticle("whiteSmokeTiny", new Vector3(x + i, y, z - 0.417F), new Vector3(0.0D, -0.015D, -0.0015D));
 					if (rand.nextInt(3) == 0) GalacticraftCore.proxy.spawnParticle("whiteSmokeTiny", new Vector3(x + i, y, z + 0.53F), new Vector3(0.0D, -0.015D, 0.0015D));
-					if (rand.nextInt(3) == 0) GalacticraftCore.proxy.spawnParticle("whiteSmokeTiny", new Vector3(x - 0.422F, y, z + i), new Vector3(-0.0015D, -0.015D, 0.0D));
+					if (rand.nextInt(3) == 0) GalacticraftCore.proxy.spawnParticle("whiteSmokeTiny", new Vector3(x - 0.417F, y, z + i), new Vector3(-0.0015D, -0.015D, 0.0D));
 					if (rand.nextInt(3) == 0) GalacticraftCore.proxy.spawnParticle("whiteSmokeTiny", new Vector3(x + 0.53F, y, z + i), new Vector3(0.0015D, -0.015D, 0.0D));
 				}
 			}
--- src/main/java/micdoodle8/mods/galacticraft/planets/mars/items/ItemCarbonFragments.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/mars/items/ItemCarbonFragments.java
@@ -4,6 +4,8 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import micdoodle8.mods.galacticraft.core.GalacticraftCore;
 import micdoodle8.mods.galacticraft.core.proxy.ClientProxyCore;
+import micdoodle8.mods.galacticraft.planets.mars.MarsModule;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
@@ -31,5 +33,12 @@ public class ItemCarbonFragments extends Item
 	public EnumRarity getRarity(ItemStack par1ItemStack)
 	{
 		return ClientProxyCore.galacticraftItem;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IIconRegister iconRegister)
+	{
+		this.itemIcon = iconRegister.registerIcon(MarsModule.TEXTURE_PREFIX + "carbonFragments");
 	}
 }
