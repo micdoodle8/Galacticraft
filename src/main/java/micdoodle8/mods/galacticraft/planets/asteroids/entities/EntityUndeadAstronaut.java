9363b43941fb9aa94a127d588d8f38e535c4f6ad
Mitchell Petrie <mitchpetrie29@gmail.com>
2014-07-25 01:00:17 -0400

    Comment out unneeded class.

--- src/main/java/micdoodle8/mods/galacticraft/planets/asteroids/entities/EntityUndeadAstronaut.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/asteroids/entities/EntityUndeadAstronaut.java
@@ -1,12 +1,12 @@
-package micdoodle8.mods.galacticraft.planets.asteroids.entities;
-
-import net.minecraft.entity.monster.EntityZombie;
-import net.minecraft.world.World;
-
-public class EntityUndeadAstronaut extends EntityZombie
-{
-    public EntityUndeadAstronaut(World world)
-    {
-        super(world);
-    }
-}
+//package micdoodle8.mods.galacticraft.planets.asteroids.entities;
+//
+//import net.minecraft.entity.monster.EntityZombie;
+//import net.minecraft.world.World;
+//
+//public class EntityUndeadAstronaut extends EntityZombie
+//{
+//    public EntityUndeadAstronaut(World world)
+//    {
+//        super(world);
+//    }
+//}
