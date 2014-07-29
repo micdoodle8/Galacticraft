ce866c30148b4133cfa84146a94b365e4330fc50
radfast <conradlondon@gmail.com>
2014-07-29 21:02:31 +0100

    Prepare for Asteroids save data system.

---
+++ src/main/java/micdoodle8/mods/galacticraft/planets/asteroids/dimension/AsteroidSaveData.java
@@ -0,0 +1,29 @@
+package micdoodle8.mods.galacticraft.planets.asteroids.dimension;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.WorldSavedData;
+
+public class AsteroidSaveData extends WorldSavedData
+{
+	public static final String saveDataID = "GCAsteroidData";
+	public NBTTagCompound datacompound;
+	private NBTTagCompound alldata;
+
+	public AsteroidSaveData(String s)
+	{
+		super(AsteroidSaveData.saveDataID);
+		this.datacompound = new NBTTagCompound();
+	}
+
+	@Override
+	public void readFromNBT(NBTTagCompound nbt)
+	{
+		this.datacompound = nbt.getCompoundTag("data");
+	}
+
+	@Override
+	public void writeToNBT(NBTTagCompound nbt)
+	{
+		nbt.setTag("data", this.datacompound);
+	}
+}
