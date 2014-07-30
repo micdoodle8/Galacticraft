606d6a2035c30212db397a6043c6d82d5d04dbb1
radfast <conradlondon@gmail.com>
2014-07-30 18:06:51 +0100

    Asteroid landing location - works with existing worlds as well.

--- src/main/java/micdoodle8/mods/galacticraft/planets/asteroids/dimension/TeleportTypeAsteroids.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/asteroids/dimension/TeleportTypeAsteroids.java
@@ -6,13 +6,17 @@ import micdoodle8.mods.galacticraft.api.vector.Vector3;
 import micdoodle8.mods.galacticraft.api.world.ITeleportType;
 import micdoodle8.mods.galacticraft.core.entities.player.GCEntityPlayerMP;
 import micdoodle8.mods.galacticraft.core.util.ConfigManagerCore;
+import micdoodle8.mods.galacticraft.planets.asteroids.blocks.AsteroidBlocks;
 import micdoodle8.mods.galacticraft.planets.asteroids.entities.EntityEntryPod;
+import micdoodle8.mods.galacticraft.planets.asteroids.world.gen.ChunkProviderAsteroids;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.ChunkProviderServer;
 
 import java.util.Random;
 
@@ -48,53 +52,17 @@ public class TeleportTypeAsteroids implements ITeleportType
 	            
 	            if (bv3 != null)
 	            {
+		            //Check whether the returned asteroid is too far from the desired entry location in which case, give up
+		            if (bv3.distanceSquared(new BlockVec3(x, 128, z)) > 25600) break;
+		            
                     if (ConfigManagerCore.enableDebug) FMLLog.info("Testing asteroid at x" + (bv3.x) + " y" + (bv3.y) + " z" + bv3.z);
 	            	world.theChunkProviderServer.loadChunk(bv3.x >> 4, bv3.z >> 4);
 	
-	                for (int k = 208; k > 48; k--)
-	                {
-	                    if (!world.isAirBlock(bv3.x, k, bv3.z))
-	                    {
-	                    	if (ConfigManagerCore.enableDebug) FMLLog.info("Found asteroid at x" + (bv3.x) + " z" + (bv3.z));
-	                        return new Vector3(bv3.x + 0.5, 310, bv3.z + 0.5);
-	                    }
-	                }
-	                
-	                for (int k = 208; k > 48; k--)
-	                {
-	                    if (!world.isAirBlock(bv3.x + 1, k, bv3.z + 1))
-	                    {
-	                    	if (ConfigManagerCore.enableDebug) FMLLog.info("Found asteroid at x" + (bv3.x + 1) + " z" + (bv3.z + 1));
-	                        return new Vector3(bv3.x + 1.5, 310, bv3.z + 1.5);
-	                    }
-	                }
-	                
-	                for (int k = 208; k > 48; k--)
-	                {
-	                    if (!world.isAirBlock(bv3.x + 1, k, bv3.z - 1))
-	                    {
-	                    	if (ConfigManagerCore.enableDebug) FMLLog.info("Found asteroid at x" + (bv3.x + 1) + " z" + (bv3.z - 1));
-	                        return new Vector3(bv3.x + 1.5, 310, bv3.z - 0.5);
-	                    }
-	                }
-	                
-	                for (int k = 208; k > 48; k--)
-	                {
-	                    if (!world.isAirBlock(bv3.x - 1, k, bv3.z + 1))
-	                    {
-	                    	if (ConfigManagerCore.enableDebug) FMLLog.info("Found asteroid at x" + (bv3.x - 1) + " z" + (bv3.z + 1));
-	                        return new Vector3(bv3.x - 0.5, 310, bv3.z + 1.5);
-	                    }
-	                }
-	                
-	                for (int k = 208; k > 48; k--)
-	                {
-	                    if (!world.isAirBlock(bv3.x - 1, k, bv3.z - 1))
-	                    {
-	                    	if (ConfigManagerCore.enableDebug) FMLLog.info("Found asteroid at x" + (bv3.x - 1) + " z" + (bv3.z - 1));
-	                        return new Vector3(bv3.x - 0.5, 310, bv3.z - 0.5);
-	                    }
-	                }
+	                if (goodAsteroidEntry(world, bv3.x, bv3.y, bv3.z)) return new Vector3(bv3.x, 310, bv3.z);
+	                if (goodAsteroidEntry(world, bv3.x + 2, bv3.y, bv3.z + 2)) return new Vector3(bv3.x + 2, 310, bv3.z + 2);
+	                if (goodAsteroidEntry(world, bv3.x + 2, bv3.y, bv3.z - 2)) return new Vector3(bv3.x + 2, 310, bv3.z - 2);
+	                if (goodAsteroidEntry(world, bv3.x - 2, bv3.y, bv3.z - 2)) return new Vector3(bv3.x - 2, 310, bv3.z - 2);
+	                if (goodAsteroidEntry(world, bv3.x - 2, bv3.y, bv3.z + 2)) return new Vector3(bv3.x - 2, 310, bv3.z + 2);
 	                
 	                //Failed to find an asteroid even though there should be one there
 	                if (ConfigManagerCore.enableDebug) FMLLog.info("Removing drilled out asteroid at x" + (bv3.x) + " z" + (bv3.z));
@@ -112,6 +80,28 @@ public class TeleportTypeAsteroids implements ITeleportType
         return new Vector3(player.posX, 310,  player.posZ);
 	}
 
+	private boolean goodAsteroidEntry(World world, int x, int yorig, int z)
+	{
+    	for (int k = 208; k > 48; k--)
+        {
+            if (!world.isAirBlock(x, k, z))
+            {
+            	if (Math.abs(k - yorig) > 20) continue;
+            	//Clear the downward path of small asteroids and any other asteroid rock
+            	for (int y = k + 1; y < 256; y++)
+            	{
+            		if (world.getBlock(x, y, z) == AsteroidBlocks.blockBasic) world.setBlockToAir(x, y, z);
+            		if (world.getBlock(x - 1, y, z) == AsteroidBlocks.blockBasic) world.setBlockToAir(x - 1, y, z);
+            		if (world.getBlock(x, y, z - 1) == AsteroidBlocks.blockBasic) world.setBlockToAir(x, y, z - 1);
+            		if (world.getBlock(x - 1, y, z - 1) == AsteroidBlocks.blockBasic) world.setBlockToAir(x - 1, y, z - 1);
+            	}
+            	if (ConfigManagerCore.enableDebug) FMLLog.info("Found asteroid at x" + (x) + " z" + (z));
+                return true;
+            }
+        }
+    	return false;
+	}
+	
 	@Override
 	public Vector3 getEntitySpawnLocation(WorldServer world, Entity entity)
 	{
@@ -126,7 +116,7 @@ public class TeleportTypeAsteroids implements ITeleportType
 
 	private void preGenChunks(World w, int cx, int cz)
 	{
-		w.getChunkFromChunkCoords(cx, cz);
+		this.preGenChunk(w, cx, cz);
 		for (int r = 1; r < 3; r++)
 		{
 			int xmin = cx - r;
@@ -135,12 +125,27 @@ public class TeleportTypeAsteroids implements ITeleportType
 			int zmax = cz + r;
 			for (int i = -r; i < r; i++)
 			{
-				w.getChunkFromChunkCoords(xmin, cz + i);
-				w.getChunkFromChunkCoords(xmax, cz - i);
-				w.getChunkFromChunkCoords(cx - i, zmin);
-				w.getChunkFromChunkCoords(cx + i, zmax);
+				this.preGenChunk(w, xmin, cz + i);
+				this.preGenChunk(w, xmax, cz - i);
+				this.preGenChunk(w, cx - i, zmin);
+				this.preGenChunk(w, cx + i, zmax);
 			}
 		}
+	}
+	
+	private void preGenChunk(World w, int chunkX, int chunkZ)
+	{
+		w.getChunkFromChunkCoords(chunkX, chunkZ);
+		
+		//Search for and add large asteroids nearby
+		IChunkProvider cp = ((ChunkProviderServer) w.getChunkProvider()).currentChunkProvider;
+		if (cp instanceof ChunkProviderAsteroids)
+		{
+			System.out.println("Looking for large asteroid at " + chunkX + "," + chunkZ);
+			((ChunkProviderAsteroids)cp).addLargeAsteroids(chunkX, chunkZ);
+		}
+		else
+			System.out.println(cp.getClass().getName());
 	}
 	
 	@Override
