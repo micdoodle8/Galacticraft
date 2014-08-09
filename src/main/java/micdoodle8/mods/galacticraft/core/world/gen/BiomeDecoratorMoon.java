265a24cdd2d0640d4409adcf2693871cd27b2b3f
radfast <conradlondon@gmail.com>
2014-08-09 15:18:49 +0100

    Fix config settings for disable moon oregen not working.  Fixes #818.

--- src/main/java/micdoodle8/mods/galacticraft/core/world/gen/BiomeDecoratorMoon.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/world/gen/BiomeDecoratorMoon.java
@@ -2,6 +2,7 @@ package micdoodle8.mods.galacticraft.core.world.gen;
 
 import micdoodle8.mods.galacticraft.api.event.wgen.GCCoreEventPopulate;
 import micdoodle8.mods.galacticraft.core.blocks.GCBlocks;
+import micdoodle8.mods.galacticraft.core.util.ConfigManagerCore;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.gen.feature.WorldGenerator;
@@ -63,9 +64,9 @@ public class BiomeDecoratorMoon
 	{
 		MinecraftForge.EVENT_BUS.post(new GCCoreEventPopulate.Pre(this.worldObj, this.randomGenerator, this.chunkX, this.chunkZ));
 		this.genStandardOre1(20, this.dirtGen, 0, 200);
-		this.genStandardOre1(26, this.copperGen, 0, 60);
-		this.genStandardOre1(23, this.tinGen, 0, 60);
-		this.genStandardOre1(12, this.cheeseGen, 0, 128);
+		if (!ConfigManagerCore.disableCopperMoon) this.genStandardOre1(26, this.copperGen, 0, 60);
+		if (!ConfigManagerCore.disableTinMoon) this.genStandardOre1(23, this.tinGen, 0, 60);
+		if (!ConfigManagerCore.disableCheeseMoon) this.genStandardOre1(12, this.cheeseGen, 0, 128);
 		MinecraftForge.EVENT_BUS.post(new GCCoreEventPopulate.Post(this.worldObj, this.randomGenerator, this.chunkX, this.chunkZ));
 	}
 }
