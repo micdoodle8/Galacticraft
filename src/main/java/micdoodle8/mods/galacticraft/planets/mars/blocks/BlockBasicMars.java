891d7b8c544576276f26112cd624a752312722bf
SteveKunG <mccommander_minecraft@hotmail.com>
2014-08-11 20:09:28 +0700

    Make desh ore drop exp when mining

--- src/main/java/micdoodle8/mods/galacticraft/planets/mars/blocks/BlockBasicMars.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/mars/blocks/BlockBasicMars.java
@@ -26,6 +26,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -343,5 +344,17 @@ public class BlockBasicMars extends Block implements IDetectableResource, IPlant
 		}
 
 		return super.getPickBlock(target, world, x, y, z);
+	}
+	
+	@Override
+	public void dropBlockAsItemWithChance(World world, int par2, int par3, int par4, int par5, float par6, int par7)
+	{
+		super.dropBlockAsItemWithChance(world, par2, par3, par4, par5, par6, par7);
+
+		if (this.getItemDropped(par5, world.rand, par7) != Item.getItemFromBlock(this))
+		{
+			int var8 =  MathHelper.getRandomIntegerInRange(world.rand, 3, 5);
+			this.dropXpOnBlockBreak(world, par2, par3, par4, var8);
+		}
 	}
 }
