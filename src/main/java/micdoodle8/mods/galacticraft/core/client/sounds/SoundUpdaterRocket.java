2cfef744c4d92a6be2a49a0b756ddcde540b57a1
radfast <conradlondon@gmail.com>
2014-09-07 19:46:33 +0100

    Remove outdated info from 2 files.

--- src/main/java/micdoodle8/mods/galacticraft/core/client/sounds/SoundUpdaterRocket.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/client/sounds/SoundUpdaterRocket.java
@@ -14,8 +14,7 @@ import net.minecraft.util.ResourceLocation;
  * This file is part of the Galacticraft project
  * 
  * @author micdoodle8
- * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
- * 
+ *  
  */
 public class SoundUpdaterRocket extends MovingSound
 {
--- src/main/java/micdoodle8/mods/galacticraft/planets/mars/client/SkyProviderMars.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/mars/client/SkyProviderMars.java
@@ -19,11 +19,11 @@ import java.util.Random;
 
 /**
  * SkyProviderMars.java
- * <p/>
+ * 
  * This file is part of the Galacticraft project
  *
  * @author micdoodle8
- * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ * 
  */
 public class SkyProviderMars extends IRenderHandler
 {
