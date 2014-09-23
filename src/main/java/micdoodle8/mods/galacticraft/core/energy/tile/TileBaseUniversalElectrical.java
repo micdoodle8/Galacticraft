micdoodle8/Galacticraft

commit 48e8b2931a21522c6b79f8660ba3685547f18b51

Fix side-by-side storage-machine energy transfers. Fixes #1028.

src/main/java/micdoodle8/mods/galacticraft/core/energy/tile/TileBaseUniversalElectrical.java
@@ -68,6 +68,26 @@ public abstract class TileBaseUniversalElectrical extends EnergyStorageTile //im
         return EnumSet.noneOf(ForgeDirection.class);
     }
 
+    @Override
+    public float getRequest(ForgeDirection direction)
+    {
+        if (this.getElectricalInputDirections().contains(direction))
+        	return super.getRequest(direction);
+        
+        return 0F;
+    }
+
+    @Override
+    public float receiveElectricity(ForgeDirection from, float receive, int tier, boolean doReceive)
+    {
+        if (this.getElectricalInputDirections().contains(from))
+        {
+        	return super.receiveElectricity(from, receive, tier, doReceive);
+        }
+        
+        return 0F;
+    }
+    
     //	@Override
     //	public float receiveElectricity(ForgeDirection from, ElectricityPack receive, boolean doReceive)
     //	{

src/main/java/micdoodle8/mods/galacticraft/core/energy/tile/TileBaseUniversalElectricalSource.java
@@ -73,8 +73,12 @@ public class TileBaseUniversalElectricalSource extends TileBaseUniversalElectric
                         IElectricityNetwork network = ((IConductor) tileAdj).getNetwork();
                         if (network != null)
                         {
-                            amountProduced += (toSend - network.produce(toSend, true, this.tierGC, this));
+                            amountProduced += (toSend - network.produce(toSend, !simulate, this.tierGC, this));
                         }
+                    }
+                    else if (tileAdj instanceof TileBaseUniversalElectrical)
+                    {
+                  		amountProduced += ((TileBaseUniversalElectrical) tileAdj).receiveElectricity(direction.getOpposite(), toSend, this.tierGC, !simulate);
                     }
                     else
                     {

commit b7ed0589774dacf1dd47082a97b63969032edfc7

Fix battery slot bug introduced in 48e8b29.

src/main/java/micdoodle8/mods/galacticraft/core/energy/tile/TileBaseUniversalElectrical.java
@@ -71,7 +71,7 @@ public abstract class TileBaseUniversalElectrical extends EnergyStorageTile //im
     @Override
     public float getRequest(ForgeDirection direction)
     {
-        if (this.getElectricalInputDirections().contains(direction))
+        if (this.getElectricalInputDirections().contains(direction) || direction == ForgeDirection.UNKNOWN)
         	return super.getRequest(direction);
         
         return 0F;
@@ -80,7 +80,7 @@ public abstract class TileBaseUniversalElectrical extends EnergyStorageTile //im
     @Override
     public float receiveElectricity(ForgeDirection from, float receive, int tier, boolean doReceive)
     {
-        if (this.getElectricalInputDirections().contains(from))
+        if (this.getElectricalInputDirections().contains(from) || from == ForgeDirection.UNKNOWN)
         {
         	return super.receiveElectricity(from, receive, tier, doReceive);
         }