8def40cd632fec1bb5c07ac8b883de49ba3c56a8
Reshetnikov Dmitriy <Nightmare@railfox.ru>
2014-12-02 05:45:51 +0300

    Fix imports

--- src/main/java/micdoodle8/mods/galacticraft/core/event/EventHandlerGC.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/event/EventHandlerGC.java
@@ -64,7 +64,7 @@ import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.biome.BiomeGenDesert;
 import net.minecraftforge.client.event.sound.PlaySoundEvent17;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.living.PlayerDropsEvent;
+import net.minecraftforge.event.entity.player.PlayerDropsEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
