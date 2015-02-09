8f71867c65b72f1004b611a652d235544903a471
SteveKunG <mccommander_minecraft@hotmail.com>
2015-02-09 19:16:11 +0700

    Add rocket tier 2 item picking in creative mode

--- src/main/java/micdoodle8/mods/galacticraft/planets/mars/entities/EntityCargoRocket.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/mars/entities/EntityCargoRocket.java
@@ -19,6 +19,7 @@ import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.MathHelper;
+import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 
@@ -46,6 +47,12 @@ public class EntityCargoRocket extends EntityAutoRocket implements IRocketType, 
     public int getFuelTankCapacity()
     {
         return 2000;
+    }
+    
+    @Override
+    public ItemStack getPickedResult(MovingObjectPosition target)
+    {
+	return new ItemStack(MarsItems.spaceship, 1, this.rocketType.getIndex() + 10);
     }
 
     public float getCargoFilledAmount()
