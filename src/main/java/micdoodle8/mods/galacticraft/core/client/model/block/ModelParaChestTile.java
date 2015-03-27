985077dbc18da2355f15572df830b11c9dbf50d4
Mitchell Petrie <mitchpetrie29@gmail.com>
2015-03-27 00:52:50 -0400

    Fix minor parachest texture issues. Fixes #1384 and #1391.

--- src/main/java/micdoodle8/mods/galacticraft/core/client/model/block/ModelParaChestTile.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/client/model/block/ModelParaChestTile.java
@@ -10,26 +10,26 @@ public class ModelParaChestTile extends ModelChest
     public ModelParaChestTile()
     {
         super();
-        this.keyParts[0] = new ModelRenderer(this, 0, 1);
-        this.keyParts[0].addBox(-1.0F, -6.0F, 0.0F, 1, 3, 1);
+        this.keyParts[0] = new ModelRenderer(this, 60, 61);
+        this.keyParts[0].setTextureSize(64, 64);
+        this.keyParts[0].addBox(-1.0F, -6.0F, 0.0F, 1, 1, 1);
         this.keyParts[0].setRotationPoint(0F, 0F, 0F);
-        this.keyParts[0].setTextureSize(32, 32);
         this.keyParts[0].mirror = true;
         this.keyParts[0].rotationPointX = 7.0F;
         this.keyParts[0].rotationPointY = 7.0F;
         this.keyParts[0].rotationPointZ = 7.5F;
-        this.keyParts[1] = new ModelRenderer(this, 0, 1);
-        this.keyParts[1].addBox(-1.0F, -5.5F, 0.0F, 4, 1, 1);
+        this.keyParts[1] = new ModelRenderer(this, 53, 57);
+        this.keyParts[1].setTextureSize(64, 64);
+        this.keyParts[1].addBox(-1.0F, -6.0F, 0.0F, 4, 1, 1);
         this.keyParts[1].setRotationPoint(0F, 0F, 0F);
-        this.keyParts[1].setTextureSize(32, 32);
         this.keyParts[1].mirror = true;
         this.keyParts[1].rotationPointX = 7.0F;
         this.keyParts[1].rotationPointY = 6.0F;
         this.keyParts[1].rotationPointZ = 7.5F;
-        this.keyParts[2] = new ModelRenderer(this, 0, 1);
-        this.keyParts[2].addBox(-1.0F, -6.0F, 0.0F, 1, 3, 1);
+        this.keyParts[2] = new ModelRenderer(this, 60, 61);
+        this.keyParts[2].setTextureSize(64, 64);
+        this.keyParts[2].addBox(-1.0F, -6.0F, 0.0F, 1, 1, 1);
         this.keyParts[2].setRotationPoint(0F, 0F, 0F);
-        this.keyParts[2].setTextureSize(32, 32);
         this.keyParts[2].mirror = true;
         this.keyParts[2].rotationPointX = 10.0F;
         this.keyParts[2].rotationPointY = 7.0F;
