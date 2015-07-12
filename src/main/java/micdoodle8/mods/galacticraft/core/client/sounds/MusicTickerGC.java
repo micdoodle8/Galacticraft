b304fd10cb8dd64162104ff9f7194c19ceb4a1d3
SteveKunG <mccommander_minecraft@hotmail.com>
2015-07-12 16:12:07 +0700

    Fix music ticker
    Some private fields need to using access transformer.

--- src/main/java/micdoodle8/mods/galacticraft/core/client/sounds/MusicTickerGC.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/client/sounds/MusicTickerGC.java
@@ -1,50 +1,50 @@
-//package micdoodle8.mods.galacticraft.core.client.sounds;
-//
-//import net.minecraftforge.fml.client.FMLClientHandler;
-//import micdoodle8.mods.galacticraft.api.world.IGalacticraftWorldProvider;
-//import micdoodle8.mods.galacticraft.core.proxy.ClientProxyCore;
-//import net.minecraft.client.Minecraft;
-//import net.minecraft.client.audio.MusicTicker;
-//import net.minecraft.client.audio.PositionedSoundRecord;
-//import net.minecraft.util.MathHelper;
-//
-//public class MusicTickerGC extends MusicTicker
-//{
-//    public MusicTickerGC(Minecraft mc)
-//    {
-//        super(mc);
-//    }
-//
-//    @Override
-//    public void update()
-//    {
-//        MusicTicker.MusicType musictype = this.field_147677_b.func_147109_W();
-//
-//        if (FMLClientHandler.instance().getWorldClient() != null && FMLClientHandler.instance().getWorldClient().provider instanceof IGalacticraftWorldProvider)
-//        {
-//            musictype = ClientProxyCore.MUSIC_TYPE_MARS;
-//        }
-//
-//        if (this.field_147678_c != null)
-//        {
-//            if (!musictype.getMusicTickerLocation().equals(this.field_147678_c.getPositionedSoundLocation()))
-//            {
-//                this.field_147677_b.getSoundHandler().stopSound(this.field_147678_c);
-//                this.field_147676_d = MathHelper.getRandomIntegerInRange(this.field_147679_a, 0, musictype.func_148634_b() / 2);
-//            }
-//
-//            if (!this.field_147677_b.getSoundHandler().isSoundPlaying(this.field_147678_c))
-//            {
-//                this.field_147678_c = null;
-//                this.field_147676_d = Math.min(MathHelper.getRandomIntegerInRange(this.field_147679_a, musictype.func_148634_b(), musictype.func_148633_c()), this.field_147676_d);
-//            }
-//        }
-//
-//        if (this.field_147678_c == null && this.field_147676_d-- <= 0)
-//        {
-//            this.field_147678_c = PositionedSoundRecord.func_147673_a(musictype.getMusicTickerLocation());
-//            this.field_147677_b.getSoundHandler().playSound(this.field_147678_c);
-//            this.field_147676_d = Integer.MAX_VALUE;
-//        }
-//    }
-//}
+package micdoodle8.mods.galacticraft.core.client.sounds;
+
+import micdoodle8.mods.galacticraft.api.world.IGalacticraftWorldProvider;
+import micdoodle8.mods.galacticraft.core.proxy.ClientProxyCore;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.audio.MusicTicker;
+import net.minecraft.client.audio.PositionedSoundRecord;
+import net.minecraft.util.MathHelper;
+import net.minecraftforge.fml.client.FMLClientHandler;
+
+public class MusicTickerGC extends MusicTicker
+{
+	public MusicTickerGC(Minecraft mc)
+	{
+		super(mc);
+	}
+
+	@Override
+	public void update()
+	{
+		MusicTicker.MusicType musictype = this.mc.getAmbientMusicType();
+
+		if (FMLClientHandler.instance().getWorldClient() != null && FMLClientHandler.instance().getWorldClient().provider instanceof IGalacticraftWorldProvider)
+		{
+			musictype = ClientProxyCore.MUSIC_TYPE_MARS;
+		}
+
+		if (this.currentMusic != null)
+		{
+			if (!musictype.getMusicLocation().equals(this.currentMusic.getSoundLocation()))
+			{
+				this.mc.getSoundHandler().stopSound(this.currentMusic);
+				this.timeUntilNextMusic = MathHelper.getRandomIntegerInRange(this.rand, 0, musictype.getMinDelay() / 2);
+			}
+
+			if (!this.mc.getSoundHandler().isSoundPlaying(this.currentMusic))
+			{
+				this.currentMusic = null;
+				this.timeUntilNextMusic = Math.min(MathHelper.getRandomIntegerInRange(this.rand, musictype.getMinDelay(), musictype.getMaxDelay()), this.timeUntilNextMusic);
+			}
+		}
+
+		if (this.currentMusic == null && this.timeUntilNextMusic-- <= 0)
+		{
+			this.currentMusic = PositionedSoundRecord.create(musictype.getMusicLocation());
+			this.mc.getSoundHandler().playSound(this.currentMusic);
+			this.timeUntilNextMusic = Integer.MAX_VALUE;
+		}
+	}
+}
