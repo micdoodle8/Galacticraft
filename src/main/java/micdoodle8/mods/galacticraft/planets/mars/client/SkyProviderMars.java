30d7bde93f8bf095b4717f30e20e52cc8216ea5d
Mitch <mitchpetrie29@gmail.com>
2015-10-22 11:27:22 -0400

    Attempt to fix blending issue...

--- src/main/java/micdoodle8/mods/galacticraft/planets/mars/client/SkyProviderMars.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/mars/client/SkyProviderMars.java
@@ -312,6 +312,7 @@ public class SkyProviderMars extends IRenderHandler
         GL11.glEnable(GL12.GL_RESCALE_NORMAL);
         GL11.glEnable(GL11.GL_COLOR_MATERIAL);
         GL11.glDisable(GL11.GL_FOG);
+        OpenGlHelper.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ZERO);
         GL11.glDepthMask(true);
     }
 
