micdoodle8/Galacticraft

commit 4e74f1949e4a849f1f3054ecbea10f6190c3ff5d

Performance: reduce memory usage of over time with multiple Oxygen Sealer setups.

src/main/java/micdoodle8/mods/galacticraft/core/oxygen/ThreadFindSeal.java
@@ -350,6 +350,19 @@ public class ThreadFindSeal
             GCLog.info("   Looped through: " + this.checked.size() + " blocks");
         }
 
+        //Help the Garbage Collector
+        this.sealers.clear();
+        this.checked.clear();
+        this.sealersAround.clear();
+        this.currentLayer.clear();
+        this.airToReplace.clear();
+        this.breatheableToReplace.clear();
+        this.airToReplaceBright.clear();
+        this.breatheableToReplaceBright.clear();
+        this.ambientThermalTracked.clear();
+        this.otherSealers.clear();
+        this.torchesToUpdate.clear();
+
         this.sealedFinal.set(this.sealed);
     }
 

commit 1a526c635faf1339576143b3eab44a24c11c0435

Fix NPE crash introduced in 4e74f19. See #1803, thank you @SirWilli.

src/main/java/micdoodle8/mods/galacticraft/core/oxygen/ThreadFindSeal.java
@@ -351,17 +351,16 @@ public class ThreadFindSeal
         }
 
         //Help the Garbage Collector
-        this.sealers.clear();
         this.checked.clear();
         this.sealersAround.clear();
         this.currentLayer.clear();
         this.airToReplace.clear();
-        this.breatheableToReplace.clear();
         this.airToReplaceBright.clear();
-        this.breatheableToReplaceBright.clear();
-        this.ambientThermalTracked.clear();
-        this.otherSealers.clear();
         this.torchesToUpdate.clear();
+        this.ambientThermalTracked.clear();
+        if (this.breatheableToReplace != null) this.breatheableToReplace.clear();
+        if (this.breatheableToReplaceBright != null) this.breatheableToReplaceBright.clear();
+        if (this.otherSealers != null) this.otherSealers.clear();
 
         this.sealedFinal.set(this.sealed);
     }