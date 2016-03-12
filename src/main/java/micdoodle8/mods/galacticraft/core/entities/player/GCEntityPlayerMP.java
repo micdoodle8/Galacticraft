930eac93e0cb30d19092e6d7964e21b45648cdac
radfast <rradfast@gmail.com>
2016-03-12 18:50:14 +0000

    Fix #2141. Build 446 broke travel between planets if server and client not set to same language.

--- src/main/java/micdoodle8/mods/galacticraft/core/entities/player/GCEntityPlayerMP.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/entities/player/GCEntityPlayerMP.java
@@ -26,7 +26,7 @@ public class GCEntityPlayerMP extends EntityPlayerMP
         super(server, WorldUtil.getStartWorld(world), profile, itemInWorldManager);
         if (this.worldObj != world)
         {
-        	GCPlayerStats.get(this).startAdventure(this.worldObj.provider.getDimensionName());
+        	GCPlayerStats.get(this).startAdventure(WorldUtil.getDimensionName(this.worldObj.provider));
         }
     }
     
