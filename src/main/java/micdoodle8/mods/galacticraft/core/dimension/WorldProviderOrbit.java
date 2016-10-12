040d004999c7c3b471edf857585ed051f839ac74
Mitch <mitchpetrie29@gmail.com>
2016-10-12 13:29:17 -0400

    Fix crash after placing spin thrusters.

--- src/main/java/micdoodle8/mods/galacticraft/core/dimension/WorldProviderOrbit.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/dimension/WorldProviderOrbit.java
@@ -637,7 +637,7 @@ public class WorldProviderOrbit extends WorldProviderSpace implements IOrbitDime
 
                 //Check for block collisions here - if so move the player appropriately
                 //First check that there are no existing collisions where the player is now (TODO: bounce the player away)
-                if (this.worldObj.getCollidingBoundingBoxes(p, p.getCollisionBoundingBox()).size() == 0)
+                if (this.worldObj.getCollidingBoundingBoxes(p, p.getEntityBoundingBox()).size() == 0)
                 {
                     //Now check for collisions in the new direction and if there are some, try reducing the movement
                     int collisions = 0;
