8a27a8daf4270ce8716e3910f81a014585a315df
SteveKunG <mccommander_minecraft@hotmail.com>
2016-12-04 23:23:11 +0700

    Fix meteor does not fall

--- src/main/java/micdoodle8/mods/galacticraft/core/blocks/BlockFallenMeteor.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/blocks/BlockFallenMeteor.java
@@ -35,7 +35,6 @@ public class BlockFallenMeteor extends Block implements ITileEntityProvider, ISh
         this.setBlockBounds(0.2F, 0.2F, 0.2F, 0.8F, 0.8F, 0.8F);
         this.setHardness(50.0F);
         this.setStepSound(Block.soundTypeStone);
-        //this.setBlockTextureName(GalacticraftCore.TEXTURE_PREFIX + assetName);
         this.setUnlocalizedName(assetName);
     }
 
@@ -45,24 +44,12 @@ public class BlockFallenMeteor extends Block implements ITileEntityProvider, ISh
         return GalacticraftCore.galacticraftBlocksTab;
     }
 
-    /*@Override
-    public void registerBlockIcons(IIconRegister par1IconRegister)
-    {
-        this.blockIcon = par1IconRegister.registerIcon(GalacticraftCore.TEXTURE_PREFIX + "fallen_meteor");
-    }*/
-
-    @Override
-    public int getRenderType()
-    {
-        return GalacticraftCore.proxy.getBlockRender(this);
-    }
-
     @Override
     public boolean isOpaqueCube()
     {
         return false;
     }
-    
+
     @Override
     public boolean isFullCube()
     {
@@ -141,47 +128,43 @@ public class BlockFallenMeteor extends Block implements ITileEntityProvider, ISh
     {
         if (!worldIn.isRemote)
         {
-            this.tryToFall(worldIn, pos);
+            this.tryToFall(worldIn, pos, state);
         }
     }
 
-    private void tryToFall(World worldIn, BlockPos pos)
+    private void tryToFall(World world, BlockPos pos, IBlockState state)
     {
-        float yPos = pos.getY() - 1;
-        if (BlockFallenMeteor.canFallBelow(worldIn, pos.offset(EnumFacing.DOWN)) && pos.getY() >= 0)
+        if (this.canFallBelow(world, pos.down()) && pos.getY() >= 0)
         {
-            int prevHeatLevel = ((TileEntityFallenMeteor) worldIn.getTileEntity(pos)).getHeatLevel();
-            worldIn.setBlockToAir(pos);
+            int prevHeatLevel = ((TileEntityFallenMeteor) world.getTileEntity(pos)).getHeatLevel();
+            world.setBlockState(pos, Blocks.air.getDefaultState(), 3);
+            BlockPos blockpos1;
 
-            while (BlockFallenMeteor.canFallBelow(worldIn, new BlockPos(pos.getX(), yPos, pos.getZ())) && yPos > 0)
-            {
-                --yPos;
-            }
+            for (blockpos1 = pos.down(); this.canFallBelow(world, blockpos1) && blockpos1.getY() > 0; blockpos1 = blockpos1.down()) {}
 
-            if (yPos > 0)
+            if (blockpos1.getY() >= 0)
             {
-                worldIn.setBlockState(pos, this.getDefaultState(), 3);
-                ((TileEntityFallenMeteor) worldIn.getTileEntity(pos)).setHeatLevel(prevHeatLevel);
+                world.setBlockState(blockpos1.up(), state, 3);
+                ((TileEntityFallenMeteor) world.getTileEntity(blockpos1.up())).setHeatLevel(prevHeatLevel);
             }
         }
     }
 
-    public static boolean canFallBelow(World worldIn, BlockPos pos)
+    private boolean canFallBelow(World world, BlockPos pos)
     {
-        final Block var4 = worldIn.getBlockState(pos).getBlock();
+        Block block = world.getBlockState(pos).getBlock();
 
-        if (var4.getMaterial() == Material.air)
+        if (block.getMaterial() == Material.air)
         {
             return true;
         }
-        else if (var4 == Blocks.fire)
+        else if (block == Blocks.fire)
         {
             return true;
         }
         else
         {
-            final Material var5 = var4.getMaterial();
-            return var5 == Material.water || var5 == Material.lava;
+            return block.getMaterial() == Material.water ? true : block.getMaterial() == Material.lava;
         }
     }
 
