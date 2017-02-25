0e7058933a610dc9b7b04f083005f40b36e57a13
SteveKunG <mccommander_minecraft@hotmail.com>
2017-02-26 01:37:52 +0700

    Fix cryogenic block placement

--- src/main/java/micdoodle8/mods/galacticraft/planets/mars/items/ItemBlockMachine.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/mars/items/ItemBlockMachine.java
@@ -3,13 +3,22 @@ package micdoodle8.mods.galacticraft.planets.mars.items;
 import micdoodle8.mods.galacticraft.api.item.IHoldableItem;
 import micdoodle8.mods.galacticraft.core.items.ItemBlockDesc;
 import micdoodle8.mods.galacticraft.core.proxy.ClientProxyCore;
+import micdoodle8.mods.galacticraft.core.util.GCCoreUtil;
 import micdoodle8.mods.galacticraft.planets.mars.blocks.BlockMachineMars;
 import micdoodle8.mods.galacticraft.planets.mars.blocks.BlockMachineMarsT2;
 import micdoodle8.mods.galacticraft.planets.mars.blocks.MarsBlocks;
 import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatStyle;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -26,6 +35,29 @@ public class ItemBlockMachine extends ItemBlockDesc implements IHoldableItem
     public int getMetadata(int damage)
     {
         return damage;
+    }
+
+    @Override
+    public boolean placeBlockAt(ItemStack itemStack, EntityPlayer player, World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, IBlockState state)
+    {
+        for (int y = 0; y < 3; y++)
+        {
+            Block blockAt = world.getBlockState(pos.add(0, y, 0)).getBlock();
+            int metaAt = itemStack.getItemDamage();
+
+            if (this.getBlock() == MarsBlocks.machine)
+            {
+                if (!blockAt.getMaterial().isReplaceable() && metaAt >= BlockMachineMars.CRYOGENIC_CHAMBER_METADATA && metaAt < BlockMachineMars.LAUNCH_CONTROLLER_METADATA)
+                {
+                    if (world.isRemote)
+                    {
+                        FMLClientHandler.instance().getClient().ingameGUI.setRecordPlaying(new ChatComponentText(GCCoreUtil.translate("gui.warning.noroom")).setChatStyle(new ChatStyle().setColor(EnumChatFormatting.RED)).getFormattedText(), false);
+                    }
+                    return false;
+                }
+            }
+        }
+        return super.placeBlockAt(itemStack, player, world, pos, facing, hitX, hitY, hitZ, state);
     }
 
     @Override
