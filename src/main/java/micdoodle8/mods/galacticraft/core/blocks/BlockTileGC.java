886112e2c2653e3c5e80e53dfe37dafc717d1543
SteveKunG <mccommander_minecraft@hotmail.com>
2017-02-27 00:10:09 +0700

    Fix crashed probe don't drop items

--- src/main/java/micdoodle8/mods/galacticraft/core/blocks/BlockTileGC.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/blocks/BlockTileGC.java
@@ -3,15 +3,11 @@ package micdoodle8.mods.galacticraft.core.blocks;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.item.EntityItem;
 import net.minecraft.inventory.IInventory;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
-
-import java.util.Random;
 
 /**
  * An advanced block class that is to be extended for wrenching capabilities.
@@ -31,7 +27,7 @@ public abstract class BlockTileGC extends BlockAdvanced implements ITileEntityPr
     @Override
     public void breakBlock(World worldIn, BlockPos pos, IBlockState state)
     {
-        this.dropEntireInventory(worldIn, pos, state);
+        InventoryHelper.dropInventoryItems(worldIn, pos, (IInventory) worldIn.getTileEntity(pos));
         super.breakBlock(worldIn, pos, state);
     }
 
@@ -46,60 +42,6 @@ public abstract class BlockTileGC extends BlockAdvanced implements ITileEntityPr
         super.onBlockEventReceived(worldIn, pos, state, eventID, eventParam);
         TileEntity tileentity = worldIn.getTileEntity(pos);
         return tileentity != null && tileentity.receiveClientEvent(eventID, eventParam);
-    }
-
-    /**
-     * Override this if you don't need it. This will eject all items out of this
-     * machine if it has an inventory.
-     */
-    public void dropEntireInventory(World worldIn, BlockPos pos, IBlockState state)
-    {
-        TileEntity tileEntity = worldIn.getTileEntity(pos);
-
-        if (tileEntity != null)
-        {
-            if (tileEntity instanceof IInventory)
-            {
-                IInventory inventory = (IInventory) tileEntity;
-
-                for (int var6 = 0; var6 < inventory.getSizeInventory(); ++var6)
-                {
-                    ItemStack var7 = inventory.getStackInSlot(var6);
-
-                    if (var7 != null)
-                    {
-                        Random random = new Random();
-                        float var8 = random.nextFloat() * 0.8F + 0.1F;
-                        float var9 = random.nextFloat() * 0.8F + 0.1F;
-                        float var10 = random.nextFloat() * 0.8F + 0.1F;
-
-                        while (var7.stackSize > 0)
-                        {
-                            int var11 = random.nextInt(21) + 10;
-
-                            if (var11 > var7.stackSize)
-                            {
-                                var11 = var7.stackSize;
-                            }
-
-                            var7.stackSize -= var11;
-                            EntityItem var12 = new EntityItem(worldIn, pos.getX() + var8, pos.getY() + var9, pos.getZ() + var10, new ItemStack(var7.getItem(), var11, var7.getItemDamage()));
-
-                            if (var7.hasTagCompound())
-                            {
-                                var12.getEntityItem().setTagCompound((NBTTagCompound) var7.getTagCompound().copy());
-                            }
-
-                            float var13 = 0.05F;
-                            var12.motionX = (float) random.nextGaussian() * var13;
-                            var12.motionY = (float) random.nextGaussian() * var13 + 0.2F;
-                            var12.motionZ = (float) random.nextGaussian() * var13;
-                            worldIn.spawnEntityInWorld(var12);
-                        }
-                    }
-                }
-            }
-        }
     }
 
     /**
