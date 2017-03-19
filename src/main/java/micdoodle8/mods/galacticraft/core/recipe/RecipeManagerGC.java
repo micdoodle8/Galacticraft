42b51bba8fde6a67f6696720845422ed8dba7ac7
radfast <rradfast@gmail.com>
2017-03-19 00:44:24 +0000

    Allow Space Stations to be created even if server and client configs have different values for idDimensionOverworld. Fixes #2242

--- src/main/java/micdoodle8/mods/galacticraft/core/recipe/RecipeManagerGC.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/recipe/RecipeManagerGC.java
@@ -249,7 +249,7 @@ public class RecipeManagerGC
         inputMap.put(aluminumIngots, 16);
         inputMap.put("waferAdvanced", 1);
         inputMap.put(Items.iron_ingot, 24);
-        GalacticraftRegistry.registerSpaceStation(new SpaceStationType(ConfigManagerCore.idDimensionOverworldOrbit, ConfigManagerCore.idDimensionOverworld, new SpaceStationRecipe(inputMap)));
+        GalacticraftRegistry.registerSpaceStation(new SpaceStationType(ConfigManagerCore.idDimensionOverworldOrbit, 0, new SpaceStationRecipe(inputMap)));
 
         RecipeUtil.addRecipe(new ItemStack(GCBlocks.aluminumWire, 6), new Object[] { "WWW", "CCC", "WWW", 'W', Blocks.wool, 'C', "ingotAluminum" });
 
