50a89306b562e84ae0bf67086cc715d09f380108
Galacticraft developer <radfast@users.noreply.github.com>
2017-04-24 11:59:24 +0100

    Add Minecraft version requirements to 1.7.

--- src/main/java/micdoodle8/mods/galacticraft/planets/GalacticraftPlanets.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/GalacticraftPlanets.java
@@ -36,7 +36,7 @@ import java.util.Map;
 
 import powercrystals.minefactoryreloaded.api.FactoryRegistry;
 
-@Mod(name = GalacticraftPlanets.NAME, version = Constants.LOCALMAJVERSION + "." + Constants.LOCALMINVERSION + "." + Constants.LOCALBUILDVERSION, useMetadata = true, modid = Constants.MOD_ID_PLANETS, dependencies = "required-after:" + Constants.MOD_ID_CORE + ";", guiFactory = "micdoodle8.mods.galacticraft.planets.ConfigGuiFactoryPlanets")
+@Mod(modid = Constants.MOD_ID_PLANETS, name = GalacticraftPlanets.NAME, version = Constants.LOCALMAJVERSION + "." + Constants.LOCALMINVERSION + "." + Constants.LOCALBUILDVERSION, acceptedMinecraftVersions = "[1.7.2],[1.7.10]", useMetadata = true, dependencies = "required-after:" + Constants.MOD_ID_CORE + ";", guiFactory = "micdoodle8.mods.galacticraft.planets.ConfigGuiFactoryPlanets")
 public class GalacticraftPlanets
 {
     public static final String NAME = "Galacticraft Planets";
