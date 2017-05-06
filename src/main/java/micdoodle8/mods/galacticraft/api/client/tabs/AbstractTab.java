micdoodle8/Galacticraft

commit 51dd0cb53f260e1846dbf97b3dadad4fe1779482

Update inventory tab to use GLStateManager - thanks to @Noppes.

src/main/java/micdoodle8/mods/galacticraft/api/client/tabs/AbstractTab.java
@@ -2,13 +2,12 @@ package micdoodle8.mods.galacticraft.api.client.tabs;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.client.FMLClientHandler;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL12;
 
 public abstract class AbstractTab extends GuiButton
 {
@@ -35,7 +34,7 @@ public abstract class AbstractTab extends GuiButton
 		}
 		if (this.visible)
 		{
-			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		    GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
 
 			int yTexPos = this.enabled ? 3 : 32;
 			int ySize = this.enabled ? 25 : 32;
@@ -48,12 +47,12 @@ public abstract class AbstractTab extends GuiButton
 			RenderHelper.enableGUIStandardItemLighting();
 			this.zLevel = 100.0F;
             this.itemRender.zLevel = 100.0F;
-			GL11.glEnable(GL11.GL_LIGHTING);
-			GL11.glEnable(GL12.GL_RESCALE_NORMAL);
+            GlStateManager.enableLighting();
+            GlStateManager.enableRescaleNormal();
             this.itemRender.renderItemAndEffectIntoGUI(this.renderStack, this.xPosition + 6, this.yPosition + 8);
             this.itemRender.renderItemOverlayIntoGUI(mc.fontRendererObj, this.renderStack, this.xPosition + 6, this.yPosition + 8, null);
-			GL11.glDisable(GL11.GL_LIGHTING);
-            GL11.glEnable(GL11.GL_BLEND);
+            GlStateManager.disableLighting();
+            GlStateManager.disableBlend();
             this.itemRender.zLevel = 0.0F;
 			this.zLevel = 0.0F;
 			RenderHelper.disableStandardItemLighting();

commit 8e3a3eacf2be5547c54ad9b7fbc19142bb63e41b

Undo GLState error introduced in 51dd0cb.

src/main/java/micdoodle8/mods/galacticraft/api/client/tabs/AbstractTab.java
@@ -52,7 +52,7 @@ public abstract class AbstractTab extends GuiButton
             this.itemRender.renderItemAndEffectIntoGUI(this.renderStack, this.xPosition + 6, this.yPosition + 8);
             this.itemRender.renderItemOverlayIntoGUI(mc.fontRendererObj, this.renderStack, this.xPosition + 6, this.yPosition + 8, null);
             GlStateManager.disableLighting();
-            GlStateManager.disableBlend();
+            GlStateManager.enableBlend();
             this.itemRender.zLevel = 0.0F;
 			this.zLevel = 0.0F;
 			RenderHelper.disableStandardItemLighting();