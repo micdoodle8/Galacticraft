7c2c2129498f03e4676a3ba1674acb9b7f97f2d3
radfast <rradfast@gmail.com>
2017-05-10 15:11:38 +0100

    Fix Cryogenic Chamber use with PlayerAPI installed. Fix #2174.

--- src/main/java/micdoodle8/mods/galacticraft/core/entities/player/GCPlayerBaseMP.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/entities/player/GCPlayerBaseMP.java
@@ -50,14 +50,14 @@ public class GCPlayerBaseMP extends ServerPlayerBase
         this.getClientHandler().moveEntity(this.player, par1, par3, par5);
     }
 
-//    @Override
-//    public void wakeUpPlayer(boolean par1, boolean par2, boolean par3)
-//    {
-//        if (!this.getClientHandler().wakeUpPlayer(this.player, par1, par2, par3))
-//        {
-//            super.wakeUpPlayer(par1, par2, par3);
-//        }
-//    }
+    @Override
+    public void wakeUpPlayer(boolean par1, boolean par2, boolean par3)
+    {
+        if (!this.getClientHandler().wakeUpPlayer(this.player, par1, par2, par3))
+        {
+            super.wakeUpPlayer(par1, par2, par3);
+        }
+    }
 
     @Override
     public boolean attackEntityFrom(DamageSource par1DamageSource, float par2)
