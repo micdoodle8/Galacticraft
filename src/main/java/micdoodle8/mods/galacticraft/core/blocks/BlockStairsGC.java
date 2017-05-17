c6cfa3924d9046f8dcd244a154999bb948ada8a8
radfast <rradfast@gmail.com>
2017-05-17 15:21:15 +0100

    Correct a vanilla bug for our corner stairs in 1.10.

--- src/main/java/micdoodle8/mods/galacticraft/core/blocks/BlockStairsGC.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/blocks/BlockStairsGC.java
@@ -5,8 +5,11 @@ import micdoodle8.mods.galacticraft.core.util.EnumSortCategoryBlock;
 import net.minecraft.block.BlockStairs;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.Mirror;
 import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 public class BlockStairsGC extends BlockStairs implements ISortableBlock
 {
@@ -41,5 +44,59 @@ public class BlockStairsGC extends BlockStairs implements ISortableBlock
     public IBlockState withMirror(IBlockState state, Mirror mirrorIn)
     {
         return state;
+    }
+    
+    @Override
+    public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos)
+    {
+        return state.withProperty(SHAPE, getStairsShape(state, worldIn, pos));
+    }
+
+    //Correct bugged version in vanilla Minecraft
+    private static BlockStairs.EnumShape getStairsShape(IBlockState state, IBlockAccess worldIn, BlockPos pos)
+    {
+        EnumFacing enumfacing = (EnumFacing)state.getValue(FACING);
+        IBlockState iblockstate = worldIn.getBlockState(pos.offset(enumfacing));
+        boolean top = state.getValue(HALF) == EnumHalf.TOP;
+
+        if (isBlockStairs(iblockstate) && state.getValue(HALF) == iblockstate.getValue(HALF))
+        {
+            EnumFacing enumfacing1 = (EnumFacing)iblockstate.getValue(FACING);
+
+            if (enumfacing1.getAxis() != ((EnumFacing)state.getValue(FACING)).getAxis() && isDifferentStairs(state, worldIn, pos, enumfacing1.getOpposite()))
+            {
+                if (enumfacing1 == (top ? enumfacing.rotateY() : enumfacing.rotateYCCW()))
+                {
+                    return BlockStairs.EnumShape.OUTER_LEFT;
+                }
+
+                return BlockStairs.EnumShape.OUTER_RIGHT;
+            }
+        }
+
+        IBlockState iblockstate1 = worldIn.getBlockState(pos.offset(enumfacing.getOpposite()));
+
+        if (isBlockStairs(iblockstate1) && state.getValue(HALF) == iblockstate1.getValue(HALF))
+        {
+            EnumFacing enumfacing2 = (EnumFacing)iblockstate1.getValue(FACING);
+
+            if (enumfacing2.getAxis() != ((EnumFacing)state.getValue(FACING)).getAxis() && isDifferentStairs(state, worldIn, pos, enumfacing2))
+            {
+                if (enumfacing2 == (top ? enumfacing.rotateY() : enumfacing.rotateYCCW()))
+                {
+                    return BlockStairs.EnumShape.INNER_LEFT;
+                }
+
+                return BlockStairs.EnumShape.INNER_RIGHT;
+            }
+        }
+
+        return BlockStairs.EnumShape.STRAIGHT;
+    }
+    
+    private static boolean isDifferentStairs(IBlockState state, IBlockAccess worldIn, BlockPos pos, EnumFacing side)
+    {
+        IBlockState iblockstate = worldIn.getBlockState(pos.offset(side));
+        return !isBlockStairs(iblockstate) || iblockstate.getValue(FACING) != state.getValue(FACING) || iblockstate.getValue(HALF) != state.getValue(HALF);
     }
 }
