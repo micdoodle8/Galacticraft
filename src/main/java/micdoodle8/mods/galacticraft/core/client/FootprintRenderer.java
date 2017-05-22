micdoodle8/Galacticraft

commit 3997bc36f3bd82e7ef05d586c50e40fbf70e8d9f

Refactor most (float) references to PI to use Constants.

src/main/java/micdoodle8/mods/galacticraft/core/client/FootprintRenderer.java
@@ -70,10 +70,10 @@ public class FootprintRenderer
 //                    worldRenderer.putBrightness4(brightness, brightness, brightness, brightness);
                     GL11.glColor4f(1 - ageScale, 1 - ageScale, 1 - ageScale, 1 - ageScale);
                     double footprintScale = 0.5F;
-                    worldRenderer.pos(Math.sin((45 - footprint.rotation) * Math.PI / 180.0D) * footprintScale, 0, Math.cos((45 - footprint.rotation) * Math.PI / 180.0D) * footprintScale).tex(f7, f9).endVertex();
-                    worldRenderer.pos(Math.sin((135 - footprint.rotation) * Math.PI / 180.0D) * footprintScale, 0, Math.cos((135 - footprint.rotation) * Math.PI / 180.0D) * footprintScale).tex(f7, f8).endVertex();
-                    worldRenderer.pos(Math.sin((225 - footprint.rotation) * Math.PI / 180.0D) * footprintScale, 0, Math.cos((225 - footprint.rotation) * Math.PI / 180.0D) * footprintScale).tex(f6, f8).endVertex();
-                    worldRenderer.pos(Math.sin((315 - footprint.rotation) * Math.PI / 180.0D) * footprintScale, 0, Math.cos((315 - footprint.rotation) * Math.PI / 180.0D) * footprintScale).tex(f6, f9).endVertex();
+                    worldRenderer.pos(Math.sin((45 - footprint.rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale, 0, Math.cos((45 - footprint.rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale).tex(f7, f9).endVertex();
+                    worldRenderer.pos(Math.sin((135 - footprint.rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale, 0, Math.cos((135 - footprint.rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale).tex(f7, f8).endVertex();
+                    worldRenderer.pos(Math.sin((225 - footprint.rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale, 0, Math.cos((225 - footprint.rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale).tex(f6, f8).endVertex();
+                    worldRenderer.pos(Math.sin((315 - footprint.rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale, 0, Math.cos((315 - footprint.rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale).tex(f6, f9).endVertex();
 
                     tessellator.draw();
                     GL11.glPopMatrix();

src/main/java/micdoodle8/mods/galacticraft/core/entities/EntityAdvancedMotion.java
@@ -2,6 +2,7 @@ package micdoodle8.mods.galacticraft.core.entities;
 
 import io.netty.buffer.ByteBuf;
 import micdoodle8.mods.galacticraft.api.vector.Vector3;
+import micdoodle8.mods.galacticraft.core.Constants;
 import micdoodle8.mods.galacticraft.core.GalacticraftCore;
 import micdoodle8.mods.galacticraft.core.network.PacketEntityUpdate;
 import micdoodle8.mods.galacticraft.core.network.PacketEntityUpdate.IEntityFullSync;
@@ -91,8 +92,8 @@ public abstract class EntityAdvancedMotion extends InventoryEntity implements IC
     {
         if (this.riddenByEntity != null)
         {
-            final double var1 = Math.cos(this.rotationYaw * Math.PI / 180.0D + 114.8) * -0.5D;
-            final double var3 = Math.sin(this.rotationYaw * Math.PI / 180.0D + 114.8) * -0.5D;
+            final double var1 = Math.cos(this.rotationYaw * Constants.RADIANS_TO_DEGREES_D + 114.8) * -0.5D;
+            final double var3 = Math.sin(this.rotationYaw * Constants.RADIANS_TO_DEGREES_D + 114.8) * -0.5D;
             this.riddenByEntity.setPosition(this.posX + var1, this.posY + this.getMountedYOffset() + this.riddenByEntity.getYOffset(), this.posZ + var3);
         }
     }

src/main/java/micdoodle8/mods/galacticraft/core/entities/EntityLander.java
@@ -3,6 +3,7 @@ package micdoodle8.mods.galacticraft.core.entities;
 import micdoodle8.mods.galacticraft.api.entity.ICameraZoomEntity;
 import micdoodle8.mods.galacticraft.api.entity.IIgnoreShift;
 import micdoodle8.mods.galacticraft.api.vector.Vector3;
+import micdoodle8.mods.galacticraft.core.Constants;
 import micdoodle8.mods.galacticraft.core.GalacticraftCore;
 import micdoodle8.mods.galacticraft.core.client.fx.EntityFXLanderFlame;
 import micdoodle8.mods.galacticraft.core.network.PacketSimple;
@@ -168,9 +169,9 @@ public class EntityLander extends EntityLanderBase implements IIgnoreShift, ICam
     @Override
     public Map<Vector3, Vector3> getParticleMap()
     {
-        final double x1 = 4 * Math.cos(this.rotationYaw * Math.PI / 180.0D) * Math.sin(this.rotationPitch * Math.PI / 180.0D);
-        final double z1 = 4 * Math.sin(this.rotationYaw * Math.PI / 180.0D) * Math.sin(this.rotationPitch * Math.PI / 180.0D);
-        final double y1 = -4 * Math.abs(Math.cos(this.rotationPitch * Math.PI / 180.0D));
+        final double x1 = 4 * Math.cos(this.rotationYaw * Constants.RADIANS_TO_DEGREES_D) * Math.sin(this.rotationPitch * Constants.RADIANS_TO_DEGREES_D);
+        final double z1 = 4 * Math.sin(this.rotationYaw * Constants.RADIANS_TO_DEGREES_D) * Math.sin(this.rotationPitch * Constants.RADIANS_TO_DEGREES_D);
+        final double y1 = -4 * Math.abs(Math.cos(this.rotationPitch * Constants.RADIANS_TO_DEGREES_D));
 
         new Vector3(this);
 
@@ -198,9 +199,9 @@ public class EntityLander extends EntityLanderBase implements IIgnoreShift, ICam
                 this.motionY -= 0.008D;
             }
 
-            double motY = -1 * Math.sin(this.rotationPitch * Math.PI / 180.0D);
-            double motX = Math.cos(this.rotationYaw * Math.PI / 180.0D) * motY;
-            double motZ = Math.sin(this.rotationYaw * Math.PI / 180.0D) * motY;
+            double motY = -1 * Math.sin(this.rotationPitch * Constants.RADIANS_TO_DEGREES_D);
+            double motX = Math.cos(this.rotationYaw * Constants.RADIANS_TO_DEGREES_D) * motY;
+            double motZ = Math.sin(this.rotationYaw * Constants.RADIANS_TO_DEGREES_D) * motY;
             this.motionX = motX / 2.0F;
             this.motionZ = motZ / 2.0F;
         }

src/main/java/micdoodle8/mods/galacticraft/core/entities/EntitySkeletonBoss.java
@@ -102,8 +102,8 @@ public class EntitySkeletonBoss extends EntityBossBase implements IEntityBreatha
     {
         if (this.riddenByEntity != null)
         {
-            final double offsetX = Math.sin(-this.rotationYawHead * (Math.PI / 180.0D));
-            final double offsetZ = Math.cos(this.rotationYawHead * (Math.PI / 180.0D));
+            final double offsetX = Math.sin(-this.rotationYawHead * (Constants.RADIANS_TO_DEGREES_D));
+            final double offsetZ = Math.cos(this.rotationYawHead * (Constants.RADIANS_TO_DEGREES_D));
             final double offsetY = 2 * Math.cos((this.throwTimer + this.postThrowDelay) * 0.05F);
 
             this.riddenByEntity.setPosition(this.posX + offsetX, this.posY + this.getMountedYOffset() + this.riddenByEntity.getYOffset() + offsetY, this.posZ + offsetZ);
@@ -232,7 +232,7 @@ public class EntitySkeletonBoss extends EntityBossBase implements IEntityBreatha
             {
                 GalacticraftCore.packetPipeline.sendToAllAround(new PacketSimple(EnumSimplePacket.C_PLAY_SOUND_BOW, GCCoreUtil.getDimensionID(this.worldObj), new Object[] {}), new TargetPoint(GCCoreUtil.getDimensionID(this.worldObj), this.posX, this.posY, this.posZ, 40.0D));
             }
-            ((EntityPlayer) this.thrownEntity).attackedAtYaw = (float) (Math.atan2(d1, d0) * 180.0D / Math.PI) - this.rotationYaw;
+            ((EntityPlayer) this.thrownEntity).attackedAtYaw = (float) Math.atan2(d1, d0) * Constants.RADIANS_TO_DEGREES - this.rotationYaw;
 
             this.thrownEntity.isAirBorne = true;
             final float f = MathHelper.sqrt_double(d0 * d0 + d1 * d1);

src/main/java/micdoodle8/mods/galacticraft/core/entities/EntityTier1Rocket.java
@@ -4,6 +4,7 @@ import micdoodle8.mods.galacticraft.api.prefab.entity.EntityTieredRocket;
 import micdoodle8.mods.galacticraft.api.tile.IFuelDock;
 import micdoodle8.mods.galacticraft.api.vector.Vector3;
 import micdoodle8.mods.galacticraft.api.world.IGalacticraftWorldProvider;
+import micdoodle8.mods.galacticraft.core.Constants;
 import micdoodle8.mods.galacticraft.core.GCItems;
 import micdoodle8.mods.galacticraft.core.GalacticraftCore;
 import micdoodle8.mods.galacticraft.core.entities.player.GCPlayerStats;
@@ -101,7 +102,7 @@ public class EntityTier1Rocket extends EntityTieredRocket
 
                 if (d != 0.0)
                 {
-                    this.motionY = -d * Math.cos((this.rotationPitch - 180) * Math.PI / 180.0D);
+                    this.motionY = -d * Math.cos((this.rotationPitch - 180) * Constants.RADIANS_TO_DEGREES_D);
                 }
             }
             else
@@ -167,9 +168,9 @@ public class EntityTier1Rocket extends EntityTieredRocket
     {
         if (!this.isDead)
         {
-            double x1 = 2 * Math.cos(this.rotationYaw * Math.PI / 180.0D) * Math.sin(this.rotationPitch * Math.PI / 180.0D);
-            double z1 = 2 * Math.sin(this.rotationYaw * Math.PI / 180.0D) * Math.sin(this.rotationPitch * Math.PI / 180.0D);
-            double y1 = 2 * Math.cos((this.rotationPitch - 180) * Math.PI / 180.0D);
+            double x1 = 2 * Math.cos(this.rotationYaw * Constants.RADIANS_TO_DEGREES_D) * Math.sin(this.rotationPitch * Constants.RADIANS_TO_DEGREES_D);
+            double z1 = 2 * Math.sin(this.rotationYaw * Constants.RADIANS_TO_DEGREES_D) * Math.sin(this.rotationPitch * Constants.RADIANS_TO_DEGREES_D);
+            double y1 = 2 * Math.cos((this.rotationPitch - 180) * Constants.RADIANS_TO_DEGREES_D);
 
             if (this.landing && this.targetVec != null)
             {

src/main/java/micdoodle8/mods/galacticraft/core/tile/TileEntitySolar.java
@@ -118,8 +118,8 @@ public class TileEntitySolar extends TileBaseUniversalElectricalSource implement
                 if (this.worldObj.isDaytime() && (this.worldObj.provider instanceof IGalacticraftWorldProvider || !this.worldObj.isRaining() && !this.worldObj.isThundering()))
                 {
                     double distance = 100.0D;
-                    double sinA = -Math.sin((this.currentAngle - 77.5D) * Math.PI / 180.0D);
-                    double cosA = Math.abs(Math.cos((this.currentAngle - 77.5D) * Math.PI / 180.0D));
+                    double sinA = -Math.sin((this.currentAngle - 77.5D) * Constants.RADIANS_TO_DEGREES_D);
+                    double cosA = Math.abs(Math.cos((this.currentAngle - 77.5D) * Constants.RADIANS_TO_DEGREES_D));
 
                     for (int x = -1; x <= 1; x++)
                     {

src/main/java/micdoodle8/mods/galacticraft/core/util/WorldUtil.java
@@ -3,6 +3,7 @@ package micdoodle8.mods.galacticraft.core.util;
 import com.google.common.collect.Lists;
 import com.google.common.collect.MapMaker;
 import com.google.common.collect.Maps;
+
 import micdoodle8.mods.galacticraft.api.GalacticraftRegistry;
 import micdoodle8.mods.galacticraft.api.entity.IAntiGrav;
 import micdoodle8.mods.galacticraft.api.entity.IWorldTransferCallback;
@@ -17,6 +18,7 @@ import micdoodle8.mods.galacticraft.api.world.IGalacticraftWorldProvider;
 import micdoodle8.mods.galacticraft.api.world.IOrbitDimension;
 import micdoodle8.mods.galacticraft.api.world.ITeleportType;
 import micdoodle8.mods.galacticraft.api.world.SpaceStationType;
+import micdoodle8.mods.galacticraft.core.Constants;
 import micdoodle8.mods.galacticraft.core.GCBlocks;
 import micdoodle8.mods.galacticraft.core.GalacticraftCore;
 import micdoodle8.mods.galacticraft.core.dimension.SpaceStationWorldData;
@@ -53,6 +55,7 @@ import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
 import org.apache.commons.lang3.ArrayUtils;
 
 import java.io.File;
@@ -1339,14 +1342,14 @@ public class WorldUtil
         mainPosX = position.intX();
         mainPosZ = position.intZ();
 
-        double x0 = (Math.sin((45 - rotation) * Math.PI / 180.0D) * footprintScale) + position.x;
-        double x1 = (Math.sin((135 - rotation) * Math.PI / 180.0D) * footprintScale) + position.x;
-        double x2 = (Math.sin((225 - rotation) * Math.PI / 180.0D) * footprintScale) + position.x;
-        double x3 = (Math.sin((315 - rotation) * Math.PI / 180.0D) * footprintScale) + position.x;
-        double z0 = (Math.cos((45 - rotation) * Math.PI / 180.0D) * footprintScale) + position.z;
-        double z1 = (Math.cos((135 - rotation) * Math.PI / 180.0D) * footprintScale) + position.z;
-        double z2 = (Math.cos((225 - rotation) * Math.PI / 180.0D) * footprintScale) + position.z;
-        double z3 = (Math.cos((315 - rotation) * Math.PI / 180.0D) * footprintScale) + position.z;
+        double x0 = (Math.sin((45 - rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.x;
+        double x1 = (Math.sin((135 - rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.x;
+        double x2 = (Math.sin((225 - rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.x;
+        double x3 = (Math.sin((315 - rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.x;
+        double z0 = (Math.cos((45 - rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.z;
+        double z1 = (Math.cos((135 - rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.z;
+        double z2 = (Math.cos((225 - rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.z;
+        double z3 = (Math.cos((315 - rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.z;
 
         double xMin = Math.min(Math.min(x0, x1), Math.min(x2, x3));
         double xMax = Math.max(Math.max(x0, x1), Math.max(x2, x3));

commit 11d857a35ce21b2af97b33ecc6b20cd6234871f2

Correct lander etc rotation errors introduced in 3997bc3.

src/main/java/micdoodle8/mods/galacticraft/core/client/FootprintRenderer.java
@@ -70,10 +70,10 @@ public class FootprintRenderer
 //                    worldRenderer.putBrightness4(brightness, brightness, brightness, brightness);
                     GL11.glColor4f(1 - ageScale, 1 - ageScale, 1 - ageScale, 1 - ageScale);
                     double footprintScale = 0.5F;
-                    worldRenderer.pos(Math.sin((45 - footprint.rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale, 0, Math.cos((45 - footprint.rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale).tex(f7, f9).endVertex();
-                    worldRenderer.pos(Math.sin((135 - footprint.rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale, 0, Math.cos((135 - footprint.rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale).tex(f7, f8).endVertex();
-                    worldRenderer.pos(Math.sin((225 - footprint.rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale, 0, Math.cos((225 - footprint.rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale).tex(f6, f8).endVertex();
-                    worldRenderer.pos(Math.sin((315 - footprint.rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale, 0, Math.cos((315 - footprint.rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale).tex(f6, f9).endVertex();
+                    worldRenderer.pos(Math.sin((45 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale, 0, Math.cos((45 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale).tex(f7, f9).endVertex();
+                    worldRenderer.pos(Math.sin((135 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale, 0, Math.cos((135 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale).tex(f7, f8).endVertex();
+                    worldRenderer.pos(Math.sin((225 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale, 0, Math.cos((225 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale).tex(f6, f8).endVertex();
+                    worldRenderer.pos(Math.sin((315 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale, 0, Math.cos((315 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale).tex(f6, f9).endVertex();
 
                     tessellator.draw();
                     GL11.glPopMatrix();

src/main/java/micdoodle8/mods/galacticraft/core/entities/EntityAdvancedMotion.java
@@ -92,8 +92,8 @@ public abstract class EntityAdvancedMotion extends InventoryEntity implements IC
     {
         if (this.riddenByEntity != null)
         {
-            final double var1 = Math.cos(this.rotationYaw * Constants.RADIANS_TO_DEGREES_D + 114.8) * -0.5D;
-            final double var3 = Math.sin(this.rotationYaw * Constants.RADIANS_TO_DEGREES_D + 114.8) * -0.5D;
+            final double var1 = Math.cos(this.rotationYaw / Constants.RADIANS_TO_DEGREES_D + 114.8) * -0.5D;
+            final double var3 = Math.sin(this.rotationYaw / Constants.RADIANS_TO_DEGREES_D + 114.8) * -0.5D;
             this.riddenByEntity.setPosition(this.posX + var1, this.posY + this.getMountedYOffset() + this.riddenByEntity.getYOffset(), this.posZ + var3);
         }
     }

src/main/java/micdoodle8/mods/galacticraft/core/entities/EntityLander.java
@@ -169,9 +169,10 @@ public class EntityLander extends EntityLanderBase implements IIgnoreShift, ICam
     @Override
     public Map<Vector3, Vector3> getParticleMap()
     {
-        final double x1 = 4 * Math.cos(this.rotationYaw * Constants.RADIANS_TO_DEGREES_D) * Math.sin(this.rotationPitch * Constants.RADIANS_TO_DEGREES_D);
-        final double z1 = 4 * Math.sin(this.rotationYaw * Constants.RADIANS_TO_DEGREES_D) * Math.sin(this.rotationPitch * Constants.RADIANS_TO_DEGREES_D);
-        final double y1 = -4 * Math.abs(Math.cos(this.rotationPitch * Constants.RADIANS_TO_DEGREES_D));
+        double sinPitch = Math.sin(this.rotationPitch / Constants.RADIANS_TO_DEGREES_D);
+        final double x1 = 4 * Math.cos(this.rotationYaw / Constants.RADIANS_TO_DEGREES_D) * sinPitch;
+        final double z1 = 4 * Math.sin(this.rotationYaw / Constants.RADIANS_TO_DEGREES_D) * sinPitch;
+        final double y1 = -4 * Math.abs(Math.cos(this.rotationPitch / Constants.RADIANS_TO_DEGREES_D));
 
         new Vector3(this);
 
@@ -199,9 +200,9 @@ public class EntityLander extends EntityLanderBase implements IIgnoreShift, ICam
                 this.motionY -= 0.008D;
             }
 
-            double motY = -1 * Math.sin(this.rotationPitch * Constants.RADIANS_TO_DEGREES_D);
-            double motX = Math.cos(this.rotationYaw * Constants.RADIANS_TO_DEGREES_D) * motY;
-            double motZ = Math.sin(this.rotationYaw * Constants.RADIANS_TO_DEGREES_D) * motY;
+            double motY = -1 * Math.sin(this.rotationPitch / Constants.RADIANS_TO_DEGREES_D);
+            double motX = Math.cos(this.rotationYaw / Constants.RADIANS_TO_DEGREES_D) * motY;
+            double motZ = Math.sin(this.rotationYaw / Constants.RADIANS_TO_DEGREES_D) * motY;
             this.motionX = motX / 2.0F;
             this.motionZ = motZ / 2.0F;
         }

src/main/java/micdoodle8/mods/galacticraft/core/entities/EntitySkeletonBoss.java
@@ -102,8 +102,8 @@ public class EntitySkeletonBoss extends EntityBossBase implements IEntityBreatha
     {
         if (this.riddenByEntity != null)
         {
-            final double offsetX = Math.sin(-this.rotationYawHead * (Constants.RADIANS_TO_DEGREES_D));
-            final double offsetZ = Math.cos(this.rotationYawHead * (Constants.RADIANS_TO_DEGREES_D));
+            final double offsetX = Math.sin(-this.rotationYawHead / Constants.RADIANS_TO_DEGREES_D);
+            final double offsetZ = Math.cos(this.rotationYawHead / Constants.RADIANS_TO_DEGREES_D);
             final double offsetY = 2 * Math.cos((this.throwTimer + this.postThrowDelay) * 0.05F);
 
             this.riddenByEntity.setPosition(this.posX + offsetX, this.posY + this.getMountedYOffset() + this.riddenByEntity.getYOffset() + offsetY, this.posZ + offsetZ);

src/main/java/micdoodle8/mods/galacticraft/core/entities/EntityTier1Rocket.java
@@ -102,7 +102,7 @@ public class EntityTier1Rocket extends EntityTieredRocket
 
                 if (d != 0.0)
                 {
-                    this.motionY = -d * Math.cos((this.rotationPitch - 180) * Constants.RADIANS_TO_DEGREES_D);
+                    this.motionY = -d * Math.cos((this.rotationPitch - 180) / Constants.RADIANS_TO_DEGREES_D);
                 }
             }
             else
@@ -168,9 +168,10 @@ public class EntityTier1Rocket extends EntityTieredRocket
     {
         if (!this.isDead)
         {
-            double x1 = 2 * Math.cos(this.rotationYaw * Constants.RADIANS_TO_DEGREES_D) * Math.sin(this.rotationPitch * Constants.RADIANS_TO_DEGREES_D);
-            double z1 = 2 * Math.sin(this.rotationYaw * Constants.RADIANS_TO_DEGREES_D) * Math.sin(this.rotationPitch * Constants.RADIANS_TO_DEGREES_D);
-            double y1 = 2 * Math.cos((this.rotationPitch - 180) * Constants.RADIANS_TO_DEGREES_D);
+            double sinPitch = Math.sin(this.rotationPitch / Constants.RADIANS_TO_DEGREES_D);
+            double x1 = 2 * Math.cos(this.rotationYaw / Constants.RADIANS_TO_DEGREES_D) * sinPitch;
+            double z1 = 2 * Math.sin(this.rotationYaw / Constants.RADIANS_TO_DEGREES_D) * sinPitch;
+            double y1 = 2 * Math.cos((this.rotationPitch - 180) / Constants.RADIANS_TO_DEGREES_D);
 
             if (this.launchPhase == EnumLaunchPhase.LANDING.ordinal() && this.targetVec != null)
             {

src/main/java/micdoodle8/mods/galacticraft/core/tile/TileEntitySolar.java
@@ -118,8 +118,8 @@ public class TileEntitySolar extends TileBaseUniversalElectricalSource implement
                 if (this.worldObj.isDaytime() && (this.worldObj.provider instanceof IGalacticraftWorldProvider || !this.worldObj.isRaining() && !this.worldObj.isThundering()))
                 {
                     double distance = 100.0D;
-                    double sinA = -Math.sin((this.currentAngle - 77.5D) * Constants.RADIANS_TO_DEGREES_D);
-                    double cosA = Math.abs(Math.cos((this.currentAngle - 77.5D) * Constants.RADIANS_TO_DEGREES_D));
+                    double sinA = -Math.sin((this.currentAngle - 77.5D) / Constants.RADIANS_TO_DEGREES_D);
+                    double cosA = Math.abs(Math.cos((this.currentAngle - 77.5D) / Constants.RADIANS_TO_DEGREES_D));
 
                     for (int x = -1; x <= 1; x++)
                     {

src/main/java/micdoodle8/mods/galacticraft/core/util/WorldUtil.java
@@ -1342,14 +1342,14 @@ public class WorldUtil
         mainPosX = position.intX();
         mainPosZ = position.intZ();
 
-        double x0 = (Math.sin((45 - rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.x;
-        double x1 = (Math.sin((135 - rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.x;
-        double x2 = (Math.sin((225 - rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.x;
-        double x3 = (Math.sin((315 - rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.x;
-        double z0 = (Math.cos((45 - rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.z;
-        double z1 = (Math.cos((135 - rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.z;
-        double z2 = (Math.cos((225 - rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.z;
-        double z3 = (Math.cos((315 - rotation) * Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.z;
+        double x0 = (Math.sin((45 - rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.x;
+        double x1 = (Math.sin((135 - rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.x;
+        double x2 = (Math.sin((225 - rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.x;
+        double x3 = (Math.sin((315 - rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.x;
+        double z0 = (Math.cos((45 - rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.z;
+        double z1 = (Math.cos((135 - rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.z;
+        double z2 = (Math.cos((225 - rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.z;
+        double z3 = (Math.cos((315 - rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale) + position.z;
 
         double xMin = Math.min(Math.min(x0, x1), Math.min(x2, x3));
         double xMax = Math.max(Math.max(x0, x1), Math.max(x2, x3));

src/main/java/micdoodle8/mods/galacticraft/planets/asteroids/entities/EntityTier3Rocket.java
@@ -184,8 +184,9 @@ public class EntityTier3Rocket extends EntityTieredRocket
     {
         if (!this.isDead)
         {
-            double x1 = 3.2 * Math.cos(this.rotationYaw / Constants.RADIANS_TO_DEGREES_D) * Math.sin(this.rotationPitch / Constants.RADIANS_TO_DEGREES_D);
-            double z1 = 3.2 * Math.sin(this.rotationYaw / Constants.RADIANS_TO_DEGREES_D) * Math.sin(this.rotationPitch / Constants.RADIANS_TO_DEGREES_D);
+            double sinPitch = Math.sin(this.rotationPitch / Constants.RADIANS_TO_DEGREES_D);
+            double x1 = 3.2 * Math.cos(this.rotationYaw / Constants.RADIANS_TO_DEGREES_D) * sinPitch;
+            double z1 = 3.2 * Math.sin(this.rotationYaw / Constants.RADIANS_TO_DEGREES_D) * sinPitch;
             double y1 = 3.2 * Math.cos((this.rotationPitch - 180) / Constants.RADIANS_TO_DEGREES_D);
             if (this.launchPhase == EnumLaunchPhase.LANDING.ordinal() && this.targetVec != null)
             {

src/main/java/micdoodle8/mods/galacticraft/planets/mars/entities/EntityCargoRocket.java
@@ -91,7 +91,7 @@ public class EntityCargoRocket extends EntityAutoRocket implements IRocketType, 
             {
                 if (motionScalar != 0.0)
                 {
-                    this.motionY = -motionScalar * Math.cos((this.rotationPitch - 180) * Constants.RADIANS_TO_DEGREES_D);
+                    this.motionY = -motionScalar * Math.cos((this.rotationPitch - 180) / Constants.RADIANS_TO_DEGREES_D);
                 }
             }
 
@@ -171,9 +171,10 @@ public class EntityCargoRocket extends EntityAutoRocket implements IRocketType, 
 
     protected void spawnParticles(boolean launched)
     {
-        double x1 = 2 * Math.cos(this.rotationYaw * Constants.RADIANS_TO_DEGREES_D) * Math.sin(this.rotationPitch * Constants.RADIANS_TO_DEGREES_D);
-        double z1 = 2 * Math.sin(this.rotationYaw * Constants.RADIANS_TO_DEGREES_D) * Math.sin(this.rotationPitch * Constants.RADIANS_TO_DEGREES_D);
-        double y1 = 2 * Math.cos((this.rotationPitch - 180) * Constants.RADIANS_TO_DEGREES_D);
+        double sinPitch = Math.sin(this.rotationPitch / Constants.RADIANS_TO_DEGREES_D);
+        double x1 = 2 * Math.cos(this.rotationYaw / Constants.RADIANS_TO_DEGREES_D) * sinPitch;
+        double z1 = 2 * Math.sin(this.rotationYaw / Constants.RADIANS_TO_DEGREES_D) * sinPitch;
+        double y1 = 2 * Math.cos((this.rotationPitch - 180) / Constants.RADIANS_TO_DEGREES_D);
 
         if (this.launchPhase == EnumLaunchPhase.LANDING.ordinal() && this.targetVec != null)
         {

src/main/java/micdoodle8/mods/galacticraft/planets/mars/entities/EntityTier2Rocket.java
@@ -121,7 +121,7 @@ public class EntityTier2Rocket extends EntityTieredRocket
 
                 if (d != 0.0)
                 {
-                    this.motionY = -d * 2.0D * Math.cos((this.rotationPitch - 180) * Constants.RADIANS_TO_DEGREES_D);
+                    this.motionY = -d * 2.0D * Math.cos((this.rotationPitch - 180) / Constants.RADIANS_TO_DEGREES_D);
                 }
             }
             else
@@ -187,9 +187,10 @@ public class EntityTier2Rocket extends EntityTieredRocket
     {
         if (!this.isDead)
         {
-            double x1 = 2.9 * Math.cos(this.rotationYaw * Constants.RADIANS_TO_DEGREES_D) * Math.sin(this.rotationPitch * Constants.RADIANS_TO_DEGREES_D);
-            double z1 = 2.9 * Math.sin(this.rotationYaw * Constants.RADIANS_TO_DEGREES_D) * Math.sin(this.rotationPitch * Constants.RADIANS_TO_DEGREES_D);
-            double y1 = 2.9 * Math.cos((this.rotationPitch - 180) * Constants.RADIANS_TO_DEGREES_D);
+            double sinPitch = Math.sin(this.rotationPitch / Constants.RADIANS_TO_DEGREES_D);
+            double x1 = 2.9 * Math.cos(this.rotationYaw / Constants.RADIANS_TO_DEGREES_D) * sinPitch;
+            double z1 = 2.9 * Math.sin(this.rotationYaw / Constants.RADIANS_TO_DEGREES_D) * sinPitch;
+            double y1 = 2.9 * Math.cos((this.rotationPitch - 180) / Constants.RADIANS_TO_DEGREES_D);
             if (this.launchPhase == EnumLaunchPhase.LANDING.ordinal() && this.targetVec != null)
             {
                 double modifier = this.posY - this.targetVec.getY();