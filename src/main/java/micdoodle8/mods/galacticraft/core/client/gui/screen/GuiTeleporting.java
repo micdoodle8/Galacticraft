672055677f36b1ff1342596fe2c639a4008bb10d
Mitchell Petrie <mitchpetrie29@gmail.com>
2017-05-24 21:43:09 +0200

    Fix teleporting gui not exiting

--- src/main/java/micdoodle8/mods/galacticraft/core/client/gui/screen/GuiTeleporting.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/client/gui/screen/GuiTeleporting.java
@@ -1,7 +1,9 @@
 package micdoodle8.mods.galacticraft.core.client.gui.screen;
 
+import micdoodle8.mods.galacticraft.api.world.IZeroGDimension;
 import micdoodle8.mods.galacticraft.core.tick.TickHandlerClient;
 import micdoodle8.mods.galacticraft.core.util.ColorUtil;
+import micdoodle8.mods.galacticraft.core.util.GCCoreUtil;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.network.play.client.C00PacketKeepAlive;
 
@@ -37,11 +39,10 @@ public class GuiTeleporting extends GuiScreen
     public void updateScreen()
     {
         super.updateScreen();
-        
         if (mc.thePlayer.worldObj != null)
         {
             // Screen will exit when the player is in the target dimension and has started moving down
-            if (mc.thePlayer.worldObj.provider.getDimensionId() == this.targetDimensionID && (mc.thePlayer.posY - mc.thePlayer.lastTickPosY) < 0.0)
+            if (mc.thePlayer.worldObj.provider.getDimensionId() == this.targetDimensionID && (mc.thePlayer.worldObj.provider instanceof IZeroGDimension || (mc.thePlayer.posY - mc.thePlayer.lastTickPosY) < 0.0))
             {
                 mc.displayGuiScreen(null);
                 TickHandlerClient.teleportingGui = null;
