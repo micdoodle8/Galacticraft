394590928d635272715c70a1d07400ca63060b32
radfast <rradfast@gmail.com>
2017-06-02 00:00:39 +0100

    Fix performance issues around Oil blocks in 1.7. Close #1712.

---
+++ src/main/java/micdoodle8/mods/galacticraft/core/util/JavaUtil.java
@@ -0,0 +1,57 @@
+package micdoodle8.mods.galacticraft.core.util;
+
+public class JavaUtil extends SecurityManager
+{
+    public static JavaUtil instance = new JavaUtil();
+
+    public Class<?> getCaller()
+    {
+        return getClassContext()[2];
+    }
+
+    public boolean isCalledBy(Class<?> clazz)
+    {
+        Class<?>[] context = getClassContext();
+        
+        int imax = Math.min(context.length, 6);
+        for (int i = 2; i < imax; i++)
+        {
+            if (clazz == context[i])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isCalledBy(Class<?> clazz1, Class<?> clazz2, Class<?> clazz3)
+    {
+        Class<?>[] context = getClassContext();
+        
+        int imax = Math.min(context.length, 6);
+        for (int i = 2; i < imax; i++)
+        {
+            Class<?> test = context[i]; 
+            if (test == clazz1 || test == clazz2 || test == clazz3)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isCalledBy(String name)
+    {
+        Class<?>[] context = getClassContext();
+        
+        int imax = Math.min(context.length, 6);
+        for (int i = 2; i < imax; i++)
+        {
+            if (context[i].getName().equals(name))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
