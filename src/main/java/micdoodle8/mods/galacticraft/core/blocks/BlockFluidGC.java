d562f1a6733e5b15f2b1590fa0e411a624ef577c
radfast <rradfast@gmail.com>
2017-07-02 16:11:04 +0100

    Backport Forge fluids fix.  See https://github.com/MinecraftForge/MinecraftForge/commit/3eb7b59cf406786bc6e28116c1ee460ab70bc227

--- src/main/java/micdoodle8/mods/galacticraft/core/blocks/BlockFluidGC.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/blocks/BlockFluidGC.java
@@ -4,6 +4,7 @@ import micdoodle8.mods.galacticraft.api.vector.Vector3;
 import micdoodle8.mods.galacticraft.core.GCFluids;
 import micdoodle8.mods.galacticraft.core.GalacticraftCore;
 import micdoodle8.mods.galacticraft.core.proxy.ClientProxyCore;
+import net.minecraft.block.BlockLiquid;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.entity.EntityPlayerSP;
@@ -12,8 +13,10 @@ import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.common.property.IExtendedBlockState;
 import net.minecraftforge.fluids.BlockFluidClassic;
 import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.IFluidBlock;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -99,5 +102,107 @@ public class BlockFluidGC extends BlockFluidClassic
             return true;
         }
         return (this.fluidName.startsWith("oil"));
+    }
+    
+    @Override
+    public IBlockState getExtendedState(IBlockState oldState, IBlockAccess world, BlockPos pos)
+    {
+        IExtendedBlockState state = (IExtendedBlockState)oldState;
+        state = state.withProperty(FLOW_DIRECTION, (float)getFlowDirection(world, pos));
+        IBlockState[][] upBlockState = new IBlockState[3][3];
+        float[][] height = new float[3][3];
+        float[][] corner = new float[2][2];
+        upBlockState[1][1] = world.getBlockState(pos.down(this.densityDir));
+        height[1][1] = this.getFluidHeightForRender(world, pos, upBlockState[1][1]);
+
+        if (height[1][1] == 1)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    corner[i][j] = 1;
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (i != 1 || j != 1)
+                    {
+                        upBlockState[i][j] = world.getBlockState(pos.add(i - 1, 0, j - 1).down(this.densityDir));
+                        height[i][j] = this.getFluidHeightForRender(world, pos.add(i - 1, 0, j - 1), upBlockState[i][j]);
+                    }
+                }
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    corner[i][j] = this.getFluidHeightAverage(height[i][j], height[i][j + 1], height[i + 1][j], height[i + 1][j + 1]);
+                }
+            }
+            //check for downflow above corners
+            boolean n =  this.isFluid(upBlockState[0][1]);
+            boolean s =  this.isFluid(upBlockState[2][1]);
+            boolean w =  this.isFluid(upBlockState[1][0]);
+            boolean e =  this.isFluid(upBlockState[1][2]);
+            boolean nw = this.isFluid(upBlockState[0][0]);
+            boolean ne = this.isFluid(upBlockState[0][2]);
+            boolean sw = this.isFluid(upBlockState[2][0]);
+            boolean se = this.isFluid(upBlockState[2][2]);
+
+            if (nw || n || w)
+            {
+                corner[0][0] = 1;
+            }
+            if (ne || n || e)
+            {
+                corner[0][1] = 1;
+            }
+            if (sw || s || w)
+            {
+                corner[1][0] = 1;
+            }
+            if (se || s || e)
+            {
+                corner[1][1] = 1;
+            }
+        }
+        state = state.withProperty(LEVEL_CORNERS[0], corner[0][0]);
+        state = state.withProperty(LEVEL_CORNERS[1], corner[0][1]);
+        state = state.withProperty(LEVEL_CORNERS[2], corner[1][1]);
+        state = state.withProperty(LEVEL_CORNERS[3], corner[1][0]);
+        return state;
+    }
+
+    private boolean isFluid(IBlockState state)
+    {
+        return state.getBlock().getMaterial().isLiquid() || state.getBlock() instanceof IFluidBlock;
+    }
+
+    private float getFluidHeightForRender(IBlockAccess world, BlockPos pos, IBlockState up)
+    {
+        IBlockState here = world.getBlockState(pos);
+
+        if (here.getBlock() == this)
+        {
+            if (up.getBlock().getMaterial().isLiquid() || up.getBlock() instanceof IFluidBlock)
+            {
+                return 1;
+            }
+            if (this.getMetaFromState(here) == this.getMaxRenderHeightMeta())
+            {
+                return 0.875F;
+            }
+        }
+        if (here.getBlock() instanceof BlockLiquid)
+        {
+            return Math.min(1 - BlockLiquid.getLiquidHeightPercent(here.getValue(BlockLiquid.LEVEL)), 14f / 16);
+        }
+        return !here.getBlock().getMaterial().isSolid() && up.getBlock() == this ? 1 : this.getQuantaPercentage(world, pos) * 0.875F;
     }
 }
