642860e9fa13e1e9d16f8a85ab074b4f8c7931ad
SteveKunG <mccommander_minecraft@hotmail.com>
2017-07-04 05:10:40 +0700

    Added XP Smelting info to Ingot Compressor JEI recipe (#2874)

--- src/main/java/micdoodle8/mods/galacticraft/core/client/jei/ingotcompressor/IngotCompressorShapedRecipeWrapper.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/client/jei/ingotcompressor/IngotCompressorShapedRecipeWrapper.java
@@ -4,9 +4,15 @@ import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.BlankRecipeWrapper;
 import mezz.jei.api.recipe.wrapper.ICraftingRecipeWrapper;
 import micdoodle8.mods.galacticraft.api.recipe.ShapedRecipesGC;
+import micdoodle8.mods.galacticraft.core.util.GCCoreUtil;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.FurnaceRecipes;
 
 import javax.annotation.Nonnull;
+
+import java.awt.Color;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -50,5 +56,26 @@ public class IngotCompressorShapedRecipeWrapper extends BlankRecipeWrapper imple
     public void getIngredients(IIngredients ingredients)
     {
 
+    }
+
+    @Override
+    public void drawInfo(Minecraft mc, int recipeWidth, int recipeHeight, int mouseX, int mouseY)
+    {
+        FurnaceRecipes furnaceRecipes = FurnaceRecipes.instance();
+        float experience = 0;
+
+        try
+        {
+            experience = furnaceRecipes.getSmeltingExperience(this.recipe.getRecipeOutput());
+        }
+        catch (Exception e) {}
+
+        if (experience > 0)
+        {
+            String experienceString = GCCoreUtil.translateWithFormat("gui.jei.category.smelting.experience", experience);
+            FontRenderer fontRendererObj = mc.fontRendererObj;
+            int stringWidth = fontRendererObj.getStringWidth(experienceString);
+            fontRendererObj.drawString(experienceString, recipeWidth + 6 - stringWidth, 8, Color.gray.getRGB());
+        }
     }
 }
--- src/main/java/micdoodle8/mods/galacticraft/core/client/jei/ingotcompressor/IngotCompressorShapelessRecipeWrapper.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/client/jei/ingotcompressor/IngotCompressorShapelessRecipeWrapper.java
@@ -4,9 +4,15 @@ import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.BlankRecipeWrapper;
 import mezz.jei.api.recipe.wrapper.ICraftingRecipeWrapper;
 import micdoodle8.mods.galacticraft.api.recipe.ShapelessOreRecipeGC;
+import micdoodle8.mods.galacticraft.core.util.GCCoreUtil;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.FurnaceRecipes;
 
 import javax.annotation.Nonnull;
+
+import java.awt.Color;
 import java.util.Collections;
 import java.util.List;
 
@@ -38,5 +44,26 @@ public class IngotCompressorShapelessRecipeWrapper extends BlankRecipeWrapper im
     public void getIngredients(IIngredients ingredients)
     {
 
+    }
+
+    @Override
+    public void drawInfo(Minecraft mc, int recipeWidth, int recipeHeight, int mouseX, int mouseY)
+    {
+        FurnaceRecipes furnaceRecipes = FurnaceRecipes.instance();
+        float experience = 0;
+
+        try
+        {
+            experience = furnaceRecipes.getSmeltingExperience(this.recipe.getRecipeOutput());
+        }
+        catch (Exception e) {}
+
+        if (experience > 0)
+        {
+            String experienceString = GCCoreUtil.translateWithFormat("gui.jei.category.smelting.experience", experience);
+            FontRenderer fontRendererObj = mc.fontRendererObj;
+            int stringWidth = fontRendererObj.getStringWidth(experienceString);
+            fontRendererObj.drawString(experienceString, recipeWidth + 6 - stringWidth, 8, Color.gray.getRGB());
+        }
     }
 }
