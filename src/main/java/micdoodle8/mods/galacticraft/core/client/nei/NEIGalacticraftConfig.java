64dbefb175d2e4bde31ca344e1f747d70ab7064c
radfast <rradfast@gmail.com>
2017-07-15 15:20:55 +0100

    Fix Ingot Compressor automation broken since 5537b00.

--- src/main/java/micdoodle8/mods/galacticraft/core/client/nei/NEIGalacticraftConfig.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/client/nei/NEIGalacticraftConfig.java
@@ -5,6 +5,8 @@ import codechicken.nei.api.API;
 import codechicken.nei.api.IConfigureNEI;
 import codechicken.nei.api.ItemInfo;
 import micdoodle8.mods.galacticraft.api.recipe.CompressorRecipes;
+import micdoodle8.mods.galacticraft.api.recipe.ShapedRecipesGC;
+import micdoodle8.mods.galacticraft.api.recipe.ShapelessOreRecipeGC;
 import micdoodle8.mods.galacticraft.core.Constants;
 import micdoodle8.mods.galacticraft.core.GCBlocks;
 import micdoodle8.mods.galacticraft.core.GCItems;
@@ -16,10 +18,7 @@ import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
-import net.minecraft.item.crafting.ShapedRecipes;
 import net.minecraftforge.oredict.OreDictionary;
-import net.minecraftforge.oredict.ShapelessOreRecipe;
-
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -287,9 +286,9 @@ public class NEIGalacticraftConfig implements IConfigureNEI
             HashMap<Integer, PositionedStack> input1 = new HashMap<Integer, PositionedStack>();
             IRecipe rec = CompressorRecipes.getRecipeList().get(i);
 
-            if (rec instanceof ShapedRecipes)
+            if (rec instanceof ShapedRecipesGC)
             {
-                ShapedRecipes recipe = (ShapedRecipes) rec;
+                ShapedRecipesGC recipe = (ShapedRecipesGC) rec;
 
                 for (int j = 0; j < recipe.recipeItems.length; j++)
                 {
@@ -298,9 +297,9 @@ public class NEIGalacticraftConfig implements IConfigureNEI
                     input1.put(j, new PositionedStack(stack, 21 + j % 3 * 18, 26 + j / 3 * 18));
                 }
             }
-            else if (rec instanceof ShapelessOreRecipe)
+            else if (rec instanceof ShapelessOreRecipeGC)
             {
-                ShapelessOreRecipe recipe = (ShapelessOreRecipe) rec;
+                ShapelessOreRecipeGC recipe = (ShapelessOreRecipeGC) rec;
 
                 for (int j = 0; j < recipe.getInput().size(); j++)
                 {
