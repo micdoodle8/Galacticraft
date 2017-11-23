bb0349e7fa379ea22b020d0ad150fe62e416980c
SteveKunG <mccommander_minecraft@hotmail.com>
2017-11-23 19:28:03 +0700

    Fix all entity lighting

--- src/main/java/micdoodle8/mods/galacticraft/core/client/render/entities/layer/LayerFrequencyModule.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/client/render/entities/layer/LayerFrequencyModule.java
@@ -75,6 +75,7 @@ public class LayerFrequencyModule implements LayerRenderer<AbstractClientPlayer>
                 if (wearingModule)
                 {
                     this.updateModels();
+                    GlStateManager.enableRescaleNormal();
                     GlStateManager.pushMatrix();
                     Minecraft.getMinecraft().getTextureManager().bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
 
--- src/main/java/micdoodle8/mods/galacticraft/planets/mars/client/render/entity/RenderLandingBalloons.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/mars/client/render/entity/RenderLandingBalloons.java
@@ -9,6 +9,7 @@ import micdoodle8.mods.galacticraft.planets.mars.client.model.ModelBalloonParach
 import micdoodle8.mods.galacticraft.planets.mars.entities.EntityLandingBalloons;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
@@ -35,22 +36,15 @@ public class RenderLandingBalloons extends Render<EntityLandingBalloons>
 
     private void updateModels()
     {
-        if (balloonModel == null)
+        if (this.balloonModel == null)
         {
             try
             {
                 OBJModel model = (OBJModel) ModelLoaderRegistry.getModel(new ResourceLocation(GalacticraftPlanets.ASSET_PREFIX, "landing_balloon.obj"));
                 model = (OBJModel) model.process(ImmutableMap.of("flip-v", "true"));
 
-                Function<ResourceLocation, TextureAtlasSprite> spriteFunction = new Function<ResourceLocation, TextureAtlasSprite>()
-                {
-                    @Override
-                    public TextureAtlasSprite apply(ResourceLocation location)
-                    {
-                        return Minecraft.getMinecraft().getTextureMapBlocks().getAtlasSprite(location.toString());
-                    }
-                };
-                balloonModel = (OBJModel.OBJBakedModel) model.bake(new OBJModel.OBJState(ImmutableList.of("Sphere"), false), DefaultVertexFormats.ITEM, spriteFunction);
+                Function<ResourceLocation, TextureAtlasSprite> spriteFunction = location -> Minecraft.getMinecraft().getTextureMapBlocks().getAtlasSprite(location.toString());
+                this.balloonModel = (OBJModel.OBJBakedModel) model.bake(new OBJModel.OBJState(ImmutableList.of("Sphere"), false), DefaultVertexFormats.ITEM, spriteFunction);
             }
             catch (Exception e)
             {
@@ -60,24 +54,24 @@ public class RenderLandingBalloons extends Render<EntityLandingBalloons>
     }
 
     @Override
-    protected ResourceLocation getEntityTexture(EntityLandingBalloons par1Entity)
+    protected ResourceLocation getEntityTexture(EntityLandingBalloons entity)
     {
-        return new ResourceLocation("missing");
+        return TextureMap.LOCATION_BLOCKS_TEXTURE;
     }
 
     @Override
-    public void doRender(EntityLandingBalloons entity, double par2, double par4, double par6, float par8, float par9)
+    public void doRender(EntityLandingBalloons entity, double x, double y, double z, float entityYaw, float partialTicks)
     {
-        GL11.glPushMatrix();
-        final float var24 = entity.prevRotationPitch + (entity.rotationPitch - entity.prevRotationPitch) * par9;
-        GL11.glTranslatef((float) par2, (float) par4 + 0.8F, (float) par6);
-        GL11.glRotatef(par8, 0.0F, 1.0F, 0.0F);
-        GL11.glRotatef(180.0F, 1.0F, 0.0F, 0.0F);
-        GL11.glRotatef(var24, 0.0F, 0.0F, 1.0F);
+        float pitch = entity.prevRotationPitch + (entity.rotationPitch - entity.prevRotationPitch) * partialTicks;
+        GlStateManager.disableRescaleNormal();
+        GlStateManager.pushMatrix();
+        GlStateManager.translate((float) x, (float) y + 0.8F, (float) z);
+        GlStateManager.rotate(entityYaw, 0.0F, 1.0F, 0.0F);
+        GlStateManager.rotate(180.0F, 1.0F, 0.0F, 0.0F);
+        GlStateManager.rotate(pitch, 0.0F, 0.0F, 1.0F);
 
         this.updateModels();
-
-        this.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+        this.bindEntityTexture(entity);
 
         if (Minecraft.isAmbientOcclusionEnabled())
         {
@@ -88,20 +82,19 @@ public class RenderLandingBalloons extends Render<EntityLandingBalloons>
             GlStateManager.shadeModel(GL11.GL_FLAT);
         }
 
-        GL11.glScalef(-1.0F, -1.0F, 1.0F);
-        GL11.glScalef(0.5F, 0.5F, 0.5F);
+        GlStateManager.scale(-1.0F, -1.0F, 1.0F);
+        GlStateManager.scale(0.5F, 0.5F, 0.5F);
         ClientUtil.drawBakedModel(this.balloonModel);
-        GL11.glPopMatrix();
+        GlStateManager.popMatrix();
 
         if (entity.posY >= 500.0F)
         {
-            GL11.glPushMatrix();
-
-            GL11.glTranslatef((float) par2 - 1.25F, (float) par4 - 0.93F, (float) par6 - 0.3F);
-            GL11.glScalef(2.5F, 3.0F, 2.5F);
+            GlStateManager.pushMatrix();
+            GlStateManager.translate((float) x - 1.25F, (float) y - 0.93F, (float) z - 0.3F);
+            GlStateManager.scale(2.5F, 3.0F, 2.5F);
             this.parachuteModel.renderAll();
-
-            GL11.glPopMatrix();
+            GlStateManager.popMatrix();
         }
+        RenderHelper.enableStandardItemLighting();
     }
 }
