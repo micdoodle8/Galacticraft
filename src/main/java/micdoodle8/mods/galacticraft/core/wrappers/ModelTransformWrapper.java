f3d4ea66a027951a08e4e4bc2f9b53c048095e3b
Mitch <mitchpetrie29@gmail.com>
2018-01-26 11:44:13 -0500

    Fix item model rotation

--- src/main/java/micdoodle8/mods/galacticraft/core/wrappers/ModelTransformWrapper.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/wrappers/ModelTransformWrapper.java
@@ -1,34 +1,73 @@
 package micdoodle8.mods.galacticraft.core.wrappers;
 
-import java.lang.reflect.Field;
-
-import javax.vecmath.Matrix4f;
-
-import com.google.common.collect.ImmutableMap;
-
-import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.*;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformType;
-import net.minecraftforge.client.model.PerspectiveMapWrapper;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.model.TRSRTransformation;
+import org.apache.commons.lang3.tuple.Pair;
 
-abstract public class ModelTransformWrapper extends PerspectiveMapWrapper
+import javax.annotation.Nullable;
+import javax.vecmath.Matrix4f;
+import java.util.List;
+
+abstract public class ModelTransformWrapper implements IBakedModel
 {
+    private final IBakedModel parent;
+
 	public ModelTransformWrapper(IBakedModel parent)
 	{
-		super(parent, (ImmutableMap<TransformType, TRSRTransformation>) null);
-		ImmutableMap.Builder<TransformType, TRSRTransformation> buildIt = ImmutableMap.builder();
-		for (TransformType tt : TransformType.values())
+	    this.parent = parent;
+	}
+
+    public boolean isAmbientOcclusion()
+    {
+        return parent.isAmbientOcclusion();
+    }
+
+    public boolean isGui3d()
+    {
+        return parent.isGui3d();
+    }
+
+    public boolean isBuiltInRenderer()
+    {
+        return parent.isBuiltInRenderer();
+    }
+
+    public TextureAtlasSprite getParticleTexture()
+    {
+        return parent.getParticleTexture();
+    }
+
+    @SuppressWarnings("deprecation")
+    public ItemCameraTransforms getItemCameraTransforms()
+    {
+        return parent.getItemCameraTransforms();
+    }
+
+    public List<BakedQuad> getQuads(@Nullable IBlockState state, @Nullable EnumFacing side, long rand)
+    {
+        return parent.getQuads(state, side, rand);
+    }
+
+    public ItemOverrideList getOverrides()
+    {
+        return parent.getOverrides();
+    }
+
+	@Override
+	public Pair<? extends IBakedModel, Matrix4f> handlePerspective(ItemCameraTransforms.TransformType cameraTransformType)
+	{
+		Matrix4f matrix4f = getTransformForPerspective(cameraTransformType);
+
+		if (matrix4f == null)
 		{
-			Matrix4f m = this.getTransformForPerspective(tt);
-			buildIt.put(tt, TRSRTransformation.blockCenterToCorner(new TRSRTransformation(m)));
+			return Pair.of(this, TRSRTransformation.blockCornerToCenter(new TRSRTransformation(ItemTransformVec3f.DEFAULT)).getMatrix());
 		}
-		try {
-			Field f = this.getClass().getSuperclass().getSuperclass().getDeclaredField("transforms");
-			f.setAccessible(true);
-			f.set(this, buildIt.build());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+
+		return Pair.of(this, matrix4f);
 	}
 
     abstract protected Matrix4f getTransformForPerspective(TransformType cameraTransformType);
