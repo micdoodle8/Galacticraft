051f9cae21109fa47f98da75dbc50f400ec3aa92
radfast <rradfast@gmail.com>
2018-02-11 02:38:13 +0000

    Fixup following merge.

--- src/main/java/micdoodle8/mods/galacticraft/planets/asteroids/items/ItemSwordAsteroids.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/asteroids/items/ItemSwordAsteroids.java
@@ -60,8 +60,8 @@ public class ItemSwordAsteroids extends ItemSword implements ISortableItem
 
         if (equipmentSlot == EntityEquipmentSlot.MAINHAND)
         {
-            multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getAttributeUnlocalizedName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", this.attackDamageD, 0));
-            multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getAttributeUnlocalizedName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", -2.8D, 0));
+            multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", this.attackDamageD, 0));
+            multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", -2.8D, 0));
         }
 
         return multimap;
