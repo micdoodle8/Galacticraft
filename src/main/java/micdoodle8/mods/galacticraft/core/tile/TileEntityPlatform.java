9844d0680d66c90ee67534691e1b687bf58d216a
radfast <rradfast@gmail.com>
2018-02-15 01:53:42 +0000

    Fix Hydraulic Platform issue on dedicated servers.

--- src/main/java/micdoodle8/mods/galacticraft/core/tile/TileEntityPlatform.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/tile/TileEntityPlatform.java
@@ -196,6 +196,7 @@ public class TileEntityPlatform extends TileEntity implements ITickable
         }
     }
 
+    @SideOnly(Side.CLIENT)
     private void startMove(TileEntityPlatform te)
     {
         this.moving = true;
@@ -450,6 +451,7 @@ public class TileEntityPlatform extends TileEntity implements ITickable
         this.moving = false;
     }
 
+    @SideOnly(Side.CLIENT)
     public float getYOffset(float partialTicks)
     {
         if (this.moving)
@@ -473,6 +475,7 @@ public class TileEntityPlatform extends TileEntity implements ITickable
         return this.renderAABB;
     }
 
+    @SideOnly(Side.CLIENT)
     public float getMeanLightX(float yOffset)
     {
         this.lightA = this.worldObj.getCombinedLight(this.getPos(), 0);
@@ -482,6 +485,7 @@ public class TileEntityPlatform extends TileEntity implements ITickable
         return (1 - f) * a + f * b;  
     }
 
+    @SideOnly(Side.CLIENT)
     public float getMeanLightZ(float yOffset)
     {
         float a = (float)(this.lightA / 65536);
@@ -490,6 +494,7 @@ public class TileEntityPlatform extends TileEntity implements ITickable
         return (1 - f) * a + f * b;  
     }
 
+    @SideOnly(Side.CLIENT)
     public boolean motionObstructed(double y, double velocityY)
     {
         EntityPlayerSP p = FMLClientHandler.instance().getClientPlayerEntity();
