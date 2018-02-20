38fc8671e3820ba70785b5dc06aa74f42d857224
radfast <rradfast@gmail.com>
2018-02-20 12:31:47 +0000

    Ingot Compressor adventure recipes - further fixes to JEI.

--- src/main/java/micdoodle8/mods/galacticraft/api/recipe/CompressorRecipes.java
+++ src/main/java/micdoodle8/mods/galacticraft/api/recipe/CompressorRecipes.java
@@ -3,6 +3,7 @@ package micdoodle8.mods.galacticraft.api.recipe;
 import micdoodle8.mods.galacticraft.api.GalacticraftConfigAccess;
 import micdoodle8.mods.galacticraft.core.GCItems;
 import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -213,9 +214,55 @@ public class CompressorRecipes
 
     public static List<IRecipe> getRecipeListAll()
     {
-        return CompressorRecipes.recipesAdventure;
+        List<IRecipe> result = new ArrayList(CompressorRecipes.recipesAdventure);
+        List<IRecipe> endList = getRecipeListHidden(true, true);
+        result.removeIf(irecipe -> endList.contains(irecipe));
+        IRecipe ice = null;
+        Item iceItem = new ItemStack(Blocks.ice).getItem();
+        for (IRecipe test : result)
+        {
+            if (test.getRecipeOutput().getItem() == iceItem)
+            {
+                ice = test;
+                break;
+            }
+        }
+        if (ice != null)
+        {
+            result.remove(ice);
+            result.add(ice);
+        }
+        result.addAll(endList);
+        return result;
     }
 
+    public static List<IRecipe> getRecipeListHidden(boolean hideSteel, boolean hideAdventure)
+    {
+        if (!hideAdventure) return new ArrayList<IRecipe>(0);
+
+        List<IRecipe> result = new ArrayList(CompressorRecipes.recipesAdventure);
+        result.removeIf(irecipe -> CompressorRecipes.recipes.contains(irecipe));
+        if (steelIngotsPresent && hideSteel)
+        {
+            List<IRecipe> resultSteelless = new ArrayList(result.size());
+            for (IRecipe recipe : result)
+            {
+                ItemStack output = recipe.getRecipeOutput();
+                if (output == null) continue;
+                if (output.getItemDamage() == 9 && output.getItem() == GCItems.basicItem && recipe instanceof ShapelessOreRecipeGC)
+                {
+                    if (((ShapelessOreRecipeGC)recipe).matches(steelRecipeGC))
+                    {
+                        continue;
+                    }
+                }
+                resultSteelless.add(recipe);
+            }
+            return resultSteelless;
+        }
+        return result;
+    }
+        
     public static List<IRecipe> getRecipeList()
     {
     	if (GalacticraftConfigAccess.getChallengeRecipes())
