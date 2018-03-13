35a49978a80f39b45c097348e7e710919a392e29
BlesseNtumble <BlesseNtumble@users.noreply.github.com>
2018-03-13 21:25:33 +0900

    Add atmosphere component on Venus
    https://en.wikipedia.org/wiki/Atmosphere_of_Venus
    Carbon dioxide 96.5 %
    Nitrogen	3.5 %

--- src/main/java/micdoodle8/mods/galacticraft/planets/venus/VenusModule.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/venus/VenusModule.java
@@ -121,6 +121,7 @@ public class VenusModule implements IPlanetsModule
         VenusModule.planetVenus.setBodyIcon(new ResourceLocation(Constants.ASSET_PREFIX, "textures/gui/celestialbodies/venus.png"));
         VenusModule.planetVenus.setDimensionInfo(ConfigManagerVenus.dimensionIDVenus, WorldProviderVenus.class).setTierRequired(3);
         VenusModule.planetVenus.setAtmosphere(new AtmosphereInfo(false, false, true, 5.0F, 0.3F, 54.0F));
+        VenusModule.planetVenus.atmosphereComponent(EnumAtmosphericGas.CO2).atmosphereComponent(EnumAtmosphericGas.NITROGEN);
         VenusModule.planetVenus.setBiomeInfo(BiomeVenus.venusFlat, BiomeVenus.venusMountain, BiomeVenus.venusValley);
         VenusModule.planetVenus.addMobInfo(new SpawnListEntry(EntityEvolvedZombie.class, 8, 2, 3));
         VenusModule.planetVenus.addMobInfo(new SpawnListEntry(EntityEvolvedSpider.class, 8, 2, 3));
