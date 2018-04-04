5ad18cad32405ca3fa278fb5cf48213bc8f776ea
Mitch <mitchpetrie29@gmail.com>
2018-04-04 14:53:19 -0400

    Fix #3276. Fog now renders correctly at all heights.

--- src/main/java/micdoodle8/mods/galacticraft/planets/mars/client/SkyProviderMars.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/mars/client/SkyProviderMars.java
@@ -65,10 +65,10 @@ public class SkyProviderMars extends IRenderHandler
             for (int l = -byte2 * i; l <= byte2 * i; l += byte2)
             {
                 worldRenderer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION);
-                worldRenderer.pos(j + 0, f, l + 0).endVertex();
-                worldRenderer.pos(j + byte2, f, l + 0).endVertex();
+                worldRenderer.pos(j, f, l).endVertex();
+                worldRenderer.pos(j + byte2, f, l).endVertex();
                 worldRenderer.pos(j + byte2, f, l + byte2).endVertex();
-                worldRenderer.pos(j + 0, f, l + byte2).endVertex();
+                worldRenderer.pos(j, f, l + byte2).endVertex();
                 tessellator.draw();
             }
         }
@@ -82,9 +82,9 @@ public class SkyProviderMars extends IRenderHandler
         {
             for (int i1 = -byte2 * i; i1 <= byte2 * i; i1 += byte2)
             {
-                worldRenderer.pos(k + byte2, f, i1 + 0).endVertex();
-                worldRenderer.pos(k + 0, f, i1 + 0).endVertex();
-                worldRenderer.pos(k + 0, f, i1 + byte2).endVertex();
+                worldRenderer.pos(k + byte2, f, i1).endVertex();
+                worldRenderer.pos(k, f, i1).endVertex();
+                worldRenderer.pos(k, f, i1 + byte2).endVertex();
                 worldRenderer.pos(k + byte2, f, i1 + byte2).endVertex();
             }
         }
@@ -329,7 +329,6 @@ public class SkyProviderMars extends IRenderHandler
         GL11.glEnable(GL11.GL_TEXTURE_2D);
         GL11.glEnable(GL12.GL_RESCALE_NORMAL);
         GL11.glEnable(GL11.GL_COLOR_MATERIAL);
-        GL11.glDisable(GL11.GL_FOG);
         OpenGlHelper.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ZERO);
         GL11.glDepthMask(true);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
