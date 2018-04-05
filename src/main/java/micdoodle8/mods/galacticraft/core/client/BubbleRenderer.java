micdoodle8/Galacticraft

commit 873008b28f03a64c4e5f60e2c1f63140b5a6aa4e

Fix #3315. Render bubbles independently of tile entities.

src/main/java/micdoodle8/mods/galacticraft/core/client/BubbleRenderer.java
@@ -1,33 +1,37 @@
-package micdoodle8.mods.galacticraft.core.client.render.tile;
+package micdoodle8.mods.galacticraft.core.client;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import micdoodle8.mods.galacticraft.api.vector.Vector3;
 import micdoodle8.mods.galacticraft.core.Constants;
-import micdoodle8.mods.galacticraft.core.entities.IBubbleProvider;
+import micdoodle8.mods.galacticraft.core.entities.IBubbleProviderColored;
 import micdoodle8.mods.galacticraft.core.util.ClientUtil;
 import micdoodle8.mods.galacticraft.core.util.ColorUtil;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.texture.TextureMap;
-import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.IFlexibleBakedModel;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
 
-@Deprecated
-public class TileEntityBubbleProviderRenderer<E extends TileEntity & IBubbleProvider> extends TileEntitySpecialRenderer<E>
+import java.util.List;
+
+public class BubbleRenderer
 {
     private static IFlexibleBakedModel sphere;
+    private static List<IBubbleProviderColored> bubbleProviders = Lists.newArrayList();
 
-    private final float colorRed;
-    private final float colorGreen;
-    private final float colorBlue;
-
-    public TileEntityBubbleProviderRenderer(float colorRed, float colorGreen, float colorBlue)
+    public static void clearBubbles()
     {
-        this.colorRed = colorRed;
-        this.colorGreen = colorGreen;
-        this.colorBlue = colorBlue;
+        bubbleProviders.clear();
+    }
+
+    public static void addBubble(IBubbleProviderColored tile)
+    {
+        bubbleProviders.add(tile);
     }
 
     private static void updateModels()
@@ -45,29 +49,23 @@ public class TileEntityBubbleProviderRenderer<E extends TileEntity & IBubbleProv
         }
     }
 
-    @Override
-    public void renderTileEntityAt(E provider, double x, double y, double z, float partialTicks, int destroyStage)
+    public static void renderBubbles(EntityPlayer player, float partialTicks)
     {
-        if (!provider.getBubbleVisible())
-        {
-            return;
-        }
+        double interpPosX = player.lastTickPosX + (player.posX - player.lastTickPosX) * partialTicks;
+        double interpPosY = player.lastTickPosY + (player.posY - player.lastTickPosY) * partialTicks;
+        double interpPosZ = player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * partialTicks;
 
         updateModels();
 
-        GL11.glPushMatrix();
+        Minecraft.getMinecraft().renderEngine.bindTexture(TextureMap.locationBlocksTexture);
+
         GL11.glEnable(GL12.GL_RESCALE_NORMAL);
-        GL11.glTranslatef((float) x + 0.5F, (float) y + 1.0F, (float) z + 0.5F);
-
-        this.bindTexture(TextureMap.locationBlocksTexture);
-
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glDisable(GL11.GL_LIGHTING);
         GL11.glDisable(GL11.GL_CULL_FACE);
 
         GL11.glAlphaFunc(GL11.GL_GREATER, 0.1F);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-        GL11.glColor4f(this.colorRed / 2.0F, this.colorGreen / 2.0F, this.colorBlue / 2.0F, 1.0F);
         GL11.glMatrixMode(GL11.GL_TEXTURE);
         GL11.glLoadIdentity();
         GL11.glMatrixMode(GL11.GL_MODELVIEW);
@@ -75,10 +73,30 @@ public class TileEntityBubbleProviderRenderer<E extends TileEntity & IBubbleProv
         float lightMapSaveX = OpenGlHelper.lastBrightnessX;
         float lightMapSaveY = OpenGlHelper.lastBrightnessY;
         OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240F, 240F);
-        GL11.glScalef(provider.getBubbleSize(), provider.getBubbleSize(), provider.getBubbleSize());
 
-        int color = ColorUtil.to32BitColor(30, (int)(this.colorBlue / 2.0F * 255), (int)(this.colorGreen / 2.0F * 255), (int)(this.colorRed / 2.0F * 255));
-        ClientUtil.drawBakedModelColored(sphere, color);
+        for (IBubbleProviderColored provider : bubbleProviders)
+        {
+            TileEntity tile = (TileEntity) provider;
+            if (!provider.getBubbleVisible())
+            {
+                return;
+            }
+
+            GL11.glPushMatrix();
+
+            float x = (float) (tile.getPos().getX() - interpPosX);
+            float y = (float) (tile.getPos().getY() - interpPosY);
+            float z = (float) (tile.getPos().getZ() - interpPosZ);
+
+            GL11.glTranslatef(x + 0.5F, y + 1.0F, z + 0.5F);
+            GL11.glScalef(provider.getBubbleSize(), provider.getBubbleSize(), provider.getBubbleSize());
+
+            Vector3 colorVec = provider.getColor();
+            int color = ColorUtil.to32BitColor(30, (int)(colorVec.z * 255), (int)(colorVec.y * 255), (int)(colorVec.x * 255));
+            ClientUtil.drawBakedModelColored(sphere, color);
+
+            GL11.glPopMatrix();
+        }
 
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         GL11.glMatrixMode(GL11.GL_TEXTURE);
@@ -89,11 +107,8 @@ public class TileEntityBubbleProviderRenderer<E extends TileEntity & IBubbleProv
         GL11.glDisable(GL11.GL_BLEND);
         GL11.glDepthFunc(GL11.GL_LEQUAL);
         GL11.glEnable(GL11.GL_CULL_FACE);
+        GL11.glDisable(GL12.GL_RESCALE_NORMAL);
 
         OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, lightMapSaveX, lightMapSaveY);
-
-        GL11.glDisable(GL12.GL_RESCALE_NORMAL);
-        GL11.glPopMatrix();
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
     }
 }

src/main/java/micdoodle8/mods/galacticraft/core/TransformerHooks.java
@@ -7,6 +7,7 @@ import micdoodle8.mods.galacticraft.api.prefab.entity.EntitySpaceshipBase;
 import micdoodle8.mods.galacticraft.api.world.IGalacticraftWorldProvider;
 import micdoodle8.mods.galacticraft.api.world.IOrbitDimension;
 import micdoodle8.mods.galacticraft.api.world.IZeroGDimension;
+import micdoodle8.mods.galacticraft.core.client.BubbleRenderer;
 import micdoodle8.mods.galacticraft.core.client.FootprintRenderer;
 import micdoodle8.mods.galacticraft.core.client.SkyProviderOverworld;
 import micdoodle8.mods.galacticraft.core.dimension.WorldProviderMoon;
@@ -560,6 +561,7 @@ public class TransformerHooks
     {
         FootprintRenderer.renderFootprints(ClientProxyCore.mc.thePlayer, partialTicks);
         MinecraftForge.EVENT_BUS.post(new ClientProxyCore.EventSpecialRender(partialTicks));
+        BubbleRenderer.renderBubbles(ClientProxyCore.mc.thePlayer, partialTicks);
     }
     
     public static double armorDamageHook(EntityLivingBase entity)

src/main/java/micdoodle8/mods/galacticraft/core/entities/IBubbleProvider.java
@@ -2,7 +2,6 @@ package micdoodle8.mods.galacticraft.core.entities;
 
 public interface IBubbleProvider
 {
-    //    public IBubble getBubble();
     float getBubbleSize();
 
     void setBubbleVisible(boolean shouldRender);

src/main/java/micdoodle8/mods/galacticraft/core/entities/IBubbleProviderColored.java
@@ -1,0 +1,11 @@
+package micdoodle8.mods.galacticraft.core.entities;
+
+import micdoodle8.mods.galacticraft.api.vector.Vector3;
+
+/**
+ * To be combined with IBubbleProvider in the next major update
+ */
+public interface IBubbleProviderColored extends IBubbleProvider
+{
+    Vector3 getColor();
+}

src/main/java/micdoodle8/mods/galacticraft/core/proxy/ClientProxyCore.java
@@ -471,7 +471,7 @@ public class ClientProxyCore extends CommonProxyCore
     {
         ClientRegistry.bindTileEntitySpecialRenderer(TileEntityTreasureChest.class, new TileEntityTreasureChestRenderer());
         ClientRegistry.bindTileEntitySpecialRenderer(TileEntitySolar.class, new TileEntitySolarPanelRenderer());
-        ClientRegistry.bindTileEntitySpecialRenderer(TileEntityOxygenDistributor.class, new TileEntityBubbleProviderRenderer<>(0.25F, 0.25F, 1.0F));
+//        ClientRegistry.bindTileEntitySpecialRenderer(TileEntityOxygenDistributor.class, new TileEntityBubbleProviderRenderer<>(0.25F, 0.25F, 1.0F));
         ClientRegistry.bindTileEntitySpecialRenderer(TileEntityScreen.class, new TileEntityScreenRenderer());
         ClientRegistry.bindTileEntitySpecialRenderer(TileEntityFluidTank.class, new TileEntityFluidTankRenderer());
         ClientRegistry.bindTileEntitySpecialRenderer(TileEntityFluidPipe.class, new TileEntityFluidPipeRenderer());

src/main/java/micdoodle8/mods/galacticraft/core/tick/TickHandlerClient.java
@@ -4,7 +4,6 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.mojang.authlib.minecraft.MinecraftProfileTexture;
 import com.mojang.authlib.minecraft.MinecraftProfileTexture.Type;
-
 import micdoodle8.mods.galacticraft.api.block.IDetectableResource;
 import micdoodle8.mods.galacticraft.api.entity.IEntityNoisy;
 import micdoodle8.mods.galacticraft.api.entity.IIgnoreShift;
@@ -29,6 +28,7 @@ import micdoodle8.mods.galacticraft.core.client.jei.GalacticraftJEI;
 import micdoodle8.mods.galacticraft.core.dimension.WorldProviderMoon;
 import micdoodle8.mods.galacticraft.core.dimension.WorldProviderSpaceStation;
 import micdoodle8.mods.galacticraft.core.entities.EntityLander;
+import micdoodle8.mods.galacticraft.core.entities.IBubbleProviderColored;
 import micdoodle8.mods.galacticraft.core.entities.player.GCPlayerStatsClient;
 import micdoodle8.mods.galacticraft.core.fluid.FluidNetwork;
 import micdoodle8.mods.galacticraft.core.items.ItemSensorGlasses;
@@ -59,6 +59,7 @@ import net.minecraft.client.resources.DefaultPlayerSkin;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.settings.GameSettings;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.WorldProviderSurface;
@@ -71,7 +72,6 @@ import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
 import net.minecraftforge.fml.common.gameevent.TickEvent.RenderTickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.GL11;
@@ -401,6 +401,16 @@ public class TickHandlerClient
 
             if (TickHandlerClient.tickCount % 20 == 0)
             {
+                BubbleRenderer.clearBubbles();
+
+                for (TileEntity tile : player.worldObj.tickableTileEntities)
+                {
+                    if (tile instanceof IBubbleProviderColored)
+                    {
+                        BubbleRenderer.addBubble((IBubbleProviderColored)tile);
+                    }
+                }
+
                 if (updateJEIhiding)
                 {
                     updateJEIhiding = false;

src/main/java/micdoodle8/mods/galacticraft/core/tile/TileEntityOxygenDistributor.java
@@ -5,10 +5,11 @@ import micdoodle8.mods.galacticraft.api.block.IOxygenReliantBlock;
 import micdoodle8.mods.galacticraft.api.item.IItemOxygenSupply;
 import micdoodle8.mods.galacticraft.api.vector.BlockVec3;
 import micdoodle8.mods.galacticraft.api.vector.BlockVec3Dim;
+import micdoodle8.mods.galacticraft.api.vector.Vector3;
 import micdoodle8.mods.galacticraft.core.Constants;
 import micdoodle8.mods.galacticraft.core.blocks.BlockOxygenDistributor;
 import micdoodle8.mods.galacticraft.core.energy.item.ItemElectricBase;
-import micdoodle8.mods.galacticraft.core.entities.IBubbleProvider;
+import micdoodle8.mods.galacticraft.core.entities.IBubbleProviderColored;
 import micdoodle8.mods.galacticraft.core.inventory.IInventoryDefaults;
 import micdoodle8.mods.galacticraft.core.util.FluidUtil;
 import micdoodle8.mods.galacticraft.core.util.GCCoreUtil;
@@ -21,7 +22,10 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.*;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.MathHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -29,7 +33,7 @@ import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.List;
 
-public class TileEntityOxygenDistributor extends TileEntityOxygen implements IInventoryDefaults, ISidedInventory, IBubbleProvider
+public class TileEntityOxygenDistributor extends TileEntityOxygen implements IInventoryDefaults, ISidedInventory, IBubbleProviderColored
 {
     public boolean active;
     public boolean lastActive;
@@ -576,5 +580,11 @@ public class TileEntityOxygenDistributor extends TileEntityOxygen implements IIn
     public boolean getBubbleVisible()
     {
         return this.shouldRenderBubble;
+    }
+
+    @Override
+    public Vector3 getColor()
+    {
+        return new Vector3(0.125F, 0.125F, 0.5F);
     }
 }

src/main/java/micdoodle8/mods/galacticraft/planets/mars/MarsModuleClient.java
@@ -1,13 +1,11 @@
 package micdoodle8.mods.galacticraft.planets.mars;
 
 import com.google.common.collect.ImmutableList;
-
 import micdoodle8.mods.galacticraft.api.client.IItemMeshDefinitionCustom;
 import micdoodle8.mods.galacticraft.api.vector.Vector3;
 import micdoodle8.mods.galacticraft.api.world.IGalacticraftWorldProvider;
 import micdoodle8.mods.galacticraft.core.Constants;
 import micdoodle8.mods.galacticraft.core.client.CloudRenderer;
-import micdoodle8.mods.galacticraft.core.client.render.tile.TileEntityBubbleProviderRenderer;
 import micdoodle8.mods.galacticraft.core.util.ClientUtil;
 import micdoodle8.mods.galacticraft.core.wrappers.ModelTransformWrapper;
 import micdoodle8.mods.galacticraft.planets.GalacticraftPlanets;
@@ -170,7 +168,7 @@ public class MarsModuleClient implements IPlanetsModuleClient
 //        // Tile Entity Renderers
         ClientRegistry.bindTileEntitySpecialRenderer(TileEntityTreasureChestMars.class, new TileEntityTreasureChestRenderer());
 //            ClientRegistry.bindTileEntitySpecialRenderer(TileEntityCryogenicChamber.class, new TileEntityCryogenicChamberRenderer(chamberModel));
-        ClientRegistry.bindTileEntitySpecialRenderer(TileEntityTerraformer.class, new TileEntityBubbleProviderRenderer<>(0.25F, 1.0F, 0.25F));
+//        ClientRegistry.bindTileEntitySpecialRenderer(TileEntityTerraformer.class, new TileEntityBubbleProviderRenderer<>(0.25F, 1.0F, 0.25F));
 //
 //        // Entities
 

commit acd5d650a5a1ebab9104f84037c62679c38ed235

Fix water lighting bug introduced in 873008b. Close #3321.

src/main/java/micdoodle8/mods/galacticraft/core/client/BubbleRenderer.java
@@ -2,21 +2,22 @@ package micdoodle8.mods.galacticraft.core.client;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
+
 import micdoodle8.mods.galacticraft.api.vector.Vector3;
 import micdoodle8.mods.galacticraft.core.Constants;
 import micdoodle8.mods.galacticraft.core.entities.IBubbleProviderColored;
 import micdoodle8.mods.galacticraft.core.util.ClientUtil;
 import micdoodle8.mods.galacticraft.core.util.ColorUtil;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.IFlexibleBakedModel;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL12;
 
+import org.lwjgl.opengl.GL11;
 import java.util.List;
 
 public class BubbleRenderer
@@ -36,40 +37,47 @@ public class BubbleRenderer
 
     private static void updateModels()
     {
-        if (sphere == null)
+        try
         {
-            try
-            {
-                sphere = ClientUtil.modelFromOBJ(new ResourceLocation(Constants.ASSET_PREFIX, "sphere.obj"), ImmutableList.of("Sphere"));
-            }
-            catch (Exception e)
-            {
-                throw new RuntimeException(e);
-            }
+            sphere = ClientUtil.modelFromOBJ(new ResourceLocation(Constants.ASSET_PREFIX, "sphere.obj"), ImmutableList.of("Sphere"));
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
         }
     }
 
     public static void renderBubbles(EntityPlayer player, float partialTicks)
     {
+        if (bubbleProviders.isEmpty())
+        {
+            return;
+        }
+
         double interpPosX = player.lastTickPosX + (player.posX - player.lastTickPosX) * partialTicks;
         double interpPosY = player.lastTickPosY + (player.posY - player.lastTickPosY) * partialTicks;
         double interpPosZ = player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * partialTicks;
 
-        updateModels();
+        if (sphere == null)
+        {
+            updateModels();
+        }
 
         Minecraft.getMinecraft().renderEngine.bindTexture(TextureMap.locationBlocksTexture);
 
-        GL11.glEnable(GL12.GL_RESCALE_NORMAL);
-        GL11.glEnable(GL11.GL_BLEND);
-        GL11.glDisable(GL11.GL_LIGHTING);
-        GL11.glDisable(GL11.GL_CULL_FACE);
+        GlStateManager.pushMatrix();
+        
+        GlStateManager.enableRescaleNormal();
+        GlStateManager.enableBlend();
+        GlStateManager.disableLighting();
+        GlStateManager.disableCull();
 
-        GL11.glAlphaFunc(GL11.GL_GREATER, 0.1F);
-        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-        GL11.glMatrixMode(GL11.GL_TEXTURE);
-        GL11.glLoadIdentity();
-        GL11.glMatrixMode(GL11.GL_MODELVIEW);
-        GL11.glDepthMask(false);
+        GlStateManager.alphaFunc(GL11.GL_GREATER, 0.1F);
+        GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GlStateManager.matrixMode(GL11.GL_TEXTURE);
+        GlStateManager.loadIdentity();
+        GlStateManager.matrixMode(GL11.GL_MODELVIEW);
+        GlStateManager.depthMask(false);
         float lightMapSaveX = OpenGlHelper.lastBrightnessX;
         float lightMapSaveY = OpenGlHelper.lastBrightnessY;
         OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240F, 240F);
@@ -79,7 +87,7 @@ public class BubbleRenderer
             TileEntity tile = (TileEntity) provider;
             if (!provider.getBubbleVisible())
             {
-                return;
+                continue;
             }
 
             GL11.glPushMatrix();
@@ -98,17 +106,7 @@ public class BubbleRenderer
             GL11.glPopMatrix();
         }
 
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        GL11.glMatrixMode(GL11.GL_TEXTURE);
-        GL11.glDepthMask(true);
-        GL11.glLoadIdentity();
-        GL11.glMatrixMode(GL11.GL_MODELVIEW);
-        GL11.glEnable(GL11.GL_LIGHTING);
-        GL11.glDisable(GL11.GL_BLEND);
-        GL11.glDepthFunc(GL11.GL_LEQUAL);
-        GL11.glEnable(GL11.GL_CULL_FACE);
-        GL11.glDisable(GL12.GL_RESCALE_NORMAL);
-
         OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, lightMapSaveX, lightMapSaveY);
+        GlStateManager.popMatrix();
     }
 }

src/main/java/micdoodle8/mods/galacticraft/core/client/FootprintRenderer.java
@@ -13,10 +13,12 @@ import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.client.FMLClientHandler;
+
 import org.lwjgl.opengl.GL11;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -28,20 +30,38 @@ public class FootprintRenderer
 
     public static void renderFootprints(EntityPlayer player, float partialTicks)
     {
-        GL11.glPushMatrix();
+        int dimActive = GCCoreUtil.getDimensionID(player.worldObj);
+        List<Footprint> footprintsToDraw = new LinkedList<>();
+
+        for (List<Footprint> footprintList : footprints.values())
+        {
+            for (Footprint footprint : footprintList)
+            {
+                if (footprint.dimension == dimActive)
+                {
+                    footprintsToDraw.add(footprint);
+                }
+            }
+        }
+
+        if (footprintsToDraw.isEmpty())
+        {
+            return;
+        }
+
+        GlStateManager.pushMatrix();
         double interpPosX = player.lastTickPosX + (player.posX - player.lastTickPosX) * partialTicks;
         double interpPosY = player.lastTickPosY + (player.posY - player.lastTickPosY) * partialTicks;
         double interpPosZ = player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * partialTicks;
         FMLClientHandler.instance().getClient().renderEngine.bindTexture(FootprintRenderer.footprintTexture);
 
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        GL11.glDepthMask(true);
-        GL11.glEnable(GL11.GL_DEPTH_TEST);
-        GL11.glEnable(GL11.GL_TEXTURE_2D);
-        GL11.glDisable(GL11.GL_CULL_FACE);
+        GlStateManager.depthMask(true);
+        GlStateManager.enableDepth();
+        GlStateManager.enableTexture2D();
+        GlStateManager.disableCull();
 
-        GL11.glEnable(GL11.GL_BLEND);
-        OpenGlHelper.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, 1, 0);
+        GlStateManager.enableBlend();
+        GlStateManager.tryBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, 1, 0);
         Tessellator tessellator = Tessellator.getInstance();
         float f7 = 1.0F;
         float f6 = 0.0F;
@@ -49,7 +69,7 @@ public class FootprintRenderer
         float f9 = 1.0F;
 
         float f10 = 0.4F;
-        GL11.glAlphaFunc(GL11.GL_GREATER, 0.1F);
+        GlStateManager.alphaFunc(GL11.GL_GREATER, 0.1F);
         float lightMapSaveX = OpenGlHelper.lastBrightnessX;
         float lightMapSaveY = OpenGlHelper.lastBrightnessY;
         boolean sensorGlasses = OverlaySensorGlasses.overrideMobTexture();
@@ -60,52 +80,46 @@ public class FootprintRenderer
 
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
 
-        for (List<Footprint> footprintList : footprints.values())
+        for (Footprint footprint : footprintsToDraw)
         {
-            for (Footprint footprint : footprintList)
+            GL11.glPushMatrix();
+
+            if (!sensorGlasses)
             {
-                if (footprint.dimension == GCCoreUtil.getDimensionID(player.worldObj))
-                {
-                    GL11.glPushMatrix();
-
-                    if (!sensorGlasses)
-                    {
-                        int j = footprint.lightmapVal % 65536;
-                        int k = footprint.lightmapVal / 65536;
-                        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float)j / 1.0F, (float)k / 1.0F);
-                    }
-
-                    float ageScale = footprint.age / (float) Footprint.MAX_AGE;
-                    WorldRenderer worldRenderer = tessellator.getWorldRenderer();
-                    worldRenderer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
-
-                    float f11 = (float) (footprint.position.x - interpPosX);
-                    float f12 = (float) (footprint.position.y - interpPosY) + 0.001F;
-                    float f13 = (float) (footprint.position.z - interpPosZ);
-
-                    GL11.glTranslatef(f11, f12, f13);
-
-
-                    int brightness = (int) (100 + ageScale * 155);
-//                    worldRenderer.putBrightness4(brightness, brightness, brightness, brightness);
-                    GL11.glColor4f(1 - ageScale, 1 - ageScale, 1 - ageScale, 1 - ageScale);
-                    double footprintScale = 0.5F;
-                    worldRenderer.pos(Math.sin((45 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale, 0, Math.cos((45 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale).tex(f7, f9).endVertex();
-                    worldRenderer.pos(Math.sin((135 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale, 0, Math.cos((135 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale).tex(f7, f8).endVertex();
-                    worldRenderer.pos(Math.sin((225 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale, 0, Math.cos((225 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale).tex(f6, f8).endVertex();
-                    worldRenderer.pos(Math.sin((315 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale, 0, Math.cos((315 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale).tex(f6, f9).endVertex();
-
-                    tessellator.draw();
-                    GL11.glPopMatrix();
-                }
+                int j = footprint.lightmapVal % 65536;
+                int k = footprint.lightmapVal / 65536;
+                OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float)j / 1.0F, (float)k / 1.0F);
             }
+
+            float ageScale = footprint.age / (float) Footprint.MAX_AGE;
+            WorldRenderer worldRenderer = tessellator.getWorldRenderer();
+            worldRenderer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
+
+            float f11 = (float) (footprint.position.x - interpPosX);
+            float f12 = (float) (footprint.position.y - interpPosY) + 0.001F;
+            float f13 = (float) (footprint.position.z - interpPosZ);
+
+            GL11.glTranslatef(f11, f12, f13);
+
+            int brightness = (int) (100 + ageScale * 155);
+            //                    worldRenderer.putBrightness4(brightness, brightness, brightness, brightness);
+            GlStateManager.color(1 - ageScale, 1 - ageScale, 1 - ageScale, 1 - ageScale);
+            double footprintScale = 0.5F;
+            worldRenderer.pos(Math.sin((45 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale, 0, Math.cos((45 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale).tex(f7, f9).endVertex();
+            worldRenderer.pos(Math.sin((135 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale, 0, Math.cos((135 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale).tex(f7, f8).endVertex();
+            worldRenderer.pos(Math.sin((225 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale, 0, Math.cos((225 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale).tex(f6, f8).endVertex();
+            worldRenderer.pos(Math.sin((315 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale, 0, Math.cos((315 - footprint.rotation) / Constants.RADIANS_TO_DEGREES_D) * footprintScale).tex(f6, f9).endVertex();
+
+            tessellator.draw();
+            GlStateManager.popMatrix();
         }
 
-        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, lightMapSaveX, lightMapSaveY);
+        if (sensorGlasses)
+        {
+            OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, lightMapSaveX, lightMapSaveY);
+        }
 
-        GL11.glDisable(GL11.GL_BLEND);
-        GL11.glEnable(GL11.GL_CULL_FACE);
-        GL11.glPopMatrix();
+        GlStateManager.popMatrix();
     }
 
     public static void addFootprint(long chunkKey, Footprint footprint)
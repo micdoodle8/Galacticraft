acd5d650a5a1ebab9104f84037c62679c38ed235
radfast <rradfast@gmail.com>
2018-04-05 23:29:22 +0100

    Fix water lighting bug introduced in 873008b. Close #3321.

--- src/main/java/micdoodle8/mods/galacticraft/core/client/BubbleRenderer.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/client/BubbleRenderer.java
@@ -2,21 +2,22 @@ package micdoodle8.mods.galacticraft.core.client;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
+
 import micdoodle8.mods.galacticraft.api.vector.Vector3;
 import micdoodle8.mods.galacticraft.core.Constants;
 import micdoodle8.mods.galacticraft.core.entities.IBubbleProviderColored;
 import micdoodle8.mods.galacticraft.core.util.ClientUtil;
 import micdoodle8.mods.galacticraft.core.util.ColorUtil;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.IFlexibleBakedModel;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL12;
 
+import org.lwjgl.opengl.GL11;
 import java.util.List;
 
 public class BubbleRenderer
@@ -36,40 +37,47 @@ public class BubbleRenderer
 
     private static void updateModels()
     {
-        if (sphere == null)
+        try
         {
-            try
-            {
-                sphere = ClientUtil.modelFromOBJ(new ResourceLocation(Constants.ASSET_PREFIX, "sphere.obj"), ImmutableList.of("Sphere"));
-            }
-            catch (Exception e)
-            {
-                throw new RuntimeException(e);
-            }
+            sphere = ClientUtil.modelFromOBJ(new ResourceLocation(Constants.ASSET_PREFIX, "sphere.obj"), ImmutableList.of("Sphere"));
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
         }
     }
 
     public static void renderBubbles(EntityPlayer player, float partialTicks)
     {
+        if (bubbleProviders.isEmpty())
+        {
+            return;
+        }
+
         double interpPosX = player.lastTickPosX + (player.posX - player.lastTickPosX) * partialTicks;
         double interpPosY = player.lastTickPosY + (player.posY - player.lastTickPosY) * partialTicks;
         double interpPosZ = player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * partialTicks;
 
-        updateModels();
+        if (sphere == null)
+        {
+            updateModels();
+        }
 
         Minecraft.getMinecraft().renderEngine.bindTexture(TextureMap.locationBlocksTexture);
 
-        GL11.glEnable(GL12.GL_RESCALE_NORMAL);
-        GL11.glEnable(GL11.GL_BLEND);
-        GL11.glDisable(GL11.GL_LIGHTING);
-        GL11.glDisable(GL11.GL_CULL_FACE);
+        GlStateManager.pushMatrix();
+        
+        GlStateManager.enableRescaleNormal();
+        GlStateManager.enableBlend();
+        GlStateManager.disableLighting();
+        GlStateManager.disableCull();
 
-        GL11.glAlphaFunc(GL11.GL_GREATER, 0.1F);
-        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-        GL11.glMatrixMode(GL11.GL_TEXTURE);
-        GL11.glLoadIdentity();
-        GL11.glMatrixMode(GL11.GL_MODELVIEW);
-        GL11.glDepthMask(false);
+        GlStateManager.alphaFunc(GL11.GL_GREATER, 0.1F);
+        GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GlStateManager.matrixMode(GL11.GL_TEXTURE);
+        GlStateManager.loadIdentity();
+        GlStateManager.matrixMode(GL11.GL_MODELVIEW);
+        GlStateManager.depthMask(false);
         float lightMapSaveX = OpenGlHelper.lastBrightnessX;
         float lightMapSaveY = OpenGlHelper.lastBrightnessY;
         OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240F, 240F);
@@ -79,7 +87,7 @@ public class BubbleRenderer
             TileEntity tile = (TileEntity) provider;
             if (!provider.getBubbleVisible())
             {
-                return;
+                continue;
             }
 
             GL11.glPushMatrix();
@@ -98,17 +106,7 @@ public class BubbleRenderer
             GL11.glPopMatrix();
         }
 
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        GL11.glMatrixMode(GL11.GL_TEXTURE);
-        GL11.glDepthMask(true);
-        GL11.glLoadIdentity();
-        GL11.glMatrixMode(GL11.GL_MODELVIEW);
-        GL11.glEnable(GL11.GL_LIGHTING);
-        GL11.glDisable(GL11.GL_BLEND);
-        GL11.glDepthFunc(GL11.GL_LEQUAL);
-        GL11.glEnable(GL11.GL_CULL_FACE);
-        GL11.glDisable(GL12.GL_RESCALE_NORMAL);
-
         OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, lightMapSaveX, lightMapSaveY);
+        GlStateManager.popMatrix();
     }
 }
