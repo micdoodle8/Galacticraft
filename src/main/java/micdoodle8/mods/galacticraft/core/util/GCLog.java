778c5a54dc473594ce5a4a20345d348214c6bc8b
marcus8448 <28928887+marcus8448@users.noreply.github.com>
2018-04-30 19:28:24 -0700

    ARGH. Formatting fail.
    Sorry all,
    This one fixes formatting. Now [Galacticraft]: "" not Galacticraft:
    Saves line space though.

--- src/main/java/micdoodle8/mods/galacticraft/core/util/GCLog.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/util/GCLog.java
@@ -1,29 +1,31 @@
 package micdoodle8.mods.galacticraft.core.util;
 
 import micdoodle8.mods.galacticraft.core.Constants;
+
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class GCLog
 {
     
-    private static Logger log;
+    private static Logger log = LogManager.getFormatterLogger(Constants.MOD_NAME_SIMPLE);
     
     public static void info(String message)
     {
-        log.info(Constants.MOD_NAME_SIMPLE + ": "+ message);
+        log.info(message);
         
     }
 
     public static void severe(String message)
     {
-        log.error(Constants.MOD_NAME_SIMPLE + ": " + message);
+        log.error(message);
     }
 
     public static void debug(String message)
     {
         if (ConfigManagerCore.enableDebug)
         {
-            log.debug(Constants.MOD_NAME_SIMPLE + ": "+ message);
+            log.debug(message);
         }
     }
 
