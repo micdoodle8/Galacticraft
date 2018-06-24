44234f7b861dd95a7fb3cee2251d60dd502dc3f5
radfast <rradfast@gmail.com>
2018-06-24 22:41:39 +0100

    Fix Frequency Module render with F3+B box. Credit SteveKunG #3421.

--- src/main/java/micdoodle8/mods/galacticraft/core/client/render/entities/layer/LayerFrequencyModule.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/client/render/entities/layer/LayerFrequencyModule.java
@@ -24,8 +24,6 @@ import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-import org.lwjgl.opengl.GL11;
-
 @SideOnly(Side.CLIENT)
 public class LayerFrequencyModule implements LayerRenderer<AbstractClientPlayer>
 {
@@ -74,15 +72,6 @@ public class LayerFrequencyModule implements LayerRenderer<AbstractClientPlayer>
                     this.updateModels();
                     GlStateManager.pushMatrix();
                     Minecraft.getMinecraft().getTextureManager().bindTexture(TextureMap.locationBlocksTexture);
-
-                    if (Minecraft.isAmbientOcclusionEnabled())
-                    {
-                        GlStateManager.shadeModel(GL11.GL_SMOOTH);
-                    }
-                    else
-                    {
-                        GlStateManager.shadeModel(GL11.GL_FLAT);
-                    }
 
                     GlStateManager.rotate(180, 1, 0, 0);
                     GlStateManager.pushMatrix();
