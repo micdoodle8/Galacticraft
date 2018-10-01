4861f941f29a4e6e4e0f311ea64fcbc55a745740
marcus8448 <marcus2475@outlook.com>
2018-09-30 18:35:57 -0700

    GC Planets achievements
    (Mars, Asteroid belt & Venus)

--- src/main/java/micdoodle8/mods/galacticraft/planets/GalacticraftPlanets.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/GalacticraftPlanets.java
@@ -2,21 +2,28 @@ package micdoodle8.mods.galacticraft.planets;
 
 import micdoodle8.mods.galacticraft.api.vector.Vector3;
 import micdoodle8.mods.galacticraft.core.Constants;
+import micdoodle8.mods.galacticraft.core.GalacticraftCore;
 import micdoodle8.mods.galacticraft.core.tile.TileEntityDeconstructor;
 import micdoodle8.mods.galacticraft.core.util.CompatibilityManager;
 import micdoodle8.mods.galacticraft.core.util.ConfigManagerCore;
 import micdoodle8.mods.galacticraft.core.util.GCLog;
 import micdoodle8.mods.galacticraft.planets.asteroids.AsteroidsModule;
 import micdoodle8.mods.galacticraft.planets.asteroids.ConfigManagerAsteroids;
+import micdoodle8.mods.galacticraft.planets.asteroids.items.AsteroidsItems;
 import micdoodle8.mods.galacticraft.planets.asteroids.world.gen.BiomeGenBaseAsteroids;
 import micdoodle8.mods.galacticraft.planets.mars.ConfigManagerMars;
 import micdoodle8.mods.galacticraft.planets.mars.MarsModule;
 import micdoodle8.mods.galacticraft.planets.mars.entities.MFRSpawnHandlerSlimeling;
+import micdoodle8.mods.galacticraft.planets.mars.items.MarsItems;
 import micdoodle8.mods.galacticraft.planets.venus.ConfigManagerVenus;
 import micdoodle8.mods.galacticraft.planets.venus.VenusModule;
 import micdoodle8.mods.galacticraft.planets.venus.world.gen.BiomeGenBaseVenus;
+
+import net.minecraftforge.common.AchievementPage;
 import net.minecraftforge.common.MinecraftForge;
 import micdoodle8.mods.galacticraft.planets.mars.world.gen.BiomeGenBaseMars;
+
+import net.minecraft.stats.Achievement;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraftforge.common.config.ConfigElement;
 import net.minecraftforge.common.config.Property;
@@ -57,6 +64,10 @@ public class GalacticraftPlanets
     public static PlanetsProxy proxy;
 
     public static Map<String, List<String>> propOrder = new TreeMap<>();
+    
+    public static Achievement mars;
+    public static Achievement asteroids;
+    public static Achievement venus;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
@@ -115,6 +126,13 @@ public class GalacticraftPlanets
         	GCLog.severe("Error when attempting to register Slimeling auto-spawnhandler in MFR");
         	GCLog.exception(e);
         }
+        mars = new Achievement("achievement.mars", "mars", 3, 0, MarsItems.rocketMars, GalacticraftCore.moon).registerStat();
+        asteroids = new Achievement("achievement.astroids", "asteroids", 6, -2, AsteroidsItems.tier3Rocket, GalacticraftPlanets.mars).registerStat();
+        venus = new Achievement("achievement.venus", "venus", 6, 2, AsteroidsItems.tier3Rocket, GalacticraftPlanets.mars).registerStat();
+        AchievementPage page = AchievementPage.getAchievementPage("Galacticraft");
+        page.getAchievements().add(1, mars);
+        page.getAchievements().add(2, asteroids);
+        page.getAchievements().add(3, venus);
     }
 
     @EventHandler
