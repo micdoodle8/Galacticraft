585ad61e6f86c684435a068fe7fd879e26a5a2f0
marcus8448 <28928887+marcus8448@users.noreply.github.com>
2019-02-03 16:48:26 -0800

    Use maven repos instead of dependencies/ folder
    Note: Removes MFR compat (but AFAIK there's no MFR for 1.12)

--- dependencies/main/java/buildcraft/api/facades/IFacade.java
+++
@@ -1,9 +0,0 @@
-package buildcraft.api.facades;
-
-public interface IFacade {
-    FacadeType getType();
-
-    boolean isHollow();
-
-    IFacadePhasedState[] getPhasedStates();
-}
--- dependencies/main/java/mezz/jei/api/recipe/BlankRecipeWrapper.java
+++
@@ -1,8 +0,0 @@
-package mezz.jei.api.recipe;
-
-/**
- * @deprecated since JEI 4.6.0. This was replaced by default methods in {@link IRecipeWrapper}.
- */
-public abstract class BlankRecipeWrapper implements IRecipeWrapper {
-
-}
