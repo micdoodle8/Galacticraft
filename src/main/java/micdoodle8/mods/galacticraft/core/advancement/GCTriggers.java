d2fe15094d7b9d9c8c6a19ddac67a2284bf97e2c
radfast <rradfast@gmail.com>
2019-10-18 01:57:04 +0100

    Advancements - criteria registration was not visible in older Forge builds.

--- src/main/java/micdoodle8/mods/galacticraft/core/advancement/GCTriggers.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/advancement/GCTriggers.java
@@ -1,7 +1,11 @@
 package micdoodle8.mods.galacticraft.core.advancement;
 
+import java.lang.reflect.Method;
+
 import micdoodle8.mods.galacticraft.core.advancement.criterion.GenericTrigger;
+import micdoodle8.mods.galacticraft.core.util.GCCoreUtil;
 import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.advancements.ICriterionTrigger;
 
 public class GCTriggers
 {
@@ -9,9 +13,27 @@ public class GCTriggers
     public static final GenericTrigger KILL_MOON_BOSS = new GenericTrigger("boss_moon");
     public static final GenericTrigger CREATE_SPACE_STATION = new GenericTrigger("create_space_station");
 
-    public static void registerTriggers() {
-        CriteriaTriggers.register(GCTriggers.LAUNCH_ROCKET);
-        CriteriaTriggers.register(GCTriggers.KILL_MOON_BOSS);
-        CriteriaTriggers.register(GCTriggers.CREATE_SPACE_STATION);
+    public static void registerTriggers()
+    {
+        Method register = null;
+        try {
+            Class clazz = CriteriaTriggers.class;
+            Method[] mm = clazz.getDeclaredMethods();
+            for (Method m : mm)
+                if (m.getParameterTypes()[0] == ICriterionTrigger.class)
+                {
+                    register = m;
+                    break;
+                }
+        } catch (Exception e) { e.printStackTrace(); }
+        
+        if (register != null)
+        {
+            try {
+                register.invoke(null, GCTriggers.LAUNCH_ROCKET);
+                register.invoke(null, GCTriggers.KILL_MOON_BOSS);
+                register.invoke(null, GCTriggers.CREATE_SPACE_STATION);
+            } catch (Exception ignore) {}
+        }
     }
 }
